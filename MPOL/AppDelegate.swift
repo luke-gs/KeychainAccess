8a1f443edb6af5d5338911b9254617802ae019df
Vazmista <val@gridstone.com.au>
2017-02-14 16:43:31 +1100

    Pods update + APNS
    Added pods for
    - Alamofire
    - RocketData
    - AppDynamics
    
    Set up registering for APNS using iOS 10 userNotifications

--- MPOL/AppDelegate.swift
+++ MPOL/AppDelegate.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
     var tabBarController: UITabBarController?
 
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.registerPushNotifications(application)
         
         let tabBarController = UITabBarController()
         
@@ -27,6 +30,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.makeKeyAndVisible()
         
         return true
+    }
+    
+   // MARK: - APNS
+    
+    func registerPushNotifications(_ application: UIApplication) {
+        
+        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.delegate = self
+        notificationCenter.requestAuthorization(options: [.badge,.sound, .alert]) { (granted, error) in
+            if error == nil {
+                application.registerForRemoteNotifications()
+            }
+        }
+
+    }
+    
+    // Called to represent what action was selected by the user
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+    }
+    
+    // Called when notification is delivered to a foreground app
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        var token = ""
+        for i in 0..<deviceToken.count {
+            token = token + String(format: "%02.2hhx", arguments: [deviceToken[i]])
+        }
+        print(token)
+        
+        // TODO: Upload token to server & register for PNS
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for push notification:\(error)")
     }
 
 }
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'MPOL' do
+  pod 'Alamofire', '~> 4.2.0'
+  pod 'AppDynamicsAgent', '~> 42.11.0'
+  pod 'RocketData', '~> 4.0.1'
+end
