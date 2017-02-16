98e6efbbd0f4ae7128eea4fd6803281dce89582b
Rod Brown <rodney.brown6@icloud.com>
2017-02-16 11:36:11 +1100

    Color improvements

--- MPOLKit/UI/Extensions/UIColor+Hex.swift
+++ MPOLKit/UI/Extensions/UIColor+Hex.swift
@@ -18,30 +18,43 @@ extension UIColor {
         var hex = hexString
         
         // Check for and remove the hash
-        let hexStart = hex.startIndex
+        let hexStartIndex = hex.startIndex
         if hex.hasPrefix("#") {
-            hex.remove(at: hexStart)
+            hex.remove(at: hexStartIndex)
         }
         
         if hex.characters.count == 3 {
-            hex = hex + "F"
+            hex += "F"
         } else if hex.characters.count == 6 {
-            hex = hex + "FF"
+            hex += "FF"
         }
         
-        if (hex.range(of: "(^[0-9A-Fa-f]{8}$)|(^[0-9A-Fa-f]{4}$)", options: .regularExpression) == nil) { return nil }
+        if hex.range(of: "(^[0-9A-Fa-f]{8}$)|(^[0-9A-Fa-f]{4}$)", options: .regularExpression) == nil { return nil }
         
+        let greenIndex: String.Index
+        let blueIndex:  String.Index
+        let alphaIndex: String.Index
         if hex.characters.count == 4 {
-            hex.insert(hex[hexStart], at: hexStart)
-            hex.insert(hex[hex.index(hexStart, offsetBy: 2)], at: hex.index(hexStart, offsetBy: 2))
-            hex.insert(hex[hex.index(hexStart, offsetBy: 4)], at: hex.index(hexStart, offsetBy: 4))
-            hex.insert(hex[hex.index(hexStart, offsetBy: 6)], at: hex.index(hexStart, offsetBy: 6))
+            hex.insert(hex[hexStartIndex], at: hexStartIndex)
+            
+            greenIndex = hex.index(hexStartIndex, offsetBy: 2)
+            hex.insert(hex[greenIndex], at: greenIndex)
+            
+            blueIndex = hex.index(hexStartIndex, offsetBy: 4)
+            hex.insert(hex[blueIndex], at: blueIndex)
+            
+            alphaIndex = hex.index(hexStartIndex, offsetBy: 6)
+            hex.insert(hex[alphaIndex], at: alphaIndex)
+        } else {
+            greenIndex = hex.index(hexStartIndex, offsetBy: 2)
+            blueIndex  = hex.index(hexStartIndex, offsetBy: 4)
+            alphaIndex = hex.index(hexStartIndex, offsetBy: 6)
         }
         
-        let redHex   = hex.substring(to: hex.index(hexStart, offsetBy: 2))
-        let greenHex = hex[hex.index(hexStart, offsetBy: 2) ..< hex.index(hexStart, offsetBy: 4)]
-        let blueHex  = hex[hex.index(hexStart, offsetBy: 4) ..< hex.index(hexStart, offsetBy: 6)]
-        let alphaHex = hex.substring(from: hex.index(hexStart, offsetBy: 6))
+        let redHex   = hex.substring(to: greenIndex)
+        let greenHex = hex[greenIndex ..< blueIndex]
+        let blueHex  = hex[blueIndex ..< alphaIndex]
+        let alphaHex = hex.substring(from: alphaIndex)
         
         var redInt:   CUnsignedInt = 0
         var greenInt: CUnsignedInt = 0
