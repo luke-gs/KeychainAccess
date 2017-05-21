04d49dab9c36a778c42ec215f991c65f54b0f1da
Herli Halim <herli@gridstone.com.au>
2017-05-21 14:44:26 +1000

    Implemented telephone number parsing

--- Classes/Core/Model/TelephoneNumber.swift
+++ Classes/Core/Model/TelephoneNumber.swift
@@ -12,6 +12,17 @@ open class TelephoneNumber: NSObject, Serialisable {
 
     open let id: String
     
+    open var suffix: String?
+    open var cityCode: String?
+    open var fullNumber: String?
+    open var prefix: String?
+    open var subscriber: String?
+    open var areaCode: String?
+    open var exchange: String?
+    open var numberType: String?
+    open var countryCode: String?
+    
+    
     public init(id: String) {
         self.id = id
         super.init()
@@ -22,6 +33,16 @@ open class TelephoneNumber: NSObject, Serialisable {
             throw ParsingError.missingRequiredField
         }
         self.id = id
+    
+        suffix = unboxer.unbox(key: "suffix")
+        cityCode = unboxer.unbox(key: "cityCode")
+        fullNumber = unboxer.unbox(key: "fullNumber")
+        prefix = unboxer.unbox(key: "prefix")
+        subscriber = unboxer.unbox(key: "subscriber")
+        areaCode = unboxer.unbox(key: "areaCode")
+        exchange = unboxer.unbox(key: "exchange")
+        numberType = unboxer.unbox(key: "numberType")
+        countryCode = unboxer.unbox(key: "countryCode")
     }
     
     public required init?(coder aDecoder: NSCoder) {
