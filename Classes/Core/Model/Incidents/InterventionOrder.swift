5f7f6c8287bf85512133ef04839fdc1424bab9c3
Herli Halim <herli@gridstone.com.au>
2017-05-21 15:29:52 +1000

    Implemented Intervetion Order parsing

--- Classes/Core/Model/Incidents/InterventionOrder.swift
+++ Classes/Core/Model/Incidents/InterventionOrder.swift
@@ -12,16 +12,36 @@ class InterventionOrder: NSObject, Serialisable {
 
     open let id: String
     
+    open var servedDate: Date?
+    open var respondentName: String?
+    open var type: String?
+    open var complainants: [NSObject]?
+    open var conditions: [NSObject]?
+    open var status: String?
+    open var respondentDateOfBirth: Date?
+    open var address: String?
+    
     public init(id: String) {
         self.id = id
         super.init()
     }
+    
+    private static let dateTransformer: ISO8601DateTransformer = ISO8601DateTransformer.shared
     
     public required init(unboxer: Unboxer) throws {
         guard let id: String = unboxer.unbox(key: "id") else {
             throw ParsingError.missingRequiredField
         }
         self.id = id
+        
+        servedDate = unboxer.unbox(key: "orderServed", formatter: InterventionOrder.dateTransformer)
+        respondentName = unboxer.unbox(key: "respondentName")
+        type = unboxer.unbox(key: "orderType")
+        complainants = unboxer.unbox(key: "complainants")
+        conditions = unboxer.unbox(key: "conditions")
+        status = unboxer.unbox(key: "status")
+        respondentDateOfBirth = unboxer.unbox(key: "respondentDateOfBirth", formatter: InterventionOrder.dateTransformer)
+        address = unboxer.unbox(key: "orderAddress")
     }
     
     public required init?(coder aDecoder: NSCoder) {
