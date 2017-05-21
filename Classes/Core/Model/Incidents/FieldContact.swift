f51282c5af7fd33c10e97d22b89b33184587a911
Herli Halim <herli@gridstone.com.au>
2017-05-21 15:24:51 +1000

    Implemented Field Contact parsing

--- Classes/Core/Model/Incidents/FieldContact.swift
+++ Classes/Core/Model/Incidents/FieldContact.swift
@@ -12,16 +12,50 @@ open class FieldContact: NSObject, Serialisable {
 
     open let id: String
     
+    open var remarks: String?
+    
+    open var contactMember: Member?
+    open var secondaryContactMember: Member?
+    open var contactDate: Date?
+    open var status: String?
+    open var areaType: String?
+    
+    // MARK: - ????
+    open var locationResponseZone: String?
+    open var neighbourhoodWatchArea: String?
+    open var contactLocation: String?
+    open var contactDescriptions: [String]?
+    open var localGovernmentArea: String?
+    open var reportingStation: String?
+    
     public init(id: String) {
         self.id = id
         super.init()
     }
+    
+    private static let dateTransformer: ISO8601DateTransformer = ISO8601DateTransformer.shared
     
     public required init(unboxer: Unboxer) throws {
         guard let id: String = unboxer.unbox(key: "id") else {
             throw ParsingError.missingRequiredField
         }
         self.id = id
+        
+        remarks = unboxer.unbox(key: "remarks")
+        
+        contactMember = unboxer.unbox(key: "contactMember")
+        secondaryContactMember = unboxer.unbox(key: "secondaryContactMember")
+        contactDate = unboxer.unbox(key: "contactDate", formatter: FieldContact.dateTransformer)
+        status = unboxer.unbox(key: "status")
+        areaType = unboxer.unbox(key: "areaType")
+        
+        locationResponseZone = unboxer.unbox(key: "locationResponseZone")
+        neighbourhoodWatchArea = unboxer.unbox(key: "neighbourhoodWatchArea")
+        contactLocation = unboxer.unbox(key: "contactLocation")
+        // Note contactDescription
+        contactDescriptions = unboxer.unbox(key: "contactDescription")
+        localGovernmentArea = unboxer.unbox(key: "localGovernmentArea")
+        reportingStation = unboxer.unbox(key: "reportingStation")
     }
     
     public required init?(coder aDecoder: NSCoder) {
