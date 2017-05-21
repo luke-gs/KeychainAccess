a787676d37f29f65df2589bd709dd0f734ad2c4d
Herli Halim <herli@gridstone.com.au>
2017-05-21 14:55:55 +1000

    Implemented missing person parsing

--- Classes/Core/Model/Incidents/MissingPerson.swift
+++ Classes/Core/Model/Incidents/MissingPerson.swift
@@ -23,15 +23,14 @@ open class MissingPerson: NSObject, Serialisable {
     
     open var physicalHealthDescription: String?
     open var mentalHeathDescription: String?
-    open var reportingStation: String?
     open var probableCause: String?
     
     open var phoneNumbers: [TelephoneNumber]?
     
     // MARK: - ???
-    open var locatingMemberId: String?
+    open var reportingStation: String?
+    open var locatingMemberID: String?
     open var locatingMemberOrganisationalUnit: String?
-    
     
     open var missingPersonReports: [String]?
     open var probableDestination: String?
@@ -41,11 +40,38 @@ open class MissingPerson: NSObject, Serialisable {
         super.init()
     }
     
+    private static let dateTransformer: ISO8601DateTransformer = ISO8601DateTransformer.shared
+    
     public required init(unboxer: Unboxer) throws {
         guard let id: String = unboxer.unbox(key: "id") else {
             throw ParsingError.missingRequiredField
         }
         self.id = id
+        
+        subincidentID = unboxer.unbox(key: "subincidentId")
+        missingFromDate = unboxer.unbox(key: "missingFrom", formatter: MissingPerson.dateTransformer)
+        reportedDate = unboxer.unbox(key: "reported", formatter: MissingPerson.dateTransformer)
+        fullName = unboxer.unbox(key: "fullName")
+        
+        foundDate = unboxer.unbox(key: "found", formatter: MissingPerson.dateTransformer)
+        // Note delcaration
+        declarationDate = unboxer.unbox(key: "delcaration", formatter: MissingPerson.dateTransformer)
+        lastKnownLocation = unboxer.unbox(key: "lastKnownLocation")
+        
+        physicalHealthDescription = unboxer.unbox(key: "physicalHealth")
+        mentalHeathDescription = unboxer.unbox(key: "mentalHeathDescription")
+        reportingStation = unboxer.unbox(key: "reportingStation")
+        probableCause = unboxer.unbox(key: "probableCause")
+        
+        phoneNumbers = unboxer.unbox(key: "phoneNumbers")
+        
+        locatingMemberID = unboxer.unbox(key: "locatingMemberId")
+        locatingMemberOrganisationalUnit = unboxer.unbox(key: "locatingMemberOrganisationalUnit")
+        
+        // Note missingPersonReport
+        missingPersonReports = unboxer.unbox(key: "missingPersonReport")
+        probableDestination = unboxer.unbox(key: "probableDestination")
+
     }
     
     public required init?(coder aDecoder: NSCoder) {
