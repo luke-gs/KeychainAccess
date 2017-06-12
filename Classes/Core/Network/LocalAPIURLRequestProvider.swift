8225e0b6851439f8c3142ac44608ac8eeb3a75ca
Herli Halim <herli@gridstone.com.au>
2017-06-13 04:20:15 +1000

    Temp implementation

--- Classes/Core/Network/LocalAPIURLRequestProvider.swift
+++ Classes/Core/Network/LocalAPIURLRequestProvider.swift
@@ -8,7 +8,6 @@
 
 open class LocalAPIURLRequestProvider<T: APIURLRequestProviderConfigurable>: WebAPIURLRequestProvider {
 
-    
     public let baseURL: URL
 
     public typealias Configuration = T
@@ -25,7 +24,10 @@ open class LocalAPIURLRequestProvider<T: APIURLRequestProviderConfigurable>: Web
     /// - Parameter grant: The grant type and required field for it.
     /// - Returns: A URLRequest to request for access token.
     open func accessTokenRequest(for grant: OAuthAuthorizationGrant) -> URLRequest {
-        fatalError()
+        guard let url = localBundle.url(forResource: "AccessToken", withExtension: "json") else {
+            throwError(message: #function)
+        }
+        return URLRequest(url: url)
     }
     
     /// Create a credentials validation using basic authentatication request.
@@ -45,7 +47,10 @@ open class LocalAPIURLRequestProvider<T: APIURLRequestProviderConfigurable>: Web
     ///   - parameters: The search criteria
     /// - Returns: A URLRequest to search the person.
     open func searchPerson(from source: T.Source, with parameters: T.PersonSearchParametersType) -> URLRequest {
-        fatalError()
+        guard let url = localBundle.url(forResource: "PersonSearch", withExtension: "json") else {
+            throwError(message: #function)
+        }
+        return URLRequest(url: url)
     }
     
     /// Create a fetch person details request.
@@ -55,17 +60,10 @@ open class LocalAPIURLRequestProvider<T: APIURLRequestProviderConfigurable>: Web
     ///   - id: The id of the person to be fetched.
     /// - Returns: A URLRequest to fetch the person details.
     open func fetchPersonDetails(from source: T.Source, with id: String) -> URLRequest {
-        fatalError()
-    }
-    
-    /// Create a fetch vehicle details request.
-    ///
-    /// - Parameters:
-    ///   - source: The source to fetch the vehicle details from.
-    ///   - id: The id of the vehicle to be fetched.
-    /// - Returns: A URLRequest to fetch the vehicle details.
-    open func fetchVehicleDetails(from source: T.Source, with id: String) -> URLRequest {
-        fatalError()
+        guard let url = localBundle.url(forResource: "PersonDetail\(id)", withExtension: "json") else {
+            throwError(message: #function)
+        }
+        return URLRequest(url: url)
     }
     
     /// Create a vehicle search request.
@@ -75,7 +73,24 @@ open class LocalAPIURLRequestProvider<T: APIURLRequestProviderConfigurable>: Web
     ///   - parameters: The search criteria.
     /// - Returns: A URLRequest to search the vehicle.
     open func searchVehicle(from source: T.Source, with parameters: T.VehicleSearchParametersType) -> URLRequest {
-        fatalError()
+        
+        guard let url = localBundle.url(forResource: "VehicleSearch", withExtension: "json") else {
+            throwError(message: #function)
+        }
+        return URLRequest(url: url)
+    }
+    
+    /// Create a fetch vehicle details request.
+    ///
+    /// - Parameters:
+    ///   - source: The source to fetch the vehicle details from.
+    ///   - id: The id of the vehicle to be fetched.
+    /// - Returns: A URLRequest to fetch the vehicle details.
+    open func fetchVehicleDetails(from source: T.Source, with id: String) -> URLRequest {
+        guard let url = localBundle.url(forResource: "VehicleDetail\(id)", withExtension: "json") else {
+            throwError(message: #function)
+        }
+        return URLRequest(url: url)
     }
     
     private func throwError(message: String) -> Never {
