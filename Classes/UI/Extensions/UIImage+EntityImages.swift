77be3233662b02e725f5f0a0e134b6be64c1e92d
Rod Brown <rodneyb@gridstone.com.au>
2017-07-21 18:08:52 +1000

    Corrected Comments

--- Classes/UI/Extensions/UIImage+EntityImages.swift
+++ Classes/UI/Extensions/UIImage+EntityImages.swift
@@ -13,10 +13,10 @@ extension UIImage {
     @nonobjc @available(*, deprecated, message: "Use AssetManager.shared.image(forKey: .entityPerson)")
     public static let personOutline = AssetManager.shared.image(forKey: .entityPerson)!
     
-    @nonobjc @available(*, deprecated, message: "Use AssetManager.shared.image(for: .entityCar)")
+    @nonobjc @available(*, deprecated, message: "Use AssetManager.shared.image(forKey: .entityCar)")
     public static let carOutline = AssetManager.shared.image(forKey: .entityCar)!
     
-    @nonobjc @available(*, deprecated, message: "Use AssetManager.image(for: .entityBuilding)")
+    @nonobjc @available(*, deprecated, message: "Use AssetManager.image(forKey: .entityBuilding)")
     public static let buildingOutline = AssetManager.shared.image(forKey: .entityBuilding)!
     
 }
