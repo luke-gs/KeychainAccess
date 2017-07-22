01be74cd0094391bc85aaeb106d877535d6dd635
Rod Brown <rodneyb@gridstone.com.au>
2017-07-22 20:02:19 +1000

    Move to official API

--- MPOL/View Controllers/Events/EventsListViewController.swift
+++ MPOL/View Controllers/Events/EventsListViewController.swift
@@ -11,11 +11,13 @@ import MPOLKit
 
 class EventsListViewController: FormCollectionViewController {
     
+    private static let formIcon = #imageLiteral(resourceName: "iconFormOccurrence")
+    
     override init() {
         super.init()
         title = NSLocalizedString("Involvements", comment: "Title")
         
-        tabBarItem.image = #imageLiteral(resourceName: "iconFormOccurrence")
+        tabBarItem.image = EventsListViewController.formIcon
         tabBarItem.selectedImage = #imageLiteral(resourceName: "iconFormOccurrenceFilled")
         //tabBarItem.isEnabled = false
         
@@ -27,7 +29,7 @@ class EventsListViewController: FormCollectionViewController {
         super.viewDidLoad()
         
         let collectionView = self.collectionView!
-        collectionView.register(EventListCell.self)
+        collectionView.register(CollectionViewFormSubtitleCell.self)
         collectionView.register(CollectionViewFormHeaderView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader)
         collectionView.register(EventListFooterView.self, forSupplementaryViewOfKind: collectionElementKindGlobalFooter)
     }
@@ -47,7 +49,7 @@ class EventsListViewController: FormCollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return 50
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -75,17 +77,25 @@ class EventsListViewController: FormCollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(of: EventListCell.self, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(of: CollectionViewFormSubtitleCell.self, for: indexPath)
         cell.titleLabel.text = "Street Check"
         cell.subtitleLabel.text = "4-12 Langridge St, Collingwood VIC 3066"
-        cell.actionLabel.text = "Open Event"
-        cell.actionSubtitleLabel.text = "Saved at 8:45 AM"
-        cell.imageView.image = #imageLiteral(resourceName: "iconFormOccurrence")
-        cell.accessoryView = cell.accessoryView as? FormDisclosureView ?? FormDisclosureView()
+        cell.imageView.image = EventsListViewController.formIcon
         
         let isCompact = traitCollection.horizontalSizeClass == .compact
-        cell.actionLabel.isHidden = isCompact
-        cell.actionSubtitleLabel.isHidden = isCompact
+        
+        let labeledAccessory: LabeledAccessoryView
+        if let currentAccessory = cell.accessoryView as? LabeledAccessoryView {
+            labeledAccessory = currentAccessory
+        } else {
+            labeledAccessory = LabeledAccessoryView(frame: .zero)
+            labeledAccessory.titleLabel.font = .preferredFont(forTextStyle: .subheadline)
+            labeledAccessory.accessoryView = FormDisclosureView()
+        }
+        labeledAccessory.titleLabel.text = isCompact ? nil : "Open Event"
+        labeledAccessory.subtitleLabel.text = isCompact ? nil : "Saved at 8:45 AM"
+        cell.accessoryView = labeledAccessory
+        cell.highlightStyle = .fade
         
         return cell
     }
@@ -96,9 +106,9 @@ class EventsListViewController: FormCollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         super.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
         
-        if let eventCell = cell as? EventListCell {
-            eventCell.actionLabel.textColor = collectionView.tintColor
-            eventCell.actionSubtitleLabel.textColor = eventCell.subtitleLabel.textColor
+        if let accessory = (cell as? CollectionViewFormCell)?.accessoryView as? LabeledAccessoryView {
+            accessory.titleLabel.textColor = collectionView.tintColor
+            accessory.subtitleLabel.textColor = secondaryTextColor
         }
     }
     
@@ -122,7 +132,7 @@ class EventsListViewController: FormCollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, layout: CollectionViewFormLayout, minimumContentHeightForItemAt indexPath: IndexPath, givenContentWidth itemWidth: CGFloat) -> CGFloat {
-        return EventListCell.minimumContentHeight(withTitle: "Street Check", subtitle: "4-12 Langridge St, Collingwood VIC 3066", inWidth: itemWidth, compatibleWith: traitCollection, image: #imageLiteral(resourceName: "iconFormOccurrence"), accessoryViewSize: FormDisclosureView.standardSize)
+        return CollectionViewFormSubtitleCell.minimumContentHeight(withTitle: "Street Check", subtitle: "4-12 Langridge St, Collingwood VIC 3066", inWidth: itemWidth, compatibleWith: traitCollection, imageSize: EventsListViewController.formIcon.size, accessoryViewSize: FormDisclosureView.standardSize)
     }
     
     
@@ -138,133 +148,6 @@ class EventsListViewController: FormCollectionViewController {
     
 }
 
-
-
-
-
-
-private var kvoContext = 1
-
-// This will refactored to the kit. Standby for a rename and shift.
-//
-// This solution currently doesn't support class sizing variations for multiple lines.
-private class EventListCell: CollectionViewFormSubtitleCell {
-    
-    let actionLabel = UILabel(frame: .zero)
-    
-    let actionSubtitleLabel = UILabel(frame: .zero)
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        highlightStyle = .fade
-        
-        actionLabel.adjustsFontForContentSizeCategory = true
-        actionSubtitleLabel.adjustsFontForContentSizeCategory = true
-        
-        actionLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        actionSubtitleLabel.font = subtitleLabel.font
-        
-        let contentView = self.contentView
-        contentView.addSubview(actionLabel)
-        contentView.addSubview(actionSubtitleLabel)
-        
-        actionLabel.addObserver(self, forKeyPath: #keyPath(UILabel.isHidden), context: &kvoContext)
-        actionSubtitleLabel.addObserver(self, forKeyPath: #keyPath(UILabel.isHidden), context: &kvoContext)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        MPLCodingNotSupported()
-    }
-    
-    deinit {
-        actionLabel.removeObserver(self, forKeyPath: #keyPath(UILabel.isHidden), context: &kvoContext)
-        actionSubtitleLabel.removeObserver(self, forKeyPath: #keyPath(UILabel.isHidden), context: &kvoContext)
-    }
-    
-   
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        let isRightToLeft = effectiveUserInterfaceLayoutDirection == .rightToLeft
-        let displayScale = traitCollection.currentDisplayScale
-        let contentRect = self.contentRect()
-        let accessoryInset = CollectionViewFormCell.accessoryContentInset
-        
-        var actionSize = actionLabel.sizeThatFits(CGSize(width: contentRect.width, height: .greatestFiniteMagnitude))
-        var actionSubtitleSize = actionSubtitleLabel.sizeThatFits(CGSize(width: contentRect.width, height: .greatestFiniteMagnitude))
-        
-        // `UILabel.sizeThatFits(_:)` can ignore the width when the line count is limited. Cap it.
-        actionSize.width = min(actionSize.width, contentRect.width)
-        actionSubtitleSize.width = min(actionSubtitleSize.width, contentRect.width)
-        
-        let actionLabelSeparation = (actionSize.isEmpty || actionLabel.isHidden) || (actionSubtitleSize.isEmpty || actionSubtitleLabel.isHidden) ? 0.0 : labelSeparation.rounded(toScale: displayScale)
-        let actionLabelContentSize = CGSize(width: max(actionSize.width, actionSize.height),
-                                            height: (actionLabel.isHidden ? 0.0 : actionSize.height) + (actionSubtitleLabel.isHidden ? 0.0 : actionSubtitleSize.height) + actionLabelSeparation)
-        
-        var actionFrame = CGRect(origin: CGPoint(x: 0.0, y: (contentRect.midY - (actionLabelContentSize.height / 2.0)).rounded(toScale: displayScale)), size: actionSize)
-        var actionSubtitleFrame = CGRect(origin: CGPoint(x: 0.0, y: actionFrame.maxY + actionLabelSeparation), size: actionSubtitleSize)
-        
-        if isRightToLeft {
-            actionFrame.origin.x = contentRect.minX
-            actionSubtitleFrame.origin.x = contentRect.minX
-        } else {
-            actionFrame.origin.x = contentRect.maxX - actionSize.width
-            actionSubtitleFrame.origin.x = contentRect.maxX - actionSubtitleSize.width
-        }
-        
-        actionLabel.frame = actionFrame
-        actionSubtitleLabel.frame = actionSubtitleFrame
-        
-        if actionLabelContentSize.isEmpty {
-            return
-        }
-        
-        var titleFrame = titleLabel.frame
-        var subtitleFrame = subtitleLabel.frame
-        
-        if isRightToLeft {
-            let endOfActionContent = (contentRect.minX + actionLabelContentSize.width + (accessoryInset * 2.0)).rounded(toScale: displayScale)
-            
-            let titleDifference = endOfActionContent - titleFrame.origin.x
-            if titleDifference >~ 0.0 {
-                titleFrame.size.width -= titleDifference
-                titleFrame.origin.x += titleDifference
-            }
-            let subtitleDifference = endOfActionContent - subtitleFrame.origin.x
-            if subtitleDifference >~ 0.0 {
-                subtitleFrame.size.width -= subtitleDifference
-                subtitleFrame.origin.x += subtitleDifference
-            }
-        } else {
-            let endOfActionContent = (contentRect.maxX - actionLabelContentSize.width - (accessoryInset * 2.0)).rounded(toScale: displayScale)
-            
-            let titleDifference = titleFrame.maxX - endOfActionContent
-            if titleDifference >~ 0 {
-                titleFrame.size.width -= titleDifference
-            }
-            
-            let subtitleDifference = subtitleFrame.maxX - endOfActionContent
-            if subtitleDifference > 0 {
-                subtitleFrame.size.width -= subtitleDifference
-            }
-        }
-        
-        titleLabel.frame = titleFrame
-        subtitleLabel.frame = subtitleFrame
-    }
-    
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if context == &kvoContext {
-            setNeedsLayout()
-        } else {
-            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-        }
-    }
-    
-}
 
 private class EventListFooterView: UICollectionReusableView, DefaultReusable {
     
--- MPOL/View Controllers/SettingsViewController.swift
+++ MPOL/View Controllers/SettingsViewController.swift
@@ -27,10 +27,6 @@ class SettingsViewController: FormTableViewController {
         
         super.init(style: .grouped)
         title = NSLocalizedString("Settings", comment: "SettingsTitle")
-<<<<<<< HEAD
-        
-=======
->>>>>>> origin/master
         wantsCalculatedContentHeight = true
     }
     
