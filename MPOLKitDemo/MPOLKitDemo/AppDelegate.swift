1aa8c9020f267bb07b9cde876c7955b9ea13aecb
Rod Brown <rodneyb@gridstone.com.au>
2017-08-07 16:30:16 +1000

    Undo demo commenting

--- MPOLKitDemo/MPOLKitDemo/AppDelegate.swift
+++ MPOLKitDemo/MPOLKitDemo/AppDelegate.swift
@@ -43,48 +43,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.tintColor = theme.color(forKey: .tint)
         self.window = window
         
-//        let pushableSplitViewController = PushableSplitViewController(viewControllers: [UINavigationController(rootViewController: CollectionDemoListViewController(style: .grouped)), UINavigationController()])
-//        pushableSplitViewController.embeddedSplitViewController.maximumPrimaryColumnWidth = 320.0
-//        pushableSplitViewController.title = "Collections"
-//        let pushableSVNavController = UINavigationController(rootViewController: pushableSplitViewController)
-//
-//        let sidebarDetail1VC = UIViewController()
-//        sidebarDetail1VC.title = "Sidebars"
-//        sidebarDetail1VC.sidebarItem.image = #imageLiteral(resourceName: "SidebarInfo")
-//        sidebarDetail1VC.sidebarItem.selectedImage = #imageLiteral(resourceName: "SidebarInfoFilled")
-//
-//        let sidebarDetail2VC = CollectionDemoListViewController(style: .plain)
-//        sidebarDetail2VC.title = "Sidebar Test 2"
-//        sidebarDetail2VC.sidebarItem.image = #imageLiteral(resourceName: "SidebarAlert")
-//        sidebarDetail2VC.sidebarItem.selectedImage = #imageLiteral(resourceName: "SidebarAlertFilled")
-//
-//        let item1 = SourceItem(title: "CRIMTRAC", state: .loaded(count: 8, color: .red))
-//        let item2 = SourceItem(title: "DS2", state: .loaded(count: 2, color: .blue))
-//        let item3 = SourceItem(title: "DS3", state: .loaded(count: 1, color: .yellow))
-//
-//        let sidebarSplitViewController = SidebarSplitViewController(detailViewControllers: [sidebarDetail1VC, sidebarDetail2VC])
-//        sidebarSplitViewController.sidebarViewController.sourceItems = [item1, item2, item3]
-//        sidebarSplitViewController.sidebarViewController.selectedSourceIndex = 1
-//        sidebarSplitViewController.title = "Sidebars"
-//
-        let layout = MapCollectionViewSidebarLayout()
-        let mapVC = MapCollectionViewController(layout: layout)
+        let pushableSplitViewController = PushableSplitViewController(viewControllers: [UINavigationController(rootViewController: CollectionDemoListViewController(style: .grouped)), UINavigationController()])
+        pushableSplitViewController.embeddedSplitViewController.maximumPrimaryColumnWidth = 320.0
+        pushableSplitViewController.title = "Collections"
+        let pushableSVNavController = UINavigationController(rootViewController: pushableSplitViewController)
+        
+        let sidebarDetail1VC = UIViewController()
+        sidebarDetail1VC.title = "Sidebars"
+        sidebarDetail1VC.sidebarItem.image = #imageLiteral(resourceName: "SidebarInfo")
+        sidebarDetail1VC.sidebarItem.selectedImage = #imageLiteral(resourceName: "SidebarInfoFilled")
+        
+        let sidebarDetail2VC = CollectionDemoListViewController(style: .plain)
+        sidebarDetail2VC.title = "Sidebar Test 2"
+        sidebarDetail2VC.sidebarItem.image = #imageLiteral(resourceName: "SidebarAlert")
+        sidebarDetail2VC.sidebarItem.selectedImage = #imageLiteral(resourceName: "SidebarAlertFilled")
+        
+        let item1 = SourceItem(title: "CRIMTRAC", state: .loaded(count: 8, color: .red))
+        let item2 = SourceItem(title: "DS2", state: .loaded(count: 2, color: .blue))
+        let item3 = SourceItem(title: "DS3", state: .loaded(count: 1, color: .yellow))
+        
+        let sidebarSplitViewController = SidebarSplitViewController(detailViewControllers: [sidebarDetail1VC, sidebarDetail2VC])
+        sidebarSplitViewController.sidebarViewController.sourceItems = [item1, item2, item3]
+        sidebarSplitViewController.sidebarViewController.selectedSourceIndex = 1
+        sidebarSplitViewController.title = "Sidebars"
         
         let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [UINavigationController(rootViewController: mapVC)]
+        tabBarController.viewControllers = [pushableSVNavController, UINavigationController(rootViewController: sidebarSplitViewController)]
         self.window?.rootViewController = tabBarController
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-            layout.setHidesMapInRegularEnvironment(true, animated: true)
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
-            layout.setHidesMapInRegularEnvironment(false, animated: true)
-        }
-        
-        
         window.makeKeyAndVisible()
-        
         
         return true
     }
