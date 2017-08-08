e1355b8bed915531f27294cefc744046111b118a
Herli Halim <herli@gridstone.com.au>
2017-08-08 11:25:26 +1000

    Fix access control

--- Classes/Core/Network/APIManagerError.swift
+++ Classes/Core/Network/APIManagerError.swift
@@ -9,6 +9,6 @@ import Foundation
 import Alamofire
 
 public struct APIManagerError: Error {
-    let underlyingError: Error
-    let response: DefaultDataResponse
+    public let underlyingError: Error
+    public let response: DefaultDataResponse
 }
