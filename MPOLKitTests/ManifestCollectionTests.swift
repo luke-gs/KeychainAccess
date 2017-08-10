bebca98910b1a20b250fc949db03588ff9fd6bc3
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2017-08-10 12:27:00 +1000

    ManifestCollection

---
+++ MPOLKitTests/ManifestCollectionTests.swift
@@ -0,0 +1,50 @@
+//
+//  ManifestCollectionTests.swift
+//  MPOLKit
+//
+//  Created by Pavel Boryseiko on 10/8/17.
+//  Copyright © 2017 Gridstone. All rights reserved.
+//
+
+import XCTest
+@testable import MPOLKit
+
+class ManifestCollectionTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testEquality() {
+        let collection = ManifestCollection(rawValue: "test")
+        let testCollection = ManifestCollection(rawValue: "test")
+
+        XCTAssertEqual(collection, testCollection)
+    }
+
+    func testInequality() {
+        let collection = ManifestCollection.States
+        let testCollection = ManifestCollection.Genders
+
+        XCTAssertNotEqual(collection, testCollection)
+    }
+
+    func testHashEquality() {
+        let collection = ManifestCollection(rawValue: "test").hashValue
+        let testCollection = ManifestCollection(rawValue: "test").hashValue
+
+        XCTAssertEqual(collection, testCollection)
+    }
+
+    func testHashInequality() {
+        let collection = ManifestCollection.Genders.hashValue
+        let testCollection = ManifestCollection.States.hashValue
+
+        XCTAssertNotEqual(collection, testCollection)
+    }
+}
+
