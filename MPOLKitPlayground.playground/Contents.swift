633d7385ec0c3fb71ed52d2ad9da272878e2df5a
Herli <herli@gridstone.com.au>
2017-08-17 08:03:16 +1000

    [Added] - MPOLKit Playground
    Everyone needs a place to play. Having to setup a place everytime? Not so great.

---
+++ MPOLKitPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+//: MPOLKitPlayground - noun: a place where people can play????
+/// This is intended to be a place to quickly test code written for MPOLKit.
+/// Please don't commit changes in here unless it's to improve this playground.
+
+import UIKit
+import MPOLKit
