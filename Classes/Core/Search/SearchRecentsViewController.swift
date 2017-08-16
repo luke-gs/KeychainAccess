5ca3a66e87ce604870bfd962cb78b03aa4d0aa0d
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2017-08-16 17:53:20 +1000

    Use min value instead

--- Classes/Core/Search/SearchRecentsViewController.swift
+++ Classes/Core/Search/SearchRecentsViewController.swift
@@ -167,7 +167,7 @@ class SearchRecentsViewController: FormCollectionViewController {
             isRecentSearches = collectionView == self.collectionView
         }
         
-        return isRecentSearches ? recentlySearched.count : (recentlyViewed.count <= 6  ? recentlyViewed.count : 6)
+        return isRecentSearches ? recentlySearched.count : min(recentlyViewed.count, 6)
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
