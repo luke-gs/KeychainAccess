d7e79bb74ac23aa0cecfe49af5e68bc8d091b1f5
Rui Wang <rui.wang@gridstone.com.au>
2017-08-16 17:35:06 +1000

    [Entity Event] - Fix pre DIRT from last commit

--- ClientKit/ClientKit/View Controllers/Entity Details/Detail View Controllers/Details View Models/Vehicle/VehicleOccurrencesViewModel.swift
+++ ClientKit/ClientKit/View Controllers/Entity Details/Detail View Controllers/Details View Models/Vehicle/VehicleOccurrencesViewModel.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017 Gridstone. All rights reserved.
 //
 
+
 import Foundation
+import MPOLKit
+
+public class VehicleOccurrencesViewModel: EntityEventViewModel {
+    
+    // MARK: - Public methods
+    
+    public func reloadSections(with filterTypes: Set<String>?, filterDateRange: FilterDateRange?, sortedBy sorting: DateSorting) {
+        
+        var events = entity?.events ?? []
+        
+        let requiresFiltering = filterTypes != nil || filterDateRange != nil
+        if requiresFiltering {
+            events = events.filter { event in
+                if let filterTypes = filterTypes {
+                    guard let type = event.eventType, filterTypes.contains(type) else {
+                        return false
+                    }
+                }
+                if let dateFilter = filterDateRange {
+                    guard let date = event.occurredDate, dateFilter.contains(date) else {
+                        return false
+                    }
+                }
+                return true
+            }
+        }
+        
+        let dateSorting = sorting.compare(_:_:)
+        events.sort { dateSorting(($0.occurredDate ?? .distantPast), ($1.occurredDate ?? .distantPast)) }
+        
+        sections = events
+        delegate?.updateFilterBarButtonItemActivity()
+    }
+}
