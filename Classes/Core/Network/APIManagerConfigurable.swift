73969dcbd6ddc209acffceb741d879158dea399f
Herli <herli@gridstone.com.au>
2017-08-18 16:55:56 +1000

    Added more properties on configuration

--- Classes/Core/Network/APIManagerConfigurable.swift
+++ Classes/Core/Network/APIManagerConfigurable.swift
@@ -13,13 +13,34 @@ import Unbox
 public protocol APIManagerConfigurable {
     associatedtype Source: EntitySource
     var url: URLConvertible { get }
+
+    var urlSessionConfiguration: URLSessionConfiguration { get }
+    var errorMapper: ErrorMapper? { get }
+    var trustPolicyManager: ServerTrustPolicyManager? { get }
 }
 
 public struct APIManagerDefaultConfiguration<S: EntitySource>: APIManagerConfigurable {
     public typealias Source = S
     public let url: URLConvertible
+    public let urlSessionConfiguration: URLSessionConfiguration
+    public let errorMapper: ErrorMapper?
+    public let trustPolicyManager: ServerTrustPolicyManager?
+
+    static func defaultConfiguration() -> URLSessionConfiguration {
+        let config = URLSessionConfiguration.default
+        config.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        return config
+    }
+
+    static func defaultErrorMapper() -> ErrorMapper {
+        let mapper = ErrorMapper(definitions: [NetworkErrorDefinition()])
+        return mapper
+    }
     
-    public init(url: URLConvertible) {
+    public init(url: URLConvertible, urlSessionConfiguration: URLSessionConfiguration = APIManagerDefaultConfiguration.defaultConfiguration(), errorMapper: ErrorMapper? = APIManagerDefaultConfiguration.defaultErrorMapper(), trustPolicyManager: ServerTrustPolicyManager? = nil) {
         self.url = url
+        self.urlSessionConfiguration = urlSessionConfiguration
+        self.errorMapper = errorMapper
+        self.trustPolicyManager = trustPolicyManager
     }
 }
