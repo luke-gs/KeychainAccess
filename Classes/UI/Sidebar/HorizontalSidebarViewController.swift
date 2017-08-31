a9f5e9a80893829a640875a06d2da783a08fddcd
Trent Fitzgibbon <trentf@gridstone.com.au>
2017-08-31 17:00:11 +1000

    Centered navigation

--- Classes/UI/Sidebar/HorizontalSidebarViewController.swift
+++ Classes/UI/Sidebar/HorizontalSidebarViewController.swift
@@ -41,19 +41,22 @@ open class HorizontalSidebarViewController: UIViewController {
                 sidebarKeys.forEach { item.addObserver(self, forKeyPath: $0, context: &sidebarItemContext) }
             }
 
+            if let selectedItem = self.selectedItem, items.contains(selectedItem) == false {
+                self.selectedItem = nil
+            }
+
             // Add each sidebar item as a cell in the stack view
             items.forEach({ (item) in
                 let label = HorizontalSidebarCell(frame: .zero)
-                label.text = item.title
-                label.textColor = item.color
                 label.setContentHuggingPriority(UILayoutPriorityRequired, for: .horizontal)
                 label.setContentCompressionResistancePriority(UILayoutPriorityRequired, for: .horizontal)
                 sidebarStackView.addArrangedSubview(label)
             })
+            updateCells()
 
-            if let selectedItem = self.selectedItem, items.contains(selectedItem) == false {
-                self.selectedItem = nil
-            }
+            // Force layout so that we can get the cell size in viewDidLayoutSubviews
+            sidebarStackView.setNeedsLayout()
+            sidebarStackView.layoutIfNeeded()
         }
     }
 
@@ -62,7 +65,9 @@ open class HorizontalSidebarViewController: UIViewController {
     /// If `clearsSelectionOnViewWillAppear` is true, this property is set to nil
     /// when it receives a viewWillAppear(_:) message.
     public var selectedItem: SidebarItem? {
-        didSet { updateSelection() }
+        didSet {
+            updateCells()
+        }
     }
 
     /// The stack view for sidebar items.
@@ -70,6 +75,9 @@ open class HorizontalSidebarViewController: UIViewController {
 
     /// The scroll view for containing the stack view.
     public private(set) var scrollView: UIScrollView!
+
+    /// The leading constraint for the stack view, to center first item
+    private var stackViewLeadingConstraint: NSLayoutConstraint!
 
     /// A Boolean value indicating whether the sidebar clears the selection when the view appears.
     ///
@@ -102,6 +110,7 @@ open class HorizontalSidebarViewController: UIViewController {
         view.backgroundColor = #colorLiteral(red: 0.1058823529, green: 0.1176470588, blue: 0.1411764706, alpha: 1)
 
         scrollView = UIScrollView(frame: .zero)
+        scrollView.alwaysBounceHorizontal = true
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.showsVerticalScrollIndicator = false
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -109,25 +118,36 @@ open class HorizontalSidebarViewController: UIViewController {
 
         sidebarStackView = UIStackView(frame: .zero)
         sidebarStackView.axis = .horizontal
-        sidebarStackView.spacing = 10.0
+        sidebarStackView.spacing = 24.0
         sidebarStackView.distribution = .equalSpacing
         sidebarStackView.alignment = .center
         sidebarStackView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.addSubview(sidebarStackView)
 
+        stackViewLeadingConstraint = sidebarStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0)
+        stackViewLeadingConstraint.isActive = true
+
+        let inset = 10 as CGFloat
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
-            scrollView.heightAnchor.constraint(equalToConstant: 56),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: inset),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: inset),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -inset),
+            scrollView.heightAnchor.constraint(equalToConstant: 36),
 
             sidebarStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            sidebarStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
             sidebarStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
             sidebarStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             sidebarStackView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            stackViewLeadingConstraint
         ])
+    }
+
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if let firstCell = self.cells.first, firstCell.bounds.width > 0 {
+            self.stackViewLeadingConstraint.constant = (self.view.bounds.width - firstCell.bounds.width - 20) / 2
+        }
     }
 
     open override func viewWillAppear(_ animated: Bool) {
@@ -164,10 +184,10 @@ open class HorizontalSidebarViewController: UIViewController {
             if key == #keyPath(SidebarItem.isEnabled) && item.isEnabled == false && selectedItem == item {
                 selectedItem = nil
             }
-
-//            if let sidebarCell = sidebarTableView?.cellForRow(at: IndexPath(row: itemIndex, section: 0)) as? SidebarTableViewCell {
-//                sidebarCell.update(for: item)
-//            }
+            // Animate the change to the cell
+            UIView.transition(with: view, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                self.updateCellAtIndex(itemIndex)
+            }, completion: nil)
         } else {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
         }
@@ -179,21 +199,27 @@ open class HorizontalSidebarViewController: UIViewController {
 
     // MARK: - Private methods
 
-    private func updateSelection() {
-        let unselectedColor      = #colorLiteral(red: 0.5450980392, green: 0.568627451, blue: 0.6235294118, alpha: 1)
-        let badgeBackgroundColor = #colorLiteral(red: 0.1647058824, green: 0.1803921569, blue: 0.2117647059, alpha: 1)
+    private func updateCells() {
+        for index in 0..<items.count {
+            updateCellAtIndex(index)
+        }
+    }
+
+    private func updateCellAtIndex(_ index: Int) {
         guard isViewLoaded, let sidebarStackView = sidebarStackView else { return }
 
-        for index in 0..<items.count {
-            let item = items[index]
-            let cell = cells[index]
-            let selected = item == self.selectedItem
-            cell.textColor = selected ? .white : unselectedColor
+        let item = items[index]
+        let cell = cells[index]
+        let selected = item == self.selectedItem
+        if let title = item.title {
+            cell.text = item.count > 0 ? "\(item.count) \(title)" : title
+        }
+        cell.textColor = selected ? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) : #colorLiteral(red: 0.5450980392, green: 0.568627451, blue: 0.6235294118, alpha: 1)
+        cell.font = selected ? UIFont.systemFont(ofSize: 16) : UIFont.systemFont(ofSize: 14)
 
-            if selected {
-                let rect = cell.convert(cell.frame, to: sidebarStackView)
-                scrollView.scrollRectToVisible(rect, animated: true)
-            }
+        if selected {
+            let rect = cell.convert(cell.frame, to: sidebarStackView)
+            scrollView.scrollRectToVisible(rect, animated: true)
         }
     }
 
