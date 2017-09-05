44b7b896f2be71508e5b23792e2f6c9c435293b6
Luke Sammut <lukes@gridstone.com.au>
2017-09-05 16:29:26 +1000

    Add the networkLoggingPlugin to mpolApp

--- MPOL/AppDelegate.swift
+++ MPOL/AppDelegate.swift
@@ -30,7 +30,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         MPOLKitInitialize()
 
-        APIManager.shared = APIManager(configuration: APIManagerDefaultConfiguration(url: "https://\(host)", trustPolicyManager: ServerTrustPolicyManager(policies: [host: .disableEvaluation])))
+        let plugins: [PluginType] = [
+            NetworkLoggingPlugin()
+        ]
+
+        APIManager.shared = APIManager(configuration: APIManagerDefaultConfiguration(url: "https://\(host)", plugins: plugins, trustPolicyManager: ServerTrustPolicyManager(policies: [host: .disableEvaluation])))
 
         NotificationCenter.default.addObserver(self, selector: #selector(interfaceStyleDidChange), name: .interfaceStyleDidChange, object: nil)
 
