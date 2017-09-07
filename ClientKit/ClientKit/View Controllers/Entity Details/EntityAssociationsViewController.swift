c5ce6296526dd608a1235f40a8d0a478d5eac35f
Luke Sammut <lukes@gridstone.com.au>
2017-09-07 17:36:27 +1000

    Add list state item back into nav bars

--- ClientKit/ClientKit/View Controllers/Entity Details/EntityAssociationsViewController.swift
+++ ClientKit/ClientKit/View Controllers/Entity Details/EntityAssociationsViewController.swift
@@ -57,7 +57,7 @@ open class EntityAssociationsViewController: EntityDetailCollectionViewControlle
         
         let filterBarItem = FilterBarButtonItem(target: nil, action: nil)
         filterBarItem.isEnabled = false
-        navigationItem.rightBarButtonItems = [filterBarItem]
+        navigationItem.rightBarButtonItems = [filterBarItem, listStateItem]
     }
     
     public required init?(coder aDecoder: NSCoder) {
