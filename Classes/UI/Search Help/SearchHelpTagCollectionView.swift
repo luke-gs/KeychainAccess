89319f259138ca02111d58251e88b79b9b1bae48
Megan Efron <megan.efron@gridstone.com.au>
2017-09-11 14:55:26 +1000

    MPLCodingNotSupported

--- Classes/UI/Search Help/SearchHelpTagCollectionView.swift
+++ Classes/UI/Search Help/SearchHelpTagCollectionView.swift
@@ -38,7 +38,7 @@ public class SearchHelpTagCollectionView: UICollectionView, UICollectionViewData
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        MPLCodingNotSupported()
     }
     
     
--- Classes/UI/Search Help/SearchHelpViewController.swift
+++ Classes/UI/Search Help/SearchHelpViewController.swift
@@ -60,7 +60,7 @@ open class SearchHelpViewController: UIViewController {
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        MPLCodingNotSupported()
     }
 
     open override func viewDidLoad() {
