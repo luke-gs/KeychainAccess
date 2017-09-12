d8ffecca9b8f776891810ce7eb32ab76ef34d1f7
James Aramroongrot <jamesa@gridstone.com.au>
2017-09-12 17:07:45 +1000

    Removed dispatch

--- Classes/Subspec/ThemeGesture/UINavigationController+Theme.m
+++ Classes/Subspec/ThemeGesture/UINavigationController+Theme.m
@@ -80,10 +80,8 @@
 }
 
 - (void)changeTheme:(id)recognizer {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        ThemeManager* manager = [ThemeManager shared];
-        [manager setCurrentInterfaceStyle: manager.currentInterfaceStyle == UserInterfaceStyleDark ? UserInterfaceStyleLight : UserInterfaceStyleDark];
-    });
+    ThemeManager* manager = [ThemeManager shared];
+    [manager setCurrentInterfaceStyle: manager.currentInterfaceStyle == UserInterfaceStyleDark ? UserInterfaceStyleLight : UserInterfaceStyleDark];
 }
 
 
