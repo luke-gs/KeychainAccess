0dca604fa0f96f454a2bcb7be9c65b175d84cc2b
Herli <herli@gridstone.com.au>
2017-09-13 14:51:26 +1000

    Make the default values public.

--- Classes/Core/Network/APIManagerConfigurable.swift
+++ Classes/Core/Network/APIManagerConfigurable.swift
@@ -29,17 +29,6 @@ public struct APIManagerDefaultConfiguration: APIManagerConfigurable {
     public let errorMapper: ErrorMapper?
     public let trustPolicyManager: ServerTrustPolicyManager?
 
-    static func defaultConfiguration() -> URLSessionConfiguration {
-        let config = URLSessionConfiguration.default
-        config.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
-        return config
-    }
-
-    static func defaultErrorMapper() -> ErrorMapper {
-        let mapper = ErrorMapper(definitions: [NetworkErrorDefinition()])
-        return mapper
-    }
-
     public init(url: URLConvertible,
                 urlSessionConfiguration: URLSessionConfiguration = APIManagerDefaultConfiguration.defaultConfiguration(),
                 plugins: [PluginType]? = nil,
@@ -52,4 +41,20 @@ public struct APIManagerDefaultConfiguration: APIManagerConfigurable {
         self.errorMapper = errorMapper
         self.trustPolicyManager = trustPolicyManager
     }
+}
+
+// The default values static methods.
+extension APIManagerDefaultConfiguration {
+
+    public static func defaultConfiguration() -> URLSessionConfiguration {
+        let config = URLSessionConfiguration.default
+        config.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        return config
+    }
+
+    public static func defaultErrorMapper() -> ErrorMapper {
+        let mapper = ErrorMapper(definitions: [NetworkErrorDefinition()])
+        return mapper
+    }
+    
 }
