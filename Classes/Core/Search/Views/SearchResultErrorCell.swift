b288932573ead1d821d74d9e8c641f31a3a5f999
Herli <herli@gridstone.com.au>
2017-09-13 16:24:31 +1000

    Use `MPLCodingNotSupported`

--- Classes/Core/Search/Views/SearchResultErrorCell.swift
+++ Classes/Core/Search/Views/SearchResultErrorCell.swift
@@ -60,7 +60,7 @@ public class SearchResultErrorCell: UICollectionViewCell, DefaultReusable {
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        MPLCodingNotSupported()
     }
     
     public func apply(theme: Theme) {
