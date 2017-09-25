f7cb1331288bb99d9443f9bcce4e768ac9043c76
Herli <herli@gridstone.com.au>
2017-09-25 15:53:03 +1000

    Added comment

--- Classes/Core/Network/PluginType.swift
+++ Classes/Core/Network/PluginType.swift
@@ -22,6 +22,8 @@ public protocol PluginType {
     // Called after a response has been received, but before the completion callback is triggered.
     func didReceiveResponse(_ response: Alamofire.DataResponse<Data>)
 
+    // Called after `didReceiveResponse:` but before the completion callback is triggered.
+    // This will give the chance for plugin to modify the response if necessary before the completion.
     func processResponse(_ response: Alamofire.DataResponse<Data>) -> Alamofire.DataResponse<Data>
 
 }
