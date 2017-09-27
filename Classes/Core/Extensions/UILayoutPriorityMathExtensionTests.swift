a86a1ba08abd89594cb2a00a7ca41dd3b8d9d1a2
Herli <herli@gridstone.com.au>
2017-09-28 02:40:22 +1000

    Added constants

--- Classes/Core/Extensions/UILayoutPriorityMathExtensionTests.swift
+++ Classes/Core/Extensions/UILayoutPriorityMathExtensionTests.swift
@@ -10,6 +10,14 @@ import UIKit
 
 extension UILayoutPriority {
 
+
+    // MARK: - Static convenience constants
+
+    /// Convenience declaration for UILayoutPriority(rawValue: UILayoutPriority.required.rawValue - 1)
+    public static let almostRequired: UILayoutPriority = UILayoutPriority.required - 1
+
+    // MARK: - Operators overloading
+
     public static func +(lhs: UILayoutPriority, rhs: Float) -> UILayoutPriority {
         let rawValue = lhs.rawValue + rhs
         return UILayoutPriority(rawValue: rawValue)
