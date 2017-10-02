49d9e89a5ddcf340802fb2c3e3f9af1652f2eaf1
Trent Fitzgibbon <trentf@gridstone.com.au>
2017-10-02 11:45:40 +1100

    Added support for dot image with stroke

--- ClientKit/ClientKit/View Controllers/CAD/ActivityLog/ActivityLogItemViewModel.swift
+++ ClientKit/ClientKit/View Controllers/CAD/ActivityLog/ActivityLogItemViewModel.swift
@@ -10,12 +10,13 @@ import Foundation
 
 /// View model for a single Activity Log item
 public struct ActivityLogItemViewModel {
-    public let dotColor: UIColor
+    public let dotFillColor: UIColor
+    public let dotStrokeColor: UIColor
     public let timestamp: String
     public let title: String
     public let subtitle: String
 
     public func dotImage() -> UIImage {
-        return UIImage.statusDot(withColor: dotColor)
+        return UIImage.statusDot(withColor: dotFillColor, strokeColor: dotStrokeColor)
     }
 }
