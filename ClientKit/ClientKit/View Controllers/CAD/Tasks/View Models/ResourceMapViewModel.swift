2edb41b9af0d8123d33f2ccf44152a3cf2c8839a
Kyle May <kyle.may@gridstone.com.au>
2017-10-02 15:27:06 +1100

    Better location description

--- ClientKit/ClientKit/View Controllers/CAD/Tasks/View Models/ResourceMapViewModel.swift
+++ ClientKit/ClientKit/View Controllers/CAD/Tasks/View Models/ResourceMapViewModel.swift
@@ -17,15 +17,5 @@ public struct ResourceMapViewModel {
     public var iconImage: UIImage?
     public var iconColor: UIColor
     public var pulsing: Bool
-    
-//    var color: UIColor {
-//        switch self {
-//        case .unassigned:
-//            return UIColor(red: 76.0 / 255.0, green: 175.0 / 255.0, blue: 80.0 / 255.0, alpha: 1.0)
-//        case .assigned, .tasked:
-//            return #colorLiteral(red: 0.8431372549, green: 0.8431372549, blue: 0.8509803922, alpha: 1)
-//        case .duress:
-//            return UIColor(red: 255.0 / 255.0, green: 59.0 / 255.0, blue: 48.0 / 255.0, alpha: 1.0)
-//        }
-//    }
 }
+
