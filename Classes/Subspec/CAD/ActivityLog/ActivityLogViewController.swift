96d8acdefcc9896143d14c53f52c103957b22ead
Trent Fitzgibbon <trentf@gridstone.com.au>
2017-10-10 13:40:58 +1100

    Converted activity log to use new base class

--- Classes/Subspec/CAD/ActivityLog/ActivityLogViewController.swift
+++ Classes/Subspec/CAD/ActivityLog/ActivityLogViewController.swift
@@ -8,74 +8,30 @@
 
 import UIKit
 
-/// Activity Log view controller
-public class ActivityLogViewController: TimelineFormCollectionViewController {
+/// View controller for displaying the activity log in the CAD tab bar controller
+///
+/// This uses CADFormCollectionViewController for consistent styling and reduced boilerplate.
+///
+public class ActivityLogViewController: TimelineFormCollectionViewController<ActivityLogItemViewModel> {
 
-    private lazy var viewModel: ActivityLogViewModel = {
-        let vm = ActivityLogViewModel()
-        // vm.delegate = self
-        return vm
-    }()
+    // MARK: - Override
 
-    // MARK: - Initializers
-
-    public override init() {
-        super.init()
+    override open func cellType() -> CollectionViewFormCell.Type {
+        return ActivityLogItemCell.self
     }
 
-    public required init?(coder aDecoder: NSCoder) {
-        MPLCodingNotSupported()
-    }
+    override open func decorate(cell: CollectionViewFormCell, with viewModel: ActivityLogItemViewModel) {
+        cell.highlightStyle = .fade
+        cell.selectionStyle = .fade
+        cell.separatorStyle = .none
+        cell.accessoryView = FormAccessoryView(style: .disclosure)
 
-    // MARK: - View lifecycle
-
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-
-        title = viewModel.navTitle()
-        loadingManager.noContentView.titleLabel.text = viewModel.noContentTitle()
-        loadingManager.noContentView.subtitleLabel.text = viewModel.noContentSubtitle()
-
-        guard let collectionView = self.collectionView else { return }
-
-        collectionView.register(CollectionViewFormHeaderView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader)
-        collectionView.register(ActivityLogItemCell.self)
-    }
-
-    // MARK: - UICollectionViewDataSource
-
-    public func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return viewModel.numberOfSections()
-    }
-
-    open override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.numberOfItems(for: section)
-    }
-
-    open override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(of: ActivityLogItemCell.self, for: indexPath)
-        if let item = viewModel.item(at: indexPath) {
-            cell.decorate(with: item)
+        if let cell = cell as? ActivityLogItemCell {
+            cell.timeLabel.text = viewModel.timestamp
+            cell.titleLabel.text = viewModel.title
+            cell.subtitleLabel.text = viewModel.subtitle
+            cell.imageView.image = viewModel.dotImage()
         }
-        return cell
-    }
-
-    open override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if kind == UICollectionElementKindSectionHeader {
-            // Create collapsible section header
-            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, class: CollectionViewFormHeaderView.self, for: indexPath)
-            header.text = viewModel.headerText(at: indexPath.section)
-            header.showsExpandArrow = true
-            header.tapHandler = { [weak self] headerView, indexPath in
-                guard let `self` = self else { return }
-                self.viewModel.toggleHeaderExpanded(at: indexPath.section)
-                self.collectionView?.reloadSections(IndexSet(integer: indexPath.section))
-                headerView.setExpanded(self.viewModel.isHeaderExpanded(at: indexPath.section), animated: true)
-            }
-            header.isExpanded = viewModel.isHeaderExpanded(at: indexPath.section)
-            return header
-        }
-        return super.collectionView(collectionView, viewForSupplementaryElementOfKind: kind, at: indexPath)
     }
 
     // MARK: - UICollectionViewDelegate
@@ -83,10 +39,6 @@ public class ActivityLogViewController: TimelineFormCollectionViewController {
     open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
         // TODO: present details?
-    }
-
-    open func collectionView(_ collectionView: UICollectionView, layout: CollectionViewFormLayout, heightForHeaderInSection section: Int) -> CGFloat {
-        return CollectionViewFormHeaderView.minimumHeight
     }
 
     open override func collectionView(_ collectionView: UICollectionView, layout: CollectionViewFormLayout, minimumContentHeightForItemAt indexPath: IndexPath, givenContentWidth itemWidth: CGFloat) -> CGFloat {
@@ -97,17 +49,3 @@ public class ActivityLogViewController: TimelineFormCollectionViewController {
     }
 }
 
-/// Extension of form detail cell that supports decorating using our view model
-extension ActivityLogItemCell {
-    public func decorate(with viewModel: ActivityLogItemViewModel) {
-        highlightStyle = .fade
-        selectionStyle = .fade
-        separatorStyle = .none
-        accessoryView = FormAccessoryView(style: .disclosure)
-
-        timeLabel.text = viewModel.timestamp
-        titleLabel.text = viewModel.title
-        subtitleLabel.text = viewModel.subtitle
-        imageView.image = viewModel.dotImage()
-    }
-}
