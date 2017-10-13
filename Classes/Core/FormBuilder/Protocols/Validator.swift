e4ec15398a2069b49cf8a7acb9a4994a7415e3bb
James Aramroongrot <jamesa@gridstone.com.au>
2017-10-13 16:11:05 +1100

    Add documentations

--- Classes/Core/FormBuilder/Protocols/Validator.swift
+++ Classes/Core/FormBuilder/Protocols/Validator.swift
@@ -34,10 +34,35 @@ public enum ValidateResult: Equatable {
     case valid
     case softInvalid(message: String)
     case strictInvalid(message: String)
+
+    /// Returns true if the result is valid.
+    ///
+    /// - Returns: True if valid.
+    public func isValid() -> Bool {
+        switch self {
+        case .valid:
+            return true
+        case .softInvalid, .strictInvalid:
+            return false
+        }
+    }
+
+    /// Returns a message if the result is invalid.
+    ///
+    /// - Returns: String if invalid.
+    public func message() -> String? {
+        switch self {
+        case .valid:
+            return nil
+        case .softInvalid(let message), .strictInvalid(let message):
+            return message
+        }
+    }
+
 }
 
 
-/// Comparison of two validation rules. Rules are considered to be equal if the case
+/// Comparison of two validation results. Results are considered to be equal if the case
 /// and the meesage are the same.
 ///
 /// - Parameters:
@@ -63,24 +88,59 @@ public func ==(lhs: ValidateResult, rhs: ValidateResult) -> Bool {
 /// - soft: This is used for live validation of text without preventing text input.
 /// - strict: This will prevent the text input if it doesn't pass the strict rule.
 /// - submit: Validations on submission or on end editing.
-public enum ValidatorRule {
+public enum ValidatorRule: Equatable {
     case soft(specification: Specification, message: String)
     case strict(specification: Specification, message: String)
     case submit(specification: Specification, message: String)
 }
 
 
+/// Comparion of two rules. Rules are considered the same if the case, spec and message
+/// are equal.
+///
+/// - Parameters:
+///   - lhs: ValidatorRule
+///   - rhs: ValidatorRule
+/// - Returns: True if the same. False otherwise.
+public func ==(lhs: ValidatorRule, rhs: ValidatorRule) -> Bool {
+    switch (lhs, rhs) {
+    case (.soft(let specification1, let message1), .soft(let specification2, let message2)):
+        return specification1 == specification2 && message1 == message2
+    case (.strict(let specification1, let message1), .strict(let specification2, let message2)):
+        return specification1 == specification2 && message1 == message2
+    case (.submit(let specification1, let message1), .submit(let specification2, let message2)):
+        return specification1 == specification2 && message1 == message2
+    default:
+        return false
+    }
+}
+
+
 /// The validator manages the rules and is responsible for validating the candidate.
 public class Validator {
 
+    /// A collection of rules.
     public private(set) var rules: [ValidatorRule]
 
-    private var timer: (timer: Timer, item: BaseFormItem)?
-
+    /// Initialize a validator with a collection of rules.
+    ///
+    /// - Parameter rules: A collection of rules.
     public init(rules: [ValidatorRule] = []) {
         self.rules = rules
     }
 
+    /// Validates the candidate against the stored rules. The validation process
+    /// stops immediately if it fails against a strict or a submit rules, and its result
+    /// is returned. If passes strict and submit validations, but has a soft invalid result,
+    /// the first soft invalid result is returned.
+    ///
+    /// - Parameters:
+    ///   - candidate: The candidate to be validated.
+    ///   - checkHardRule: True to validate using hard rules.
+    ///   - checkSoftRule: True to validate using soft rules.
+    ///   - checkSubmitRule: True to validate using submit rules.
+    /// - Returns: Valid result if the candidcate passes all the rules. Otherwise returns
+    ///            the first invalid result.
     public func validate(_ candidate: Any?, checkHardRule: Bool, checkSoftRule: Bool, checkSubmitRule: Bool) -> ValidateResult {
         var results = [ValidateResult]()
 
@@ -108,27 +168,17 @@ public class Validator {
         return results[0]
     }
 
-
-    /// MARK: - Add rule methods
-
+    /// Add a rule to validator.
+    ///
+    /// - Parameter rule: The rule.
     public func addRule(_ rule: ValidatorRule) {
         rules.append(rule)
     }
 
-    public func addSoftRule(_ specification: Specification, message: String) {
-        rules.append(.soft(specification: specification, message: message))
-    }
-
-    public func addStrictRule(_ specification: Specification, message: String) {
-        rules.append(.strict(specification: specification, message: message))
-    }
-
-    public func addSubmitRule(_ specification: Specification, message: String) {
-        rules.append(.submit(specification: specification, message: message))
-    }
-
 
     /// MARK: - Validation methods
+
+    private var timer: (timer: Timer, item: BaseFormItem)?
 
     @discardableResult public func validateAndUpdateErrorIfNeeded(_ candidate: Any?, shouldInstallTimer: Bool, checkSubmitRule: Bool, forItem item: BaseFormItem) -> Bool {
         invalidateTimer()
