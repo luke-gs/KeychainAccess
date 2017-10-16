aed8ce6bc7e5f2eeaad4ee4310a599ca42e4bfde
Kyle May <kyle.may@gridstone.com.au>
2017-10-17 09:15:30 +1100

    Callsign tab

--- CAD/CAD/AppDelegate.swift
+++ CAD/CAD/AppDelegate.swift
@@ -30,6 +30,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         applyCurrentTheme()
         window.makeKeyAndVisible()
         
+        let tempCallsignController = UIViewController() // TODO: Add callsign view
+        tempCallsignController.tabBarItem = UITabBarItem(title: "Callsign", image: AssetManager.shared.image(forKey: .entityCar), selectedImage: nil)
+        
         let searchProxyViewController = UIViewController() // TODO: Take me back to the search app
         searchProxyViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
         searchProxyViewController.tabBarItem.isEnabled = false
@@ -45,6 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         activityNavController.tabBarItem.title = NSLocalizedString("Activity Log", comment: "Activity Log Tab Bar Item")
 
         statusTabBarController.viewControllers = [searchProxyViewController, tasksNavController, activityNavController]
+        statusTabBarController.compactViewControllers = [tempCallsignController]
         statusTabBarController.selectedViewController = tasksNavController
 
         self.window?.rootViewController = statusTabBarController
