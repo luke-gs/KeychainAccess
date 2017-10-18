9cedd58dbb83f9fc3506312c2a36bccaa0fe4405
Vazmista <val@gridstone.com.au>
2017-10-18 14:30:59 +1100

    Location update timer

--- Classes/Core/Location Manager/LocationManager.swift
+++ Classes/Core/Location Manager/LocationManager.swift
@@ -28,9 +28,25 @@ public extension NSNotification.Name {
 
 public final class LocationManager: NSObject, CLLocationManagerDelegate {
     
+    static let interval:TimeInterval = 5*60
+    static let timeBuffer:Double = 60
+    
     /// Used to see the last time a location was retrieved
     open private(set) var lastLocationTime: Date? = nil
     fileprivate var locationManager: CLLocationManager = CLLocationManager()
+    
+    /// Automatic timer to periodically update location if no location has been obtained recently.
+    fileprivate var timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { (timer) in
+        if let time = LocationManager.shared.lastLocationTime {
+            if Date().timeIntervalSince(time) > timeBuffer { // Refresh location
+                LocationManager.shared.requestLocation(withCompletion: { (location, error) in
+                })
+            }
+        } else {
+            LocationManager.shared.requestLocation(withCompletion: { (location, error) in
+            })
+        }
+    }
     
     /// Used to get the last saved location.
     open var lastLocation: CLLocation? {
@@ -140,6 +156,11 @@ public final class LocationManager: NSObject, CLLocationManagerDelegate {
             requestLocation(withCompletion: completionBlock)
         }
         requestAuthorizationCompletionArray.removeAll()
+    }
+    
+    // MARK: - Automatic refresh
+    public func locationUpdate() {
+        print("update fired")
     }
 }
 
