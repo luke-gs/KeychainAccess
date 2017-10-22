f05b790d43f84b9d6d391933e08cbc2f92c27d90
Vazmista <val@gridstone.com.au>
2017-10-23 10:55:53 +1100

    Removed old code

--- Classes/Core/Location Manager/LocationManager.swift
+++ Classes/Core/Location Manager/LocationManager.swift
@@ -44,12 +44,12 @@ public final class LocationManager: NSObject, CLLocationManagerDelegate {
     fileprivate var timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { (timer) in
         if let time = LocationManager.shared.lastLocationTime {
             if Date().timeIntervalSince(time) > timeBuffer { // Refresh location
-                LocationManager.shared.requestLocation(withCompletion: { (location, error) in
-                })
+                LocationManager.shared.requestLocation().always {
+                }
             }
         } else {
-            LocationManager.shared.requestLocation(withCompletion: { (location, error) in
-            })
+            LocationManager.shared.requestLocation().always {
+            }
         }
     }
     
@@ -70,9 +70,6 @@ public final class LocationManager: NSObject, CLLocationManagerDelegate {
         }
     }
     
-    fileprivate var requestCompletionArray:[((CLLocation?, Error?) -> Void)] = []
-    fileprivate var requestAuthorizationCompletionArray:[((CLLocation?, Error?) -> Void)] = []
-    
     /// The singleton shared locationManager. This is the only instance of this class.
     public static let shared = LocationManager()
     
@@ -85,89 +82,16 @@ public final class LocationManager: NSObject, CLLocationManagerDelegate {
     
     private var latestLocation: CLLocation? = nil
     
+    /// Request a single location. Will automatically request authorization for both when in use and always depending if the valid description is in the info.plist
+    ///
+    /// - Return:
+    ///     - A promise with a Location
+    ///
     open func requestLocation() -> Promise<CLLocation> {
         return CLLocationManager.promise().then { location -> CLLocation in
             self.latestLocation = location
             return location
         }
-    }
-    
-    /// Request a single location. Will automatically request authorization for both when in use and always depending if the valid description is in the info.plist
-    ///
-    /// - Parameters:
-    ///   - completion:  Will return a CLLocation if one is found, or an Error (If authorization is not given of type LocationError).
-    ///
-    open func requestLocation(withCompletion completion:@escaping ((CLLocation?, Error?) -> Void)) {
-        let authorizationStatus = CLLocationManager.authorizationStatus()
-
-        if authorizationStatus == CLAuthorizationStatus.notDetermined {
-            requestAuthorizationCompletionArray.append(completion)
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.requestAlwaysAuthorization()
-            return
-        }
-        
-        if (authorizationStatus != .authorizedAlways  && authorizationStatus != .authorizedWhenInUse) || CLLocationManager.locationServicesEnabled() == false  {
-            if (authorizationStatus == .denied) {
-                completion(nil, LocationError.deniedAccess)
-                return
-            } else if CLLocationManager.locationServicesEnabled() == false {
-                completion(nil, LocationError.locationServicesTurnedOff)
-                return
-            }
-            completion(nil, nil)
-            return
-        }
-
-        requestCompletionArray.append(completion)
-        locationManager.requestLocation()
-    }
-    
-    // MARK: - CLLocationManager delegates
-    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let lastLocation = locations.last else {
-            for completionBlock in requestCompletionArray {
-                completionBlock(nil, nil)
-            }
-            requestCompletionArray.removeAll()
-            return
-        }
-
-        for completionBlock in requestCompletionArray {
-            completionBlock(lastLocation, nil)
-        }
-        requestCompletionArray.removeAll()
-        
-        NotificationCenter.default.post(name: .LocationDidUpdate, object: self)
-    }
-    
-    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        for completionBlock in requestCompletionArray {
-            completionBlock(nil, error)
-        }
-        requestCompletionArray.removeAll()
-    }
-    
-    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if (status != .authorizedAlways  && status != .authorizedWhenInUse) || CLLocationManager.locationServicesEnabled() == false  {
-            if (status == .denied) {   // Authorisation not given
-                for completionBlock in requestAuthorizationCompletionArray {
-                    completionBlock(nil, LocationError.deniedAccess)
-                }
-                requestAuthorizationCompletionArray.removeAll()
-            } else if CLLocationManager.locationServicesEnabled() == false {
-                for completionBlock in requestAuthorizationCompletionArray {
-                    completionBlock(nil, LocationError.locationServicesTurnedOff)
-                }
-                requestAuthorizationCompletionArray.removeAll()
-            }
-            return
-        }
-        
-        for completionBlock in requestAuthorizationCompletionArray {
-            requestLocation(withCompletion: completionBlock)
-        }
-        requestAuthorizationCompletionArray.removeAll()
     }
 }
 
