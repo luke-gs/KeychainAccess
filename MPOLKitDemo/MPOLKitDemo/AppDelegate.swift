4982b0ec22e3964ee345e2f3cf6d8b3c9a42142e
James Aramroongrot <jamesa@gridstone.com.au>
2017-10-24 15:01:13 +1100

    Provide test and example

--- MPOLKitDemo/MPOLKitDemo/AppDelegate.swift
+++ MPOLKitDemo/MPOLKitDemo/AppDelegate.swift
@@ -132,6 +132,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let picker = PickerViewController()
         picker.sidebarItem.regularTitle = "Pickers"
 
+        let stepper = StepperViewController()
+        stepper.sidebarItem.regularTitle = "Steppers"
+
         let personDetail = PersonDetailViewController()
         personDetail.sidebarItem.regularTitle = "Person Details"
 
@@ -144,7 +147,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let subscription = SubscriptionViewController()
         subscription.sidebarItem.regularTitle = "Subscription"
 
-        return [basic, list, custom, accessory, header, picker, personDetail, results, signup, subscription]
+        return [basic, list, custom, accessory, header, picker, stepper, personDetail, results, signup, subscription]
     }()
     
 }
