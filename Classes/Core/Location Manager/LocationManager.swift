c7e4cc0b6358921d15371e401ee180065b5c1e9c
Vazmista <val@gridstone.com.au>
2017-10-27 09:26:17 +1100

    requestPlacemark

--- Classes/Core/Location Manager/LocationManager.swift
+++ Classes/Core/Location Manager/LocationManager.swift
@@ -42,6 +42,7 @@ public final class LocationManager: NSObject {
     
     /// Used to get the last saved location.
     open var lastLocation: CLLocation? = nil
+    open var lastPlacemark: CLPlacemark? = nil
     
     private override init() {
         super.init()
@@ -58,6 +59,31 @@ public final class LocationManager: NSObject {
             self.lastLocation = location
             NotificationCenter.default.post(name: .LocationDidUpdate, object: self)
             return location
+        }
+    }
+    
+    /// Request a location to be reversegeocoded. Will automatically request authorization for both when in use and always depending if the valid description is in the info.plist
+    ///
+    /// - Return:
+    ///     - A promise with a Placemark
+    ///
+    open func requestPlacemark(from location: CLLocation) -> Promise<CLPlacemark> {
+        return CLGeocoder().reverseGeocode(location: location)
+    }
+    
+    /// Request a single location reversegeocoded. Will automatically request authorization for both when in use and always depending if the valid description is in the info.plist
+    ///
+    /// - Return:
+    ///     - A promise with a Placemark
+    ///
+    @discardableResult
+    open func requestPlacemark() -> Promise<CLPlacemark> {
+        return CLLocationManager.promise().then { location in
+            self.lastLocation = location
+            return CLGeocoder().reverseGeocode(location: location).then { placemark -> CLPlacemark in
+                self.lastPlacemark = placemark
+                return placemark
+            }
         }
     }
 }
