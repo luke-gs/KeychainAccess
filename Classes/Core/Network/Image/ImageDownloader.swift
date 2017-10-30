b2988f0cc9f30f8b9e6d4c4d8741ff90c7605ea9
Herli <herli@gridstone.com.au>
2017-10-30 15:00:42 +1100

    Added blank downloader system

---
+++ Classes/Core/Network/Image/ImageDownloader.swift
@@ -0,0 +1,13 @@
+//
+//  ImageDownloader.swift
+//  MPOLKit
+//
+//  Created by Herli Halim on 30/10/17.
+//  Copyright © 2017 Gridstone. All rights reserved.
+//
+
+import Alamofire
+
+public class ImageDownloader {
+
+}
