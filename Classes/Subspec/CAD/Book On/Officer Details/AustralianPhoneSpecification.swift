5872090cc99d6bfc7f0bb0d6990de41442c6c715
Kyle May <kyle.may@gridstone.com.au>
2017-11-01 09:42:34 +1100

    Add phone number validation

---
+++ Classes/Subspec/CAD/Book On/Officer Details/AustralianPhoneSpecification.swift
@@ -0,0 +1,27 @@
+//
+//  AustralianPhoneSpecification.swift
+//  MPOLKit
+//
+//  Created by Kyle May on 1/11/17.
+//  Copyright © 2017 Gridstone. All rights reserved.
+//
+
+import UIKit
+
+public class AustralianPhoneSpecification: Specification {
+    public func isSatisfiedBy(_ candidate: Any?) -> Bool {
+        if let number = candidate as? String {
+            return isValidAustralianPhoneNumber(number)
+        }
+        
+        return false
+    }
+    
+    /// Checks if the string is a valid Australian mobile phone number
+    func isValidAustralianPhoneNumber(_ number: String) -> Bool {
+        let phoneRegex = "^\\+?61[0-9]{9}|^(0[0-9])?[0-9]{8}"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+
+        return phoneTest.evaluate(with: number.replacingOccurrences(of: " ", with: ""))
+    }
+}
