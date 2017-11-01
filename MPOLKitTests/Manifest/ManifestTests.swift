afa9a0e592779d714d383d2f8597de0d67a85020
Vazmista <val@gridstone.com.au>
2017-11-01 15:11:15 +1100

    Updated tests

--- MPOLKitTests/Manifest/ManifestTests.swift
+++ MPOLKitTests/Manifest/ManifestTests.swift
@@ -27,101 +27,98 @@ class ManifestTests: XCTestCase {
                 return
         }
         
-        Manifest.shared.saveManifest(with: manifestArray, at: Date(), completion: { error in
-            if let error = error {
-                XCTFail("Manifest Error: \(error.localizedDescription)")
-            } else {
+        Manifest.shared.saveManifest(with: manifestArray, at: Date()).always {
                 for item in manifestArray {
-                    if let id = item["id"] as? String {
-                        if let fetchedItem = Manifest.shared.entry(withID: id) {
-                            XCTAssertEqual(fetchedItem.id, id)
-                            if let active = item["active"] as? Bool {
-                                XCTAssertEqual(fetchedItem.active, active)
+                if let id = item["id"] as? String {
+                    if let fetchedItem = Manifest.shared.entry(withID: id) {
+                        XCTAssertEqual(fetchedItem.id, id)
+                        if let active = item["active"] as? Bool {
+                            XCTAssertEqual(fetchedItem.active, active)
+                        } else {
+                            XCTFail("Manifest Error: Active cannot be nil")
+                        }
+                        if let category = item["category"] as? String {
+                            XCTAssertEqual(fetchedItem.collection, category)
+                        } else {
+                            XCTAssertEqual(fetchedItem.collection, nil)
+                        }
+                        if let title = item["title"] as? String {
+                            XCTAssertEqual(fetchedItem.title, title)
+                        } else {
+                            XCTAssertEqual(fetchedItem.title, nil)
+                        }
+                        if let subtitle = item["subtitle"] as? String {
+                            XCTAssertEqual(fetchedItem.subtitle, subtitle)
+                        } else {
+                            XCTAssertEqual(fetchedItem.subtitle, nil)
+                        }
+                        if let shortTitle = item["shortTitle"] as? String {
+                            XCTAssertEqual(fetchedItem.shortTitle, shortTitle)
+                        } else {
+                            XCTAssertEqual(fetchedItem.shortTitle, nil)
+                        }
+                        if let rawValue = item["value"] as? String {
+                            XCTAssertEqual(fetchedItem.rawValue, rawValue)
+                        } else {
+                            XCTAssertEqual(fetchedItem.rawValue, nil)
+                        }
+                        if let sortOrder = item["sortOrder"] as? Double {
+                            XCTAssertEqual(fetchedItem.sortOrder, sortOrder)
+                        } else {
+                            XCTAssertEqual(fetchedItem.sortOrder, nil)
+                        }
+                        
+                        if let effectiveTI = item["effectiveDate"] as? String {
+                            XCTAssertEqual(fetchedItem.effectiveDate, Manifest.dateFormatter.date(from: effectiveTI) as Date?)
+                        } else {
+                            XCTAssertEqual(fetchedItem.effectiveDate, nil)
+                        }
+                        
+                        if let expiryTI = item["expiryDate"] as? String {
+                            XCTAssertEqual(fetchedItem.expiryDate, Manifest.dateFormatter.date(from: expiryTI) as Date?)
+                        } else {
+                            XCTAssertEqual(fetchedItem.expiryDate, nil)
+                        }
+                        
+                        if let lastUpdatedTI = item["dateLastUpdated"] as? String {
+                            XCTAssertEqual(fetchedItem.lastUpdated, Manifest.dateFormatter.date(from: lastUpdatedTI) as Date?)
+                        } else {
+                            XCTAssertEqual(fetchedItem.lastUpdated, nil)
+                        }
+                        
+                        if var additionalData = item["additionalData"] as? [String: Any] {
+                            
+                            if let latitude = additionalData["latitude"] as? NSNumber {
+                                XCTAssertEqual(fetchedItem.latitude, latitude)
+                                additionalData.removeValue(forKey: "latitude")
                             } else {
-                                XCTFail("Manifest Error: Active cannot be nil")
-                            }
-                            if let category = item["category"] as? String {
-                                XCTAssertEqual(fetchedItem.collection, category)
-                            } else {
-                                XCTAssertEqual(fetchedItem.collection, nil)
-                            }
-                            if let title = item["title"] as? String {
-                                XCTAssertEqual(fetchedItem.title, title)
-                            } else {
-                                XCTAssertEqual(fetchedItem.title, nil)
-                            }
-                            if let subtitle = item["subtitle"] as? String {
-                                XCTAssertEqual(fetchedItem.subtitle, subtitle)
-                            } else {
-                                XCTAssertEqual(fetchedItem.subtitle, nil)
-                            }
-                            if let shortTitle = item["shortTitle"] as? String {
-                                XCTAssertEqual(fetchedItem.shortTitle, shortTitle)
-                            } else {
-                                XCTAssertEqual(fetchedItem.shortTitle, nil)
-                            }
-                            if let rawValue = item["value"] as? String {
-                                XCTAssertEqual(fetchedItem.rawValue, rawValue)
-                            } else {
-                                XCTAssertEqual(fetchedItem.rawValue, nil)
-                            }
-                            if let sortOrder = item["sortOrder"] as? Double {
-                                XCTAssertEqual(fetchedItem.sortOrder, sortOrder)
-                            } else {
-                                XCTAssertEqual(fetchedItem.sortOrder, nil)
+                                XCTAssertEqual(fetchedItem.latitude, nil)
                             }
                             
-                            if let effectiveTI = item["effectiveDate"] as? String {
-                                XCTAssertEqual(fetchedItem.effectiveDate, Manifest.dateFormatter.date(from: effectiveTI) as Date?)
+                            if let longitude = additionalData["longitude"] as? NSNumber {
+                                XCTAssertEqual(fetchedItem.longitude, longitude)
+                                additionalData.removeValue(forKey: "longitude")
                             } else {
-                                XCTAssertEqual(fetchedItem.effectiveDate, nil)
+                                XCTAssertEqual(fetchedItem.longitude, nil)
                             }
                             
-                            if let expiryTI = item["expiryDate"] as? String {
-                                XCTAssertEqual(fetchedItem.expiryDate, Manifest.dateFormatter.date(from: expiryTI) as Date?)
-                            } else {
-                                XCTAssertEqual(fetchedItem.expiryDate, nil)
-                            }
-                            
-                            if let lastUpdatedTI = item["dateLastUpdated"] as? String {
-                                XCTAssertEqual(fetchedItem.lastUpdated, Manifest.dateFormatter.date(from: lastUpdatedTI) as Date?)
-                            } else {
-                                XCTAssertEqual(fetchedItem.lastUpdated, nil)
-                            }
-                            
-                            if var additionalData = item["additionalData"] as? [String: Any] {
-                                
-                                if let latitude = additionalData["latitude"] as? NSNumber {
-                                    XCTAssertEqual(fetchedItem.latitude, latitude)
-                                    additionalData.removeValue(forKey: "latitude")
-                                } else {
-                                    XCTAssertEqual(fetchedItem.latitude, nil)
-                                }
-                                
-                                if let longitude = additionalData["longitude"] as? NSNumber {
-                                    XCTAssertEqual(fetchedItem.longitude, longitude)
-                                    additionalData.removeValue(forKey: "longitude")
-                                } else {
-                                    XCTAssertEqual(fetchedItem.longitude, nil)
-                                }
-                                
-                                if let newData = try? JSONSerialization.data(withJSONObject: additionalData), let json = String(data: newData, encoding: .utf8) {
-                                    XCTAssertEqual(fetchedItem.additionalData, json)
-                                } else {
-                                    XCTAssertEqual(fetchedItem.additionalData, nil)
-                                }
+                            if let newData = try? JSONSerialization.data(withJSONObject: additionalData), let json = String(data: newData, encoding: .utf8) {
+                                XCTAssertEqual(fetchedItem.additionalData, json)
                             } else {
                                 XCTAssertEqual(fetchedItem.additionalData, nil)
                             }
                         } else {
-                            XCTFail("Manifest Error: Unable to find item")
+                            XCTAssertEqual(fetchedItem.additionalData, nil)
                         }
-                        
+                    } else {
+                        XCTFail("Manifest Error: Unable to find item")
                     }
+                    
                 }
-                
             }
-        })
+            }.catch { error in
+                XCTFail("Manifest Error: \(error.localizedDescription)")
+        }
     }
     
 }
