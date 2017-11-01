d2b6eab8a60333cb43ae8def60726a1fc2e74663
Herli <herli@gridstone.com.au>
2017-11-02 05:03:27 +1100

    Only send one request per URL

--- Classes/Core/Network/Image/ImageDownloader.swift
+++ Classes/Core/Network/Image/ImageDownloader.swift
@@ -20,21 +20,72 @@ public class ImageDownloader {
 
     public static let `default` = ImageDownloader()
 
-    // If `APIManager` is not provided, the `APIManager.shared` will be used.
-    // This is to allow to pass in different APIManager setup if required while
-    // allowing using the global mutating `APIManager.shared`.
+    private let barrierQueue: DispatchQueue
+    private var fetchRequests = [URL: ImageFetchRequest]()
+
+    /// If `APIManager` is not provided, the `APIManager.shared` will be used.
+    /// This is to allow to pass in different APIManager setup if required while
+    /// allowing using the global mutating `APIManager.shared`.
     public init(apiManager: APIManager? = nil, diskCacheConfig: DiskConfig = ImageDownloader.defaultDiskCacheConfig) {
         self.imageDiskCacheConfig = diskCacheConfig
         // Use default `MemoryConfig`, it'll automatically clear anyway.
 
         self.apiManager = apiManager
         imageCache = try! Storage(diskConfig: diskCacheConfig, memoryConfig: MemoryConfig())
+
+        barrierQueue = DispatchQueue(label: "au.com.gridstone.ImageDownloader.Barrier.\(diskCacheConfig.name)", attributes: .concurrent)
     }
 
+    /// Fetch image using the the `RemoteResourceDescribing`. The result will be cached
+    /// and used for the subsequent requests.
+    ///
+    /// - Parameter imageResourceDescription: The description to download and cache the image
+    /// - Returns: Promise to return a UIImage when the fetch is completed.
     @discardableResult
     public func fetchImage(using imageResourceDescription: RemoteResourceDescribing) -> Promise<UIImage> {
-        // Try to retrieve from cache first.
 
+        let toBeFulfilled = Promise<UIImage>.pending()
+
+        barrierQueue.sync(flags: .barrier) {
+            let request: ImageFetchRequest
+
+            if let fetchRequest = fetchRequests[imageResourceDescription.downloadURL] {
+                request = fetchRequest
+            } else {
+                let fetchPromise = fetchAndCacheImage(using: imageResourceDescription)
+                request = ImageFetchRequest(url: imageResourceDescription.downloadURL, fetchPromise: fetchPromise)
+            }
+            request.pendingPromises.append(toBeFulfilled)
+
+            request.fetchPromise.then { [weak self] image -> Void in
+                guard let `self` = self else {
+                    return
+                }
+                if let fetchRequest = self.fetchRequest(for: imageResourceDescription.downloadURL) {
+                    for (_, fulfill, _) in fetchRequest.pendingPromises {
+                        fulfill(image)
+                    }
+                }
+            }.catch { error in
+                    if let fetchRequest = self.fetchRequest(for: imageResourceDescription.downloadURL) {
+                        for (_, _, reject) in fetchRequest.pendingPromises {
+                            reject(error)
+                        }
+                    }
+            }.always { [weak self] in
+                _ = self?.barrierQueue.sync(flags: .barrier) {
+                    self?.fetchRequests.removeValue(forKey: imageResourceDescription.downloadURL)
+                }
+            }
+
+            fetchRequests[imageResourceDescription.downloadURL] = request
+        }
+        return toBeFulfilled.promise
+    }
+
+    private func fetchAndCacheImage(using imageResourceDescription: RemoteResourceDescribing) -> Promise<UIImage> {
+
+        // Try to retrieve from cache first.
         func retrieveAndCacheImagePromise() -> Promise<UIImage> {
             let networkRequest = try! NetworkRequest(pathTemplate: imageResourceDescription.downloadURL.absoluteString, parameters: [:], isRelativePath: false)
             let promise: Promise<UIImage> = try! _actualAPIManager.performRequest(networkRequest)
@@ -76,6 +127,17 @@ public class ImageDownloader {
         } else {
             return retrieveAndCacheImagePromise()
         }
+
+    }
+
+    // MARK: - Private utilities
+
+    private func fetchRequest(for url: URL) -> ImageFetchRequest? {
+        var fetchRequest: ImageFetchRequest?
+        barrierQueue.sync(flags: .barrier) {
+            fetchRequest = fetchRequests[url]
+        }
+        return fetchRequest
     }
 
     private var _actualAPIManager: APIManager {
@@ -83,6 +145,17 @@ public class ImageDownloader {
             return APIManager.shared
         }
         return apiManager
+    }
+}
+
+private class ImageFetchRequest {
+    let url: URL
+    let fetchPromise: Promise<UIImage>
+    var pendingPromises = [(promise: Promise<UIImage>, fulfill: (UIImage) -> Void, reject: (Error) -> Void)]()
+
+    init(url: URL, fetchPromise: Promise<UIImage>) {
+        self.url = url
+        self.fetchPromise = fetchPromise
     }
 }
 
