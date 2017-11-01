d88db8453e8ff8f5412a5664929c187c71813f99
Herli <herli@gridstone.com.au>
2017-11-02 03:15:18 +1100

    Implemented downloader

--- Classes/Core/Network/Image/ImageDownloader.swift
+++ Classes/Core/Network/Image/ImageDownloader.swift
@@ -8,10 +8,93 @@
 
 import Alamofire
 import PromiseKit
+import Cache
 
 public class ImageDownloader {
 
+    public let apiManager: APIManager?
 
+    private let imageCache: Storage
+
+    public let imageDiskCacheConfig: DiskConfig
+
+    public let `default` = ImageDownloader()
+
+    // If `APIManager` is not provided, the `APIManager.shared` will be used.
+    // This is to allow to pass in different APIManager setup if required while
+    // allowing using the global mutating `APIManager.shared`.
+    public init(apiManager: APIManager? = nil, diskCacheConfig: DiskConfig = ImageDownloader.defaultDiskCacheConfig) {
+        self.imageDiskCacheConfig = diskCacheConfig
+        // Use default `MemoryConfig`, it'll automatically clear anyway.
+
+        self.apiManager = apiManager
+        imageCache = try! Storage(diskConfig: diskCacheConfig, memoryConfig: MemoryConfig())
+    }
+
+    public func fetchImage(using imageResourceDescription: RemoteResourceDescribing) -> Promise<UIImage> {
+        // Try to retrieve from cache first.
+
+        func retrieveAndCacheImagePromise() -> Promise<UIImage> {
+            let networkRequest = try! NetworkRequest(pathTemplate: imageResourceDescription.downloadURL.absoluteString, parameters: [:])
+            let promise: Promise<UIImage> = try! _actualAPIManager.performRequest(networkRequest)
+
+            return promise.then { [weak self] image -> Promise<UIImage> in
+
+                if let strongSelf = self {
+                    let imageWrapper = ImageWrapper(image: image)
+                    // Do nothing with the completion, it's not that important anyway.
+                    strongSelf.imageCache.async.setObject(imageWrapper, forKey: imageResourceDescription.cacheKey, completion: { _ in })
+                }
+
+                return Promise(value: image)
+            }
+        }
+
+        let isCached = try? imageCache.existsObject(ofType: ImageWrapper.self, forKey: imageResourceDescription.cacheKey)
+
+        if let isCached = isCached, isCached { // If cache for the key exists.
+
+            let promise = Promise<UIImage> { [weak self] fulfill, reject in
+                guard let `self` = self else {
+                    throw NSError.cancelledError()
+                }
+                self.imageCache.async.entry(ofType: ImageWrapper.self, forKey: imageResourceDescription.cacheKey, completion: { result in
+                    switch result {
+                    case .value(let entry):
+                        fulfill(entry.object.image)
+                    case .error(let error):
+                        reject(error)
+                    }
+                })
+            }
+
+            // If for whatever reasons fetching from cache failed, then go fetch from remote.
+            return promise.recover(execute: { _ -> Promise<UIImage> in
+                return retrieveAndCacheImagePromise()
+            })
+        } else {
+            return retrieveAndCacheImagePromise()
+        }
+    }
+
+    private var _actualAPIManager: APIManager {
+        guard let apiManager = apiManager else {
+            return APIManager.shared
+        }
+        return apiManager
+    }
+}
+
+// MARK: - Default
+extension ImageDownloader {
+
+    public static let defaultCacheName: String = "au.com.gridstone.ImageDownloader.cache.default"
+
+    // Default config has expiry of 1 hoursand size of 100 MB.
+    public static var defaultDiskCacheConfig: DiskConfig {
+        let config = DiskConfig(name: ImageDownloader.defaultCacheName, expiry: .seconds(3600), maxSize: 150 * 1024 * 1024, directory: nil, protectionType: nil)
+        return config
+    }
 }
 
 public extension APIManager {
