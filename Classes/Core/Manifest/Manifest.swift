a9e33f7ec3b392fa85ce8dc94892afef986e1946
Vazmista <val@gridstone.com.au>
2017-11-09 09:39:29 +1100

    Removed commented code

--- Classes/Core/Manifest/Manifest.swift
+++ Classes/Core/Manifest/Manifest.swift
@@ -259,7 +259,6 @@ public final class Manifest: NSObject {
             isSaving = true
             let managedObjectContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
             managedObjectContext.parent = viewContext
-//            managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
             managedObjectContext.perform { [weak managedObjectContext] in
                 guard manifestItems.isEmpty ==  false, let context = managedObjectContext else {
                     fulfill(())
