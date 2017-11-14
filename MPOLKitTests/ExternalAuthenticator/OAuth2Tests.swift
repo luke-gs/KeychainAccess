eaf582fdfff04e0d7a1b3228d01b6a1b6e58448d
Herli <herli@gridstone.com.au>
2017-11-14 13:22:21 +1100

    Promise to write test??

---
+++ MPOLKitTests/ExternalAuthenticator/OAuth2Tests.swift
@@ -0,0 +1,14 @@
+//
+//  OAuth2Tests.swift
+//  MPOLKitTests
+//
+//  Created by Herli Halim on 13/11/17.
+//  Copyright © 2017 Gridstone. All rights reserved.
+//
+
+import XCTest
+import MPOLKit
+
+class OAuth2Tests: XCTestCase {
+
+}
