b9a03f3c6c30c18bc09786c4fbdf4c2eafb25368
Herli <herli@gridstone.com.au>
2017-11-15 10:59:08 +1100

    Don’t check for the scheme

--- Classes/Subspec/ExternalAuthenticator/ExternalAuthenticator.swift
+++ Classes/Subspec/ExternalAuthenticator/ExternalAuthenticator.swift
@@ -35,9 +35,6 @@ public final class ExternalAuthenticator<T: AuthenticationProvider> {
     /// - Returns: A promise with the result returned by the provider.
     public func authenticate() -> Promise<T.Result> {
 
-        let scheme = authenticationProvider.urlScheme
-        precondition(Bundle.main.containsURLScheme(scheme), "\(scheme) is not registered in the Info.plist")
-
         let pendingTuple: Promise<T.Result>.PendingTuple = Promise.pending()
         self.pendingPromiseResult = pendingTuple
 
