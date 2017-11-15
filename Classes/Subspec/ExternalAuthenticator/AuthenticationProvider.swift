f9783ddb9445cf3fdc310891eecf6a5dbb6c5667
Herli <herli@gridstone.com.au>
2017-11-15 11:22:45 +1100

    Delegate the check to the provider
    Provider would know more about what they can do

--- Classes/Subspec/ExternalAuthenticator/AuthenticationProvider.swift
+++ Classes/Subspec/ExternalAuthenticator/AuthenticationProvider.swift
@@ -18,6 +18,12 @@ public protocol AuthenticationProvider {
     /// The URL app url scheme that this provider should handle.
     var urlScheme: String { get }
 
+    /// Whether the provider could handle given callback
+    ///
+    /// - Parameter url: The callback URL that's triggered.
+    /// - Returns: true if it's supported URL.
+    func canHandleURL(_ url: URL) -> Bool
+    
     /// The result of the authentication.
     ///
     /// - Parameter url: The callback URL that's triggered. This URL should contain the result returned in
