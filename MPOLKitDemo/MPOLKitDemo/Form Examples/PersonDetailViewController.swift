db927ca3e17990f506e24294a87c0ec9e0325b16
James Aramroongrot <jamesa@gridstone.com.au>
2017-11-17 16:56:11 +1100

    Provide example

--- MPOLKitDemo/MPOLKitDemo/Form Examples/PersonDetailViewController.swift
+++ MPOLKitDemo/MPOLKitDemo/Form Examples/PersonDetailViewController.swift
@@ -11,9 +11,39 @@ import MPOLKit
 
 class PersonDetailViewController: FormBuilderViewController {
 
+
     override func construct(builder: FormBuilder) {
 
         builder.title = "Person Details"
+
+        let dataSource: MediaDataSource<PhotoMedia> = [
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Avatar 1"), image: #imageLiteral(resourceName: "Avatar 1"), title: "Jeff Handerson", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Login"), image: #imageLiteral(resourceName: "Login"), title: "Background of some random screen", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "SidebarInfoFilled"), image: #imageLiteral(resourceName: "SidebarInfoFilled"), title: "The davin ci icon", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Avatar 1"), image: #imageLiteral(resourceName: "Avatar 1"), title: "Jeff Handerson in the fresh.", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Login"), image: #imageLiteral(resourceName: "Login"), title: "A group of officers pretending to be working", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "SidebarInfoFilled"), image: #imageLiteral(resourceName: "SidebarInfoFilled"), title: "Icon of death", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Avatar 1"), image: #imageLiteral(resourceName: "Avatar 1"), title: "A man", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Login"), image: #imageLiteral(resourceName: "Login"), title: "Another group of officers", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "SidebarInfoFilled"), image: #imageLiteral(resourceName: "SidebarInfoFilled"), title: "A pointless infomration icon", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Avatar 1"), image: #imageLiteral(resourceName: "Avatar 1"), title: "A real man", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "Login"), image: #imageLiteral(resourceName: "Login"), title: "The blue officers", sensitive: false),
+            PhotoMedia(thumbnailImage: #imageLiteral(resourceName: "SidebarInfoFilled"), image: #imageLiteral(resourceName: "SidebarInfoFilled"), title: "Jeff Handerson", sensitive: false)
+        ]
+
+        let mediaItem = MediaFormItem().dataSource(dataSource)
+
+        if let viewController = UIApplication.shared.keyWindow?.rootViewController {
+            mediaItem.previewingController(viewController)
+        }
+
+        builder += HeaderFormItem(text: "PHOTOS").actionButton(title: "ADD", handler: { [weak self] in
+            if let viewController = mediaItem.delegate.viewControllerForMediaDataSource(dataSource) {
+                self?.present(viewController, animated: true, completion: nil)
+            }
+        })
+
+        builder += mediaItem
 
         builder += HeaderFormItem(text: "DETAILS")
 
@@ -44,3 +74,45 @@ class PersonDetailViewController: FormBuilderViewController {
     }
 
 }
+
+
+class DocumentItem: MediaPreviewable {
+
+    enum DocumentType {
+        case pdf
+        case docs
+        case txt
+        case unknown
+
+        func image() -> UIImage {
+            switch self {
+            case .pdf: return AssetManager.shared.image(forKey: .direction)!
+            case .docs: return AssetManager.shared.image(forKey: .event)!
+            case .txt: return AssetManager.shared.image(forKey: .document)!
+            case .unknown: return AssetManager.shared.image(forKey: .attachment)!
+            }
+        }
+    }
+
+    let title: String?
+
+    let type: DocumentType
+
+    let thumbnailImage: ImageLoadable?
+
+    let sensitiveText: String? = nil
+
+    init(type: DocumentType, title: String?) {
+        self.type = type
+        self.title = title
+
+        var sizing = type.image().sizing()
+        sizing.contentMode = .center
+        thumbnailImage = sizing
+    }
+
+}
+
+
+
+
