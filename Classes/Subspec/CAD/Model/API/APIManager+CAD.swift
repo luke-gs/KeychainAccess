59ae9f7fe7c4d274a1d90313a38c93a9e6c7f6bd
Trent Fitzgibbon <trentf@gridstone.com.au>
2017-11-20 15:41:10 +1100

    Added models for syncSummaries

--- Classes/Subspec/CAD/Model/API/APIManager+CAD.swift
+++ Classes/Subspec/CAD/Model/API/APIManager+CAD.swift
@@ -8,8 +8,42 @@
 
 import PromiseKit
 import Alamofire
+import Unbox
 
 /// Extension for APIManager for CAD specific network requests
 extension APIManager {
 
+    /// Fetch details about an offier by username.
+    ///
+    /// - Parameters:
+    ///   - username: The officer username
+    /// - Returns: A promise to return a X.
+    open func getOfficerByUsername(username: String) -> Promise<Data> {
+
+        let path = "/officer/username/{username}"
+        let parameters = ["username": username]
+
+        let networkRequest = try! NetworkRequest(pathTemplate: path, parameters: parameters)
+        return firstly {
+            return try! performRequest(networkRequest)
+        }.then { (data, response) in
+            return data
+        }.catch { (error) in
+            print(error)
+        }
+    }
+
+    open func syncSummaries() -> Promise<CADSyncSummaries> {
+        let path = "/sync/summaries"
+
+        let networkRequest = try! NetworkRequest(pathTemplate: path, parameters: [:], method: .post)
+        return firstly {
+            return try! performRequest(networkRequest)
+        }.then { arg -> Promise<CADSyncSummaries> in
+            guard let json = try! JSONSerialization.jsonObject(with: arg.0, options: []) as? [String: AnyObject] else { throw UnboxError.customUnboxingFailed }
+            return Promise(value: CADSyncSummaries(fromDictionary: json))
+        }.catch { (error) in
+            print(error)
+        }
+    }
 }
--- Classes/Subspec/CAD/Model/Session/CADStateManager.swift
+++ Classes/Subspec/CAD/Model/Session/CADStateManager.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PromiseKit
 
 public extension NSNotification.Name {
 
@@ -25,4 +26,24 @@ open class CADStateManager: NSObject {
             NotificationCenter.default.post(name: .CallsignChanged, object: self)
         }
     }
+
+    /// The last sync
+    open var lastSync: CADSyncSummaries?
+
+    /// Sync the latest manifest items
+    open func syncManifestItems() -> Promise<Void> {
+        return Manifest.shared.update()
+    }
+
+    /// Sync the latest task summaries
+    open func syncSummaries() -> Promise<CADSyncSummaries> {
+        // Perform sync and keep result
+        return firstly {
+            return APIManager.shared.syncSummaries()
+        }.then { summaries -> CADSyncSummaries in
+            self.lastSync = summaries
+            return summaries
+        }
+    }
+
 }
