62c8b99a0ccd3e9c19f5e4e7525519973bd7eea3
Kyle May <kyle.may@gridstone.com.au>
2017-11-30 11:27:29 +1100

    Update incidents for different types

--- Classes/Subspec/CAD/Tasks Details/Incidents/View Models/IncidentAssociationsViewModel.swift
+++ Classes/Subspec/CAD/Tasks Details/Incidents/View Models/IncidentAssociationsViewModel.swift
@@ -25,38 +25,49 @@ public class IncidentAssociationsViewModel: CADFormCollectionViewModel<EntitySum
         return [
             CADFormCollectionSectionViewModel(title: "4 People",
                                               items: [
-                                                IncidentPersonViewModel(category: "DS1",
-                                                                             initials: "JC",
-                                                                             title: "Citizen, John R.",
-                                                                             detail1: "08/05/87 (29 Male)",
-                                                                             detail2: "8 Catherine Street, Southbank VIC 3006",
-                                                                             borderColor: .orangeRed,
-                                                                             badge: 0),
-                                                IncidentPersonViewModel(category: "DS1",
-                                                                             initials: "NK",
-                                                                             title: "Kim, Nari S.",
-                                                                             detail1: "04/11/92 (24 Female)",
-                                                                             detail2: "23 Somerset Street, Richmond VIC 3121",
-                                                                             borderColor: nil,
-                                                                             badge: 0),
-                                                IncidentPersonViewModel(category: "DS1",
-                                                                             initials: "TK",
-                                                                             title: "Lazeron, Timo K.",
-                                                                             detail1: "27/02/1998 (29 Male)",
-                                                                             detail2: "27 Corsair Street, Richmond VIC 3121",
-                                                                             borderColor: .brightBlue,
-                                                                             badge: 0),
-                                                IncidentPersonViewModel(category: "DS1",
-                                                                             initials: "RW",
-                                                                             title: "Wang, Rocky T.",
-                                                                             detail1: "23/09/81 (35 Male)",
-                                                                             detail2: "302 Chandler Road, Keysborough VIC 3173",
-                                                                             borderColor: nil,
-                                                                             badge: 0)
-                ])
+                                                IncidentAssociationItemViewModel(category: "DS1",
+                                                                                 entityType: .person(initials: "JC"),
+                                                                                 title: "Citizen, John R.",
+                                                                                 detail1: "08/05/87 (29 Male)",
+                                                                                 detail2: "8 Catherine Street, Southbank VIC 3006",
+                                                                                 borderColor: .orangeRed,
+                                                                                 badge: 0),
+                                                IncidentAssociationItemViewModel(category: "DS1",
+                                                                                 entityType: .person(initials: "NK"),
+                                                                                 title: "Kim, Nari S.",
+                                                                                 detail1: "04/11/92 (24 Female)",
+                                                                                 detail2: "23 Somerset Street, Richmond VIC 3121",
+                                                                                 borderColor: nil,
+                                                                                 badge: 0),
+                                                IncidentAssociationItemViewModel(category: "DS1",
+                                                                                 entityType: .person(initials: "TK"),
+                                                                                 title: "Lazeron, Timo K.",
+                                                                                 detail1: "27/02/1998 (29 Male)",
+                                                                                 detail2: "27 Corsair Street, Richmond VIC 3121",
+                                                                                 borderColor: .brightBlue,
+                                                                                 badge: 0),
+                                                IncidentAssociationItemViewModel(category: "DS1",
+                                                                                 entityType: .person(initials: "RW"),
+                                                                                 title: "Wang, Rocky T.",
+                                                                                 detail1: "23/09/81 (35 Male)",
+                                                                                 detail2: "302 Chandler Road, Keysborough VIC 3173",
+                                                                                 borderColor: nil,
+                                                                                 badge: 0)
+            ]),
+            CADFormCollectionSectionViewModel(title: "1 Vehicle",
+                                              items: [
+                                                IncidentAssociationItemViewModel(category: "DS1",
+                                                                                 entityType: .vehicle,
+                                                                                 title: "ARP067",
+                                                                                 detail1: "2017 Tesla Model S",
+                                                                                 detail2: "Coupe • Black / Black",
+                                                                                 borderColor: .orangeRed,
+                                                                                 iconColor: .orangeRed,
+                                                                                 badge: 0)
+            ])
         ]
     }()
-
+    
     /// The title to use in the navigation bar
     override open func navTitle() -> String {
         return NSLocalizedString("Associations", comment: "Associations sidebar title")
@@ -70,5 +81,6 @@ public class IncidentAssociationsViewModel: CADFormCollectionViewModel<EntitySum
     override open func noContentSubtitle() -> String? {
         return nil
     }
-
+    
 }
+
