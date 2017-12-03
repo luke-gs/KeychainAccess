9251034295ae8a8823c8b34c761b1e140d81178f
Luke Sammut <lukes@gridstone.com.au>
2017-12-04 09:14:04 +1100

    Move the default imageDownloader to the kit

---
+++ Classes/Core/Network/Image/ImageDownloader.swift
@@ -0,0 +1,23 @@
+//
+//  ImageDownloader.swift
+//  MPOLKit
+//
+//  Created by QHMW64 on 4/12/17.
+//  Copyright © 2017 Gridstone. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+import Cache
+
+public class ImageDownloader {
+
+    private let _downloader = RemoteResourceDownloader<ImageWrapper>()
+    public static let `default` = ImageDownloader()
+    private init() {}
+
+    @discardableResult
+    public func fetch(for keypath: RemoteResourceDescribing) -> Promise<ImageWrapper> {
+        return _downloader.fetchResource(using: keypath.downloadURL)
+    }
+}
