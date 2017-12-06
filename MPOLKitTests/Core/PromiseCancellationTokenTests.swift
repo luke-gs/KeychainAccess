c3435ae5023d0c097bdfe68e26b11cfe437d378f
Herli <herli@gridstone.com.au>
2017-12-07 01:27:01 +1100

    Fixed the tests.

--- MPOLKitTests/Core/PromiseCancellationTokenTests.swift
+++ MPOLKitTests/Core/PromiseCancellationTokenTests.swift
@@ -16,7 +16,8 @@ class PromiseCancellationTokenTests: XCTestCase {
     func testCancellationToken() {
         let cancelExpectation = expectation(description: "testCancellation")
 
-        let (promise, token) = generatePromise()
+        let token = PromiseCancellationToken()
+        let promise = generatePromise(cancelToken: token)
 
         DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 2, execute: {
             token.cancel()
@@ -33,17 +34,99 @@ class PromiseCancellationTokenTests: XCTestCase {
         waitForExpectations(timeout: 15000, handler: nil)
     }
 
-    func generatePromise() -> CancellablePromise<Int> {
+    func testCancellationTokenCommandIsExecuted() {
+        let cancelExpectation = expectation(description: "testCancellationTokenCommandIsExecuted")
+
+        let token = PromiseCancellationToken()
+
+        var actuallyCancelled = false
+        token.addCancelCommand(ClosureCancelCommand {
+            actuallyCancelled = true
+        })
+
+        let promise = generatePromise(cancelToken: token)
+
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 2, execute: {
+            token.cancel()
+        })
+
+        promise.then { _ -> Void in
+            XCTAssert(false, "This operation should be cancelled and should never reach the `.then`.")
+            return ()
+        }.catch(policy: .allErrors) { error in
+                XCTAssert(error.isCancelledError, "Error is not due to cancellation.")
+                cancelExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 15000, handler: nil)
+        XCTAssertTrue(actuallyCancelled)
+    }
+
+    func testAddingCommandAfterCancelledNotExecuted() {
+        let cancelExpectation = expectation(description: "testAddingCommandAfterCancelledNotExecuted")
+
+        let token = PromiseCancellationToken()
+
+        let promise = generatePromise(cancelToken: token)
+
+        promise.then { _ -> Void in
+            XCTAssert(false, "This operation should be cancelled and should never reach the `.then`.")
+            return ()
+            }.catch(policy: .allErrors) { error in
+                XCTAssert(error.isCancelledError, "Error is not due to cancellation.")
+                cancelExpectation.fulfill()
+        }
+
+        var actuallyCancelled = false
+        // Cancel first
+        token.cancel()
+        // Add the command, this shouldn't be executed.
+        token.addCancelCommand(ClosureCancelCommand {
+            actuallyCancelled = true
+        })
+
+        waitForExpectations(timeout: 15000, handler: nil)
+        XCTAssertFalse(actuallyCancelled)
+    }
+
+    func testCancellingTwiceNotTriggeringMultipleCancelCommandsMultipleTimes() {
+        let cancelExpectation = expectation(description: "testCancellingTwiceNotTriggeringMultipleCancelCommandsMultipleTimes")
+
+        let token = PromiseCancellationToken()
+
+        var cancelledCount = 0
+        // Add the command, this shouldn't be executed.
+        token.addCancelCommand(ClosureCancelCommand {
+            cancelledCount += 1
+        })
+
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 2, execute: {
+            token.cancel()
+            token.cancel()
+        })
+
+        let promise = generatePromise(cancelToken: token)
+
+        promise.then { _ -> Void in
+            XCTAssert(false, "This operation should be cancelled and should never reach the `.then`.")
+            return ()
+            }.catch(policy: .allErrors) { error in
+                XCTAssert(error.isCancelledError, "Error is not due to cancellation.")
+                cancelExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 15000, handler: nil)
+        XCTAssertEqual(cancelledCount, 1)
+    }
+
+    func generatePromise(cancelToken: PromiseCancellationToken? = nil) -> Promise<Int> {
 
         let (promise, fulfill, reject) = Promise<Int>.pending()
-        let token = PromiseCancellationToken {
-            // Empty block, underlying task isn't really cancellable.
-        }
 
         DispatchQueue.global().async {
             var total: Int = 0
             for i in 0 ..< 50 {
-                if token.isCancelled {
+                if let token = cancelToken, token.isCancelled {
                     reject(NSError.cancelledError())
                     return
                 }
@@ -53,7 +136,7 @@ class PromiseCancellationTokenTests: XCTestCase {
             fulfill(total)
         }
 
-        return (promise, token)
+        return promise
 
     }
 }
