8d39f861db0967ca4d5b5bceb68ac45066cffd46
bryan-gs <bryan.hathaway@motorolasolutions.com>
2017-12-07 12:29:06 +1100

    Fix up icon imageview

--- Classes/UI/Views/SearchHeaderView.swift
+++ Classes/UI/Views/SearchHeaderView.swift
@@ -12,7 +12,7 @@ open class SearchHeaderView: UIView {
 
     // MARK: - Public Properties
 
-    /// Image to display inside the rounded imageView.
+    /// Image to display inside the circle.
     public var image: UIImage? {
         get {
             return imageView.image
@@ -37,6 +37,8 @@ open class SearchHeaderView: UIView {
     private var imageView: UIImageView = UIImageView(image: AssetManager.shared.image(forKey: .edit))
     private let searchBar: UISearchBar = UISearchBar(frame: .zero)
 
+    private let imageWidth: CGFloat = 48.0
+
     // MARK: - Initializers
 
     public override init(frame: CGRect) {
@@ -53,19 +55,22 @@ open class SearchHeaderView: UIView {
         backgroundColor = UIColor(red: 133.0/255.0, green: 134.0/255.0, blue: 141.0/255.0, alpha: 0.16)
         translatesAutoresizingMaskIntoConstraints = false
 
+        let circleView = UIView()
+        circleView.backgroundColor = #colorLiteral(red: 0.3364340067, green: 0.344623208, blue: 0.3837811649, alpha: 1)
+        circleView.clipsToBounds = true
+        circleView.layer.cornerRadius = 24.0
+        circleView.layer.shouldRasterize = true
+        circleView.layer.rasterizationScale = traitCollection.currentDisplayScale
+        circleView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(circleView)
+
         imageView.tintColor = UIColor.white
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 24.0
-        imageView.layer.shouldRasterize = true
-        imageView.layer.rasterizationScale = traitCollection.currentDisplayScale
-        imageView.backgroundColor = #colorLiteral(red: 0.3364340067, green: 0.344623208, blue: 0.3837811649, alpha: 1)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         addSubview(imageView)
 
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.font = .systemFont(ofSize: 17.0, weight: UIFont.Weight.semibold)
-        titleLabel.text = ""
         titleLabel.textColor = #colorLiteral(red: 0.3364340067, green: 0.344623208, blue: 0.3837811649, alpha: 1)
         titleLabel.numberOfLines = 1
         addSubview(titleLabel)
@@ -73,27 +78,32 @@ open class SearchHeaderView: UIView {
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
         subtitleLabel.numberOfLines = 0
         subtitleLabel.font = .systemFont(ofSize: 13.0, weight: UIFont.Weight.regular)
-        subtitleLabel.text = ""
         subtitleLabel.textColor = #colorLiteral(red: 0.5212343931, green: 0.5251564384, blue: 0.5512983203, alpha: 1)
         addSubview(subtitleLabel)
 
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         searchBar.delegate = self
-//        searchBar.searchBarStyle = .minimal
         searchBar.barTintColor = UIColor.white
         searchBar.backgroundColor = UIColor.clear
         searchBar.backgroundImage = UIImage()
-        searchBar.placeholder = "Search"
+        searchBar.placeholder = NSLocalizedString("Search", comment: "Search Text Placeholder")
         addSubview(searchBar)
 
+        let halfWidth = imageWidth / 2.0
+        let quarterWidth = imageWidth / 4.0
 
         NSLayoutConstraint.activate([
-            imageView.heightAnchor.constraint(equalToConstant: 48.0),
-            imageView.widthAnchor.constraint(equalToConstant: 48.0),
-            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 24.0),
-            imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24.0),
+            circleView.heightAnchor.constraint(equalToConstant: imageWidth),
+            circleView.widthAnchor.constraint(equalToConstant: imageWidth),
+            circleView.topAnchor.constraint(equalTo: self.topAnchor, constant: halfWidth),
+            circleView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: halfWidth),
 
-            titleLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 16.0),
+            imageView.topAnchor.constraint(equalTo: circleView.topAnchor, constant: quarterWidth),
+            imageView.leadingAnchor.constraint(equalTo: circleView.leadingAnchor, constant: quarterWidth),
+            imageView.bottomAnchor.constraint(equalTo: circleView.bottomAnchor, constant: -quarterWidth),
+            imageView.trailingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: -quarterWidth),
+
+            titleLabel.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: 16.0),
             titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24.0),
             titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 28.0),
 
@@ -102,7 +112,7 @@ open class SearchHeaderView: UIView {
             subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
 
             searchBar.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 20.0),
-            searchBar.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: circleView.leadingAnchor),
             searchBar.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
             searchBar.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -24.0),
             searchBar.heightAnchor.constraint(equalToConstant: 32.0)
