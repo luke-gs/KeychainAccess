6fc9088aa71fb4a842c5600489aa046294298f1a
Trent Fitzgibbon <trentf@gridstone.com.au>
2017-12-08 14:56:04 +1100

    Implemented changing status for incident if current

--- Classes/Subspec/CAD/Tasks Details/View Model/IncidentTaskItemViewModel.swift
+++ Classes/Subspec/CAD/Tasks Details/View Model/IncidentTaskItemViewModel.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 open class IncidentTaskItemViewModel: TaskItemViewModel {
-    
+
+    open private(set) var incident: SyncDetailsIncident?
+
     public init(incidentNumber: String, iconImage: UIImage?, iconTintColor: UIColor?, color: UIColor?, statusText: String?, itemName: String?, lastUpdated: String?) {
         super.init(iconImage: iconImage, iconTintColor: iconTintColor, color: color, statusText: statusText, itemName: itemName, lastUpdated: lastUpdated)
         
@@ -29,5 +31,42 @@ open class IncidentTaskItemViewModel: TaskItemViewModel {
                   statusText: resource?.status.title ?? incident.status.rawValue,
                   itemName: [incident.type, incident.resourceCountString].joined(),
                   lastUpdated: incident.lastUpdated.elapsedTimeIntervalForHuman())
+        self.incident = incident
+    }
+
+    override open func didTapTaskStatus(presenter: PopoverPresenter) {
+        if allowChangeResourceStatus() {
+            let callsignStatus = CADStateManager.shared.currentResource?.status ?? .unavailable
+            let sections = [CADFormCollectionSectionViewModel(
+                title: "",
+                items: [
+                    ManageCallsignStatusItemViewModel(.proceeding),
+                    ManageCallsignStatusItemViewModel(.atIncident),
+                    ManageCallsignStatusItemViewModel(.finalise),
+                    ManageCallsignStatusItemViewModel(.inquiries2) ])
+            ]
+            let viewModel = CallsignStatusViewModel(sections: sections, selectedStatus: callsignStatus)
+            let viewController = viewModel.createViewController()
+
+            // Add done button
+            if let dismisser = presenter as? TargetActionDismisser {
+                viewController.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: dismisser, action: #selector(dismissAnimated))
+            }
+
+            // Manually create form sheet to give custom size
+            let nav = PopoverNavigationController(rootViewController: viewController)
+            nav.modalPresentationStyle = .formSheet
+            nav.preferredContentSize = CGSize(width: 540.0, height: 120)
+
+            presenter.present(nav, animated: true, completion: nil)
+        }
+    }
+
+    open func allowChangeResourceStatus() -> Bool {
+        // If this task is the current incident for our booked on resource, allow changing resource state
+        if let incident = incident, incident.identifier == CADStateManager.shared.currentIncident?.identifier {
+            return true
+        }
+        return false
     }
 }
