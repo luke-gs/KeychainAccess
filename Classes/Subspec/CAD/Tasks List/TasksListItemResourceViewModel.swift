6f1e2d8840408639468ddec9a9070495f73b9cdf
Kyle May <kyle.may@gridstone.com.au>
2017-12-12 14:42:26 +1100

    Update task list for resources

--- Classes/Subspec/CAD/Tasks List/TasksListItemResourceViewModel.swift
+++ Classes/Subspec/CAD/Tasks List/TasksListItemResourceViewModel.swift
@@ -14,4 +14,22 @@ public struct TasksListItemResourceViewModel {
     public let statusText: String
     public let tintColor: UIColor?
     public let useBoldStatusText: Bool
+    
+    init(image: UIImage?, resourceTitle: String, statusText: String, tintColor: UIColor?, useBoldStatusText: Bool) {
+        self.image = image
+        self.resourceTitle = resourceTitle
+        self.statusText = statusText
+        self.tintColor = tintColor
+        self.useBoldStatusText = useBoldStatusText
+    }
+    
+    init(with resource: SyncDetailsResource) {
+        let inDuress = resource.status == .duress
+        self.init(image: resource.type.icon,
+                  resourceTitle: [resource.callsign, resource.officerCountString].joined(),
+                  statusText: resource.status.title,
+                  tintColor: inDuress ? .orangeRed : nil,
+            useBoldStatusText: inDuress)
+    }
+    
 }
