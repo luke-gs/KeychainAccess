08add16373c919b3a3b66a0e152a7467666a592a
Trent Fitzgibbon <trentf@gridstone.com.au>
2017-12-15 16:00:14 +1100

    Changed book on request to use sync models

--- Classes/Subspec/CAD/Model/API/Requests/BookOnRequest.swift
+++ Classes/Subspec/CAD/Model/API/Requests/BookOnRequest.swift
@@ -8,47 +8,23 @@
 
 import UIKit
 
-open class BookOnEquipment: Codable {
-
-    /// NOT IN API: The equipment manifest item id
-    open var equipmentId: String!
-
-    /// The count of the item.
-    open var count: String!
-}
-
-open class BookOnOfficer: Codable {
-
-    /// NOT IN API: The officer payrollId.
-    open var payrollId: String!
-
-    /// NOT IN API: The contact number
-    open var phoneNumber: String!
-
-    /// NOT IN API: The licence type manifest item id
-    open var licenceTypeId: String!
-
-    /// NOT IN API: The capabilities of the officer, list of manifest item ids
-    open var capabilities: [String]!
-
-    /// NOT IN API: The optional remarks
-    open var remarks: String!
-}
-
 /// Request object for the call to /shift/bookOn
 open class BookOnRequest: Codable {
 
     /// The callsign of the resource to book on to.
     open var callsign: String!
 
-    /// The shift period expressed in 00-99 hour format.
-    open var shift: String!
+    /// The current shift start time of the resource.
+    open var shiftStart: Date!
+
+    /// The current shift end time of the resource.
+    open var shiftEnd: Date!
 
     /// The list of officers to book on
-    open var officers: [BookOnOfficer]!
+    open var officers: [SyncDetailsOfficer]!
 
     /// The list of equipment items for the resource.
-    open var equipment: [BookOnEquipment]!
+    open var equipment: [SyncDetailsResourceEquipment]!
 
     /// The fleet number for the resource.
     open var fleetNumber: String!
