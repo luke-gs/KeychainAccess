30661df2dc5b8de121ea06fad74e5ba4789ac0d1
Trent Fitzgibbon <trentf@gridstone.com.au>
2017-12-19 10:43:33 +1100

    Replaced sample data with real data for officer search

--- Classes/Subspec/CAD/Book On/Officer Search/OfficerListViewModel.swift
+++ Classes/Subspec/CAD/Book On/Officer Search/OfficerListViewModel.swift
@@ -18,7 +18,7 @@ public class OfficerListViewModel: GenericSearchDefaultViewModel {
     open weak var delegate: OfficerListViewModelDelegate?
     
     public init() {
-        super.init(items: OfficerListViewModel.sampleData) // TODO: Get from network or something
+        super.init(items: viewModelData)
         title = NSLocalizedString("Add Officer", comment: "")
     }
     
@@ -45,18 +45,17 @@ public class OfficerListViewModel: GenericSearchDefaultViewModel {
         return detailsViewModel.createViewController()
     }
     
-    private static var sampleData: [GenericSearchable] {
+    private lazy var viewModelData: [GenericSearchable] = {
         let section = "Recently Used".uppercased()
-        return [
-            OfficerListItemViewModel(firstName: "Herli", lastName: "Halim", rank: "Senior Sergeant", callsign: "800256", section: section, image: nil),
-            OfficerListItemViewModel(firstName: "Bryan", lastName: "Hathaway", rank: "Constable", callsign: "8005823", section: section, image: nil),
-            OfficerListItemViewModel(firstName: "James", lastName: "Aramroongrot", rank: "Constable", callsign: "800851", section: section, image: nil),
-            OfficerListItemViewModel(firstName: "Luke", lastName: "Sammut", rank: "Constable", callsign: "820827", section: section, image: nil),
-            OfficerListItemViewModel(firstName: "Gavin", lastName: "Raison", rank: "Inspector", callsign: "820904", section: section, image: nil),
-            OfficerListItemViewModel(firstName: "Amit", lastName: "Benjamin", rank: "Senior Sergeant", callsign: "800405", section: section, image: nil),
-        ]
-    }
-
+        var result: [GenericSearchable] = []
+        if let syncDetails = CADStateManager.shared.lastSync {
+            for officer in syncDetails.officers {
+                let viewModel = OfficerListItemViewModel(firstName: officer.firstName, lastName: officer.lastName, rank: officer.rank, callsign: officer.payrollId, section: section, image: nil)
+                result.append(viewModel)
+            }
+        }
+        return result
+    }()
 }
 
 extension OfficerListViewModel: OfficerDetailsViewModelDelegate {
