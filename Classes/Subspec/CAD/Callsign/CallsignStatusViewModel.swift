ef62fef8ab8b95be56781b576682f4f3fe537249
Kyle May <kyle.may@gridstone.com.au>
2017-12-20 15:05:38 +1100

    Further genericise CallsignStatusViewController

--- Classes/Subspec/CAD/Callsign/CallsignStatusViewModel.swift
+++ Classes/Subspec/CAD/Callsign/CallsignStatusViewModel.swift
@@ -10,13 +10,13 @@ import UIKit
 import PromiseKit
 
 /// View model for the callsign status screen
-open class CallsignStatusViewModel: CADFormCollectionViewModel<ManageCallsignStatusItemViewModel> {
+open class CallsignStatusViewModel: CADFormCollectionViewModel<ManageCallsignStatusItemViewModel>, IndexPathSelectable {
 
     /// The incident related to the resource status
     open private(set) var incident: SyncDetailsIncident?
 
     /// The currently selected state, can be nil
-    open private(set) var selectedIndexPath: IndexPath?
+    public private(set) var selectedIndexPath: IndexPath?
 
     /// The current status
     open var currentStatus: ResourceStatus? {
---
+++ Classes/Subspec/CAD/Shared/IndexPathSelectable.swift
@@ -0,0 +1,13 @@
+//
+//  IndexPathSelectable.swift
+//  MPOLKit
+//
+//  Created by Kyle May on 20/12/17.
+//  Copyright © 2017 Gridstone. All rights reserved.
+//
+
+import UIKit
+
+public protocol IndexPathSelectable {
+    var selectedIndexPath: IndexPath? { get }
+}
