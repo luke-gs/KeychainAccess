3a16cf2a3ff27808fe39b62a4099448ac797af5d
James Aramroongrot <jamesa@gridstone.com.au>
2018-01-09 10:39:54 +1100

    Make Larry happy

--- MPOL/Presenters/LandingPresenter.swift
+++ MPOL/Presenters/LandingPresenter.swift
@@ -130,16 +130,16 @@ public class LandingPresenter: AppGroupLandingPresenter {
             let entityFormatter = EntitySummaryDisplayFormatter.default
 
             entityFormatter.registerEntityType(Person.self,
-                                               forSummary: .function({ return PersonSummaryDisplayable($0) }),
-                                               andPresentable: .function({ return EntityScreen.entityDetails(entity: $0 as! Entity, delegate: searchViewController ) }))
+                                               forSummary: .function { return PersonSummaryDisplayable($0) },
+                                               andPresentable: .function { return EntityScreen.entityDetails(entity: $0 as! Entity, delegate: searchViewController ) })
 
             entityFormatter.registerEntityType(Vehicle.self,
-                                               forSummary: .function({ return VehicleSummaryDisplayable($0) }),
-                                               andPresentable: .function({ return EntityScreen.entityDetails(entity: $0 as! Entity, delegate: searchViewController ) }))
+                                               forSummary: .function { return VehicleSummaryDisplayable($0) },
+                                               andPresentable: .function { return EntityScreen.entityDetails(entity: $0 as! Entity, delegate: searchViewController ) })
 
             entityFormatter.registerEntityType(Address.self,
-                                               forSummary: .function({ return AddressSummaryDisplayable($0) }),
-                                               andPresentable: .function({ return EntityScreen.entityDetails(entity: $0 as! Entity, delegate: searchViewController) }))
+                                               forSummary: .function { return AddressSummaryDisplayable($0) },
+                                               andPresentable: .function { return EntityScreen.entityDetails(entity: $0 as! Entity, delegate: searchViewController) })
 
             return tabBarController
         }
