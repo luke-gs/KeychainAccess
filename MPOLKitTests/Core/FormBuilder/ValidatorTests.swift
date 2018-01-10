c1a0e44059da2e27044d6b25e8c0365e8a9b39b4
bryan-gs <bryan.hathaway@motorolasolutions.com>
2018-01-10 14:49:04 +1100

    Fix tests

--- MPOLKitTests/Core/FormBuilder/ValidatorTests.swift
+++ MPOLKitTests/Core/FormBuilder/ValidatorTests.swift
@@ -1,5 +1,5 @@
 //
-//  ValidatorTests.swift
+//  FormValidatorTests.swift
 //  MPOLKitTests
 //
 //  Created by KGWH78 on 13/10/17.
@@ -9,11 +9,11 @@
 import XCTest
 @testable import MPOLKit
 
-class ValidatorTests: XCTestCase {
+class FormValidatorTests: XCTestCase {
 
     func testThatItInstantiatesWithDefault() {
         // When
-        let validator = Validator()
+        let validator = FormValidator()
 
         // Then
         XCTAssertEqual(validator.rules.count, 0)
@@ -24,7 +24,7 @@ class ValidatorTests: XCTestCase {
         let rule = ValidatorRule.soft(specification: TrueSpecification(), message: "Hello")
 
         // When
-        let validator = Validator(rules: [rule])
+        let validator = FormValidator(rules: [rule])
 
         // Then
         XCTAssertEqual(validator.rules.count, 1)
@@ -33,7 +33,7 @@ class ValidatorTests: XCTestCase {
     func testThatItAddsRule() {
         // Given
         let rule = ValidatorRule.soft(specification: TrueSpecification(), message: "Hello")
-        let validator = Validator()
+        let validator = FormValidator()
 
         // When
         validator.addRule(rule)
@@ -48,7 +48,7 @@ class ValidatorTests: XCTestCase {
         let soft = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Soft rule message")
         let strict = ValidatorRule.strict(specification: CountSpecification.min(10), message: "Strict rule message")
         let submit = ValidatorRule.submit(specification: CountSpecification.min(20), message: "Submit rule message")
-        let validator = Validator(rules: [soft, strict, submit])
+        let validator = FormValidator(rules: [soft, strict, submit])
 
         // When
         let result = validator.validate("Four", checkHardRule: false, checkSoftRule: true, checkSubmitRule: false)
@@ -63,7 +63,7 @@ class ValidatorTests: XCTestCase {
         let soft = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Soft rule message")
         let strict = ValidatorRule.strict(specification: CountSpecification.min(10), message: "Strict rule message")
         let submit = ValidatorRule.submit(specification: CountSpecification.min(20), message: "Submit rule message")
-        let validator = Validator(rules: [soft, strict, submit])
+        let validator = FormValidator(rules: [soft, strict, submit])
 
         // When
         let result = validator.validate("Four", checkHardRule: true, checkSoftRule: false, checkSubmitRule: false)
@@ -78,7 +78,7 @@ class ValidatorTests: XCTestCase {
         let soft = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Soft rule message")
         let strict = ValidatorRule.strict(specification: CountSpecification.min(10), message: "Strict rule message")
         let submit = ValidatorRule.submit(specification: CountSpecification.min(20), message: "Submit rule message")
-        let validator = Validator(rules: [soft, strict, submit])
+        let validator = FormValidator(rules: [soft, strict, submit])
 
         // When
         let result = validator.validate("Four", checkHardRule: false, checkSoftRule: false, checkSubmitRule: true)
@@ -93,7 +93,7 @@ class ValidatorTests: XCTestCase {
         let soft = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Soft rule message")
         let strict = ValidatorRule.strict(specification: CountSpecification.min(10), message: "Strict rule message")
         let submit = ValidatorRule.submit(specification: CountSpecification.min(20), message: "Submit rule message")
-        let validator = Validator(rules: [soft, strict, submit])
+        let validator = FormValidator(rules: [soft, strict, submit])
 
         // When
         let result = validator.validate("Four", checkHardRule: true, checkSoftRule: true, checkSubmitRule: true)
@@ -108,7 +108,7 @@ class ValidatorTests: XCTestCase {
         let soft = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Soft rule message")
         let strict = ValidatorRule.strict(specification: CountSpecification.min(10), message: "Strict rule message")
         let submit = ValidatorRule.submit(specification: CountSpecification.min(20), message: "Submit rule message")
-        let validator = Validator(rules: [soft, strict, submit])
+        let validator = FormValidator(rules: [soft, strict, submit])
 
         // When
         let result = validator.validate("This is a message that is longer than 2 characters", checkHardRule: false, checkSoftRule: true, checkSubmitRule: false)
@@ -122,7 +122,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let soft1 = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Min 5 characters")
         let soft2 = ValidatorRule.soft(specification: CharacterSetSpecification.alphanumerics, message: "Alphanumeric only")
-        let validator = Validator(rules: [soft1, soft2])
+        let validator = FormValidator(rules: [soft1, soft2])
 
         // When
         let result = validator.validate("Four", checkHardRule: false, checkSoftRule: true, checkSubmitRule: false)
@@ -136,7 +136,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let soft1 = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Min 5 characters")
         let soft2 = ValidatorRule.soft(specification: CharacterSetSpecification.alphanumerics, message: "Alphanumeric only")
-        let validator = Validator(rules: [soft1, soft2])
+        let validator = FormValidator(rules: [soft1, soft2])
 
         // When
         let result = validator.validate("$FourAndAbove", checkHardRule: false, checkSoftRule: true, checkSubmitRule: false)
@@ -150,7 +150,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let item = TextFieldFormItem()
         let soft1 = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Min 5 characters")
-        let validator = Validator(rules: [soft1])
+        let validator = FormValidator(rules: [soft1])
 
         // When
         validator.validateAndUpdateErrorIfNeeded("Four", shouldInstallTimer: false, checkSubmitRule: false, forItem: item)
@@ -165,7 +165,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let item = TextFieldFormItem()
         let soft1 = ValidatorRule.strict(specification: CountSpecification.min(5), message: "Min 5 characters")
-        let validator = Validator(rules: [soft1])
+        let validator = FormValidator(rules: [soft1])
 
         // When
         validator.validateAndUpdateErrorIfNeeded("Four", shouldInstallTimer: false, checkSubmitRule: false, forItem: item)
@@ -180,7 +180,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let item = TextFieldFormItem()
         let soft1 = ValidatorRule.submit(specification: CountSpecification.min(5), message: "Min 5 characters")
-        let validator = Validator(rules: [soft1])
+        let validator = FormValidator(rules: [soft1])
 
         // When
         validator.validateAndUpdateErrorIfNeeded("Four", shouldInstallTimer: false, checkSubmitRule: true, forItem: item)
@@ -195,7 +195,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let item = TextFieldFormItem().focusedText("Existing error message")
         let soft1 = ValidatorRule.submit(specification: CountSpecification.min(5), message: "Min 5 characters")
-        let validator = Validator(rules: [soft1])
+        let validator = FormValidator(rules: [soft1])
 
         // When
         validator.validateAndUpdateErrorIfNeeded("FiveOrMore", shouldInstallTimer: false, checkSubmitRule: true, forItem: item)
@@ -210,7 +210,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let item = TextFieldFormItem().focusedText("Existing error message")
         let soft1 = ValidatorRule.soft(specification: CountSpecification.min(5), message: "Min 5 characters")
-        let validator = Validator(rules: [soft1])
+        let validator = FormValidator(rules: [soft1])
 
         // When
         validator.validateAndUpdateErrorIfNeeded(nil, shouldInstallTimer: false, checkSubmitRule: false, forItem: item)
@@ -225,7 +225,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let item = TextFieldFormItem()
         let soft1 = ValidatorRule.strict(specification: CountSpecification.min(5), message: "Min 5 characters")
-        let validator = Validator(rules: [soft1])
+        let validator = FormValidator(rules: [soft1])
 
         // When
         validator.validateAndUpdateErrorIfNeeded("Four", shouldInstallTimer: true, checkSubmitRule: false, forItem: item)
@@ -248,7 +248,7 @@ class ValidatorTests: XCTestCase {
         // Given
         let item = SubtitleFormItem()
         let soft1 = ValidatorRule.strict(specification: CountSpecification.min(5), message: "Min 5 characters")
-        let validator = Validator(rules: [soft1])
+        let validator = FormValidator(rules: [soft1])
 
         // When
         validator.validateAndUpdateErrorIfNeeded("Four", shouldInstallTimer: true, checkSubmitRule: false, forItem: item)
