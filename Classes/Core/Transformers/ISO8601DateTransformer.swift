6944aab335dd642599e13af2847e0113a77935d3
Luke Sammut <lukes@gridstone.com.au>
2018-01-22 16:19:22 +1100

    Custom ISO transformer that follows actual ISO formats

--- Classes/Core/Transformers/ISO8601DateTransformer.swift
+++ Classes/Core/Transformers/ISO8601DateTransformer.swift
@@ -15,6 +15,27 @@ public class ISO8601DateTransformer: OptionalTransformer {
     private lazy var dateFormatter: ISO8601DateFormatter = {
         return ISO8601DateFormatter()
     }()
+
+    private lazy var noTimeZoneDateFormatter: ISO8601DateFormatter = {
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withYear,
+                                       .withMonth,
+                                       .withDay,
+                                       .withTime,
+                                       .withDashSeparatorInDate,
+                                       .withColonSeparatorInTime,
+                                       .withColonSeparatorInTimeZone]
+        return dateFormatter
+    }()
+
+    private lazy var dateOnlyFormatter: ISO8601DateFormatter = {
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withYear,
+                                       .withMonth,
+                                       .withDay,
+                                       .withDashSeparatorInDate]
+        return dateFormatter
+    }()
     
     // Since the underlying formatter is thread-safe, allow this
     // transformer to be shared.
@@ -22,9 +43,13 @@ public class ISO8601DateTransformer: OptionalTransformer {
     
     public func transform(_ value: String) -> Date? {
         return dateFormatter.date(from: value)
+            ?? noTimeZoneDateFormatter.date(from: value)
+            ?? dateOnlyFormatter.date(from: value)
     }
     
     public func reverse(_ transformedValue: Date) -> String? {
         return dateFormatter.string(from: transformedValue)
+            ?? noTimeZoneDateFormatter.string(from: transformedValue)
+            ?? dateOnlyFormatter.string(from: transformedValue)
     }
 }
