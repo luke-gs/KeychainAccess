97701eac672ea39bac52f089fd295e20cccdb862
Kara Valentine <kara.valentine@gridstone.com.au>
2018-02-01 18:35:17 +1100

    Add new template implementation with better scope partitioning

--- Classes/Core/Templates/Template.swift
+++ Classes/Core/Templates/Template.swift
@@ -6,24 +6,49 @@
 //  Copyright © 2017 Gridstone. All rights reserved.
 //
 
-///  Base template class. Extend this for your own templates!
-///  Overrides equality behaviour for set comparison reasons.
-public class Template: Hashable, Codable {
-    public let id: String
+import Foundation
+
+/// Core template objects that provides uniqueness.
+public final class TemplateCore: Codable {
+    /// A UUID.
+    public let id: UUID
+    /// A timestamp.
     public let timestamp: Date
-
-    public var hashValue: Int {
-        return id.hashValue
+    
+    /// Default initialiser. Provides generated UUID and timestamp (now).
+    /// Use this unless you have a very good reason.
+    public init() {
+        self.id = UUID()
+        self.timestamp = Date()
     }
-
-    // disallow initialising base templates
-    internal init(id: String = UUID().uuidString, timestamp: Date = Date()) {
+    
+    /// Initialiser with id and timestamp parameters.
+    /// Use this in cases where these have already been generated (e.g.
+    /// they are coming from a persistent source).
+    public init(id: UUID, timestamp: Date) {
         self.id = id
         self.timestamp = timestamp
     }
+}
 
-    // overrides default behaviour for set comparisons
-    public static func ==(lhs: Template, rhs: Template) -> Bool {
-        return lhs.id == rhs.id
+/// Base Template protocol used for template data sources.
+/// Codable behaviour is provided for free.
+public protocol Template: Codable, Hashable {
+    /// Core member providing core template functionality through composition.
+    var core: TemplateCore { get }
+}
+
+/// Extension of Template providing implementation of `hashValue`.
+/// `==` must, sadly, be implemented by conformers of Template.
+public extension Template {
+    public var hashValue: Int {
+        return core.id.hashValue
     }
+    
+    // if uncommented:
+    // protocol 'Template' can only be used as a generic constraint because it has Self or associated type requirements
+    
+    //    public static func ==(lhs: Template, rhs: Template) -> Bool {
+    //        return lhs.core.id == rhs.core.id
+    //    }
 }
--- Classes/Core/Templates/TextTemplate.swift
+++ Classes/Core/Templates/TextTemplate.swift
@@ -1,49 +1,33 @@
 //
-//  TextTemplate.swift
+//  TempTemplate.swift
 //  MPOLKit
 //
-//  Created by Kara Valentine on 19/1/18.
+//  Created by Kara Valentine on 1/2/18.
 //  Copyright © 2018 Gridstone. All rights reserved.
 //
 
 import Foundation
 
-/// Sample Template subclass that provides a name, description and value.
-/// Demonstrates one way of implementing encoding/decoding.
 public class TextTemplate: Template {
+    
+    public var core: TemplateCore
     public let name: String
     public let description: String
     public let value: String
-
-    public enum CodingKeys: String, CodingKey {
-        case name
-        case description
-        case value
-        case id
-        case timestamp
+    
+    convenience init(name: String, description: String, value: String) {
+        self.init(name: name, description: description, value: value, core: TemplateCore())
     }
-
-    public init(name: String, description: String, value: String, id: String = UUID().uuidString, timestamp: Date = Date()) {
+    
+    init(name: String, description: String, value: String, core: TemplateCore) {
         self.name = name
         self.description = description
         self.value = value
-        super.init(id: id, timestamp: timestamp)
+        self.core = core
     }
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.description = try container.decode(String.self, forKey: .description)
-        self.value = try container.decode(String.self, forKey: .value)
-        super.init(id: try! container.decode(String.self, forKey: .id), timestamp: try container.decode(Date.self, forKey: .timestamp))
-    }
-
-    override public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-        try container.encode(description, forKey: .description)
-        try container.encode(value, forKey: .value)
-        try container.encode(id, forKey: .id)
-        try container.encode(timestamp, forKey: .timestamp)
+    
+    // having to implement this in each implementation of `Template` is bad
+    public static func ==(lhs: TextTemplate, rhs: TextTemplate) -> Bool {
+        return lhs.core.id == rhs.core.id
     }
 }
