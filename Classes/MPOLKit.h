b616db3822ac4526401cf544f7542c593433b24e
Herli <herli@gridstone.com.au>
2018-02-13 12:09:56 +1100

    Added setNightMode on Map

--- Classes/MPOLKit.h
+++ Classes/MPOLKit.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char MPOLKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MPOLKit/PublicHeader.h>
 
+#import <MPOLKit/MKMapView+MKMapView_NightMode.h>
 
