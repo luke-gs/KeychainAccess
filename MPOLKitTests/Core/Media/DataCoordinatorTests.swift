c8cc7135542c1e9fa1c49d9d94e44b4853e6f95c
James Aramroongrot <jamesa@gridstone.com.au>
2018-02-21 16:54:48 +1100

    Added more tests

--- MPOLKitTests/Core/Media/DataCoordinatorTests.swift
+++ MPOLKitTests/Core/Media/DataCoordinatorTests.swift
@@ -110,7 +110,7 @@ class DataCoordinatorTests: XCTestCase {
         wait(for: [expectation], timeout: 2.0)
     }
 
-    func testThatItHasMoreItems() {
+    func testThatItHasMoreItemsWhenThereIsAKnownResult() {
         // Given
         let store = WritableStore(numbers: [1, 2], additionalItems: [3, 4])
         let provider = DataStoreCoordinator(dataStore: store)
@@ -128,7 +128,37 @@ class DataCoordinatorTests: XCTestCase {
         wait(for: [expectation], timeout: 3.0)
     }
 
-    func testThatItRetrievesMoreItems() {
+    func testThatItDoesNotHaveMoreItemsWhenThereIsAKnownResult() {
+        // Given
+        let store = WritableStore(numbers: [1, 2])
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        let expectation = XCTestExpectation()
+
+        // When
+        provider.retrieveItems().then { _ -> () in
+            XCTAssertFalse(provider.hasMoreItems())
+            expectation.fulfill()
+        }.catch { _ in
+            XCTFail("This should not happen.")
+        }
+
+        wait(for: [expectation], timeout: 3.0)
+    }
+
+    func testThatItDoesNotHaveMoreItemsWhenThereisNoKnownResult() {
+        // Given
+        let store = WritableStore(numbers: [1, 2])
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        // When
+        let hasMoreItems = provider.hasMoreItems()
+
+        // Then
+        XCTAssertFalse(hasMoreItems)
+    }
+
+    func testThatItRetrievesMoreItemsWhenKnownResultHasMoreItems() {
         // Given
         let store = WritableStore(numbers: [1, 2], additionalItems: [3, 4])
         let provider = DataStoreCoordinator(dataStore: store)
@@ -139,6 +169,7 @@ class DataCoordinatorTests: XCTestCase {
         provider.retrieveItems().then { _ in
             return provider.retrieveMoreItems()
         }.then { items -> () in
+            // Then
             XCTAssertEqual(items, [1, 2, 3, 4])
             expectation.fulfill()
         }.catch { _ in
@@ -147,6 +178,160 @@ class DataCoordinatorTests: XCTestCase {
 
         wait(for: [expectation], timeout: 3.0)
     }
+
+    func testThatItFallsBackToRetrieveItemsWhenNoResultIsKnownWhenCallingRetrievesMoreItems() {
+        // Given
+        let store = WritableStore(numbers: [1, 2], additionalItems: [3, 4])
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        let expectation = XCTestExpectation()
+
+        // When
+        provider.retrieveMoreItems().then { items -> () in
+            // Then
+            XCTAssertEqual(items, [1, 2])
+            expectation.fulfill()
+        }.catch { _ in
+            XCTFail("This should not happen.")
+        }
+
+        wait(for: [expectation], timeout: 3.0)
+    }
+
+    func testThatItRecoversFromErrorIfFailedDuringRetrieveItems() {
+        // Given
+        let store = ReadOnlyStore(forceErrorOnRetrieve: true, forceErrorOnRetrieveMoreItems: true)
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        let expectation = XCTestExpectation()
+
+        // When
+        provider.retrieveItems().then { _ in
+            XCTFail("This should not happen.")
+        }.catch { error in
+            // Then
+            if case .error(let error) = provider.state {
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            } else {
+                XCTFail("This should not happen.")
+            }
+        }
+
+        wait(for: [expectation], timeout: 3.0)
+    }
+
+    func testThatItRecoversFromErrorIfFailedDuringRetrieveMoreItems() {
+        // Given
+        let store = ReadOnlyStore(forceErrorOnRetrieve: false, forceErrorOnRetrieveMoreItems: true)
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        let expectation = XCTestExpectation()
+
+        // When
+        provider.retrieveItems().then { _ in
+            return provider.retrieveMoreItems()
+        }.then { _ -> () in
+            XCTFail("This should not happen.")
+        }.catch { error in
+            // Then
+            if case .error(let error) = provider.state {
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            } else {
+                XCTFail("This should not happen.")
+            }
+        }
+
+        wait(for: [expectation], timeout: 3.0)
+    }
+
+    func testThatItHandlesMultipleRetrieveRequestsBeautifully() {
+        // Given
+        let store = ReadOnlyStore(numbers: [1, 2, 3])
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        let expectation = XCTestExpectation()
+
+        // When
+        _ = after(seconds: 0.1).then {
+            XCTAssertEqual(provider.state, .loading)
+        }
+
+        let firstRetrieve = provider.retrieveItems()
+        let secondRetrieve = provider.retrieveItems()
+
+        when(fulfilled: firstRetrieve, secondRetrieve).then { firstResults, secondResults -> () in
+            XCTAssertEqual(firstResults, secondResults)
+            XCTAssertEqual(store.retrieveCount, 1)
+            expectation.fulfill()
+        }.catch { _ in
+            XCTFail("This should not happen.")
+        }
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    func testThatItHandlesMultipleRetrieveMoreItemsRequestsBeautifully() {
+        // Given
+        let store = ReadOnlyStore(numbers: [1, 2, 3])
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        let expectation = XCTestExpectation()
+
+        // When
+        _ = after(seconds: 0.1).then {
+            XCTAssertEqual(provider.state, .loading)
+        }
+
+        _ = provider.retrieveItems().then { items -> Promise<([Int], [Int])>  in
+            let firstRetrieve = provider.retrieveMoreItems()
+            let secondRetrieve = provider.retrieveMoreItems()
+            return when(fulfilled: firstRetrieve, secondRetrieve)
+        }.then { firstResults, secondResults -> () in
+            XCTAssertEqual(firstResults, secondResults)
+            XCTAssertEqual(store.retrieveCount, 2)
+            expectation.fulfill()
+        }.catch { _ in
+            XCTFail("This should not happen.")
+        }
+
+        wait(for: [expectation], timeout: 4.0)
+    }
+
+    func testThatItCancelsPreviusLowPriorityRequestWhenHigherRequestIsMade() {
+        // Given
+        let store = ReadOnlyStore(numbers: [1, 2, 3], additionalItems: [4, 5, 6])
+        let provider = DataStoreCoordinator(dataStore: store)
+
+        let expectation = XCTestExpectation()
+
+        // When
+        _ = provider.retrieveItems().then { _ -> () in
+            XCTAssertTrue(provider.hasMoreItems())
+
+            _ = provider.retrieveMoreItems().then { _ -> () in
+                XCTFail("This should not happen.")
+            }.catch { error in
+                XCTAssertEqual((error as NSError).code, NSURLErrorCancelled)
+            }
+
+            after(seconds: 0.1).then {
+                return provider.retrieveItems()
+            }.then { items -> () in
+                // Then
+                XCTAssertEqual(items, [1, 2, 3])
+                expectation.fulfill()
+            }.catch { _ in
+                XCTFail("This should not happen.")
+            }
+        }
+
+        wait(for: [expectation], timeout: 4.0)
+    }
+
+
+
 
 }
 
@@ -173,16 +358,39 @@ class ReadOnlyStore: ReadableDataStore {
 
     private(set) var additionalItems: [Int]?
 
+    private(set) var retrieveCount: Int = 0
+
+    let forceErrorOnRetrieve: Bool
+
+    let forceErrorOnRetrieveMoreItems: Bool
+
     init(numbers: [Int] = [], additionalItems: [Int]? = nil, delay: Double = 1.0) {
         self.numbers = numbers
         self.delay = delay
         self.additionalItems = additionalItems
+        self.forceErrorOnRetrieve = false
+        self.forceErrorOnRetrieveMoreItems = false
+    }
+
+    init(forceErrorOnRetrieve: Bool, forceErrorOnRetrieveMoreItems: Bool, delay: Double = 1.0) {
+        self.numbers = []
+        self.delay = delay
+        self.additionalItems = nil
+        self.forceErrorOnRetrieve = forceErrorOnRetrieve
+        self.forceErrorOnRetrieveMoreItems = forceErrorOnRetrieveMoreItems
     }
 
     func retrieveItems(withLastKnownResults results: ReadOnlyStore.Result?, cancelToken: PromiseCancellationToken?) -> Promise<ReadOnlyStore.Result> {
         return Promise { fullfill, reject in
+            retrieveCount += 1
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delay, execute: {
-                fullfill(NumberResult(items: results != nil ? self.additionalItems ?? [] : self.numbers, hasMoreItems: results == nil && self.additionalItems?.count ?? 0 > 0))
+                if (self.forceErrorOnRetrieve && results == nil) || (self.forceErrorOnRetrieveMoreItems && results != nil) {
+                    reject(NSError(domain: "com.readonly", code: NSURLErrorDataNotAllowed, userInfo: [:]))
+                } else if cancelToken?.isCancelled == true {
+                    reject(NSError.cancelledError())
+                } else {
+                    fullfill(NumberResult(items: results != nil ? self.additionalItems ?? [] : self.numbers, hasMoreItems: results == nil && self.additionalItems?.count ?? 0 > 0))
+                }
             })
         }
     }
