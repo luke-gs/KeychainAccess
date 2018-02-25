8c1a0be90d81909fa44cc0d32688570731d662e2
Luke Sammut <lukes@gridstone.com.au>
2018-02-26 10:10:11 +1100

    Remove pickable sorting

--- Classes/Subspec/Events/OOTB/Incident/IncidentSearchDataSource.swift
+++ Classes/Subspec/Events/OOTB/Incident/IncidentSearchDataSource.swift
@@ -20,21 +20,18 @@ public class IncidentSearchDataSource: CustomSearchPickerDatasource {
     public var header: CustomisableSearchHeaderView?
     public var allowsMultipleSelection: Bool
 
-    public let sort: PickableSorting
-
     public init(objects: [Pickable],
                 selectedObjects: [Pickable] = [],
                 title: String? = "Incidents",
                 allowsMultipleSelection: Bool = true,
-                configuration: SearchHeaderConfiguration? = nil,
-                sort: PickableSorting = .none) {
+                configuration: SearchHeaderConfiguration? = nil) {
 
-        self.objects = objects.sorted(by: sort.function())
+        self.objects = objects.sorted(using: [SortDescriptor<Pickable>(ascending: true, key: {$0.title }),
+                                              SortDescriptor<Pickable>(ascending: true, key: {$0.subtitle })])
         self.selectedObjects = selectedObjects
         self.title = title
         self.allowsMultipleSelection = allowsMultipleSelection
         self.headerConfiguration = configuration
-        self.sort = sort
     }
 
     public func allowsSelection(of object: Pickable) -> Bool {
