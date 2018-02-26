bbfce7e5072c7239e4149998e2caad8586b2c0f6
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2018-02-26 11:26:40 +1100

    Create basis on incidents.

---
+++ MPOL/MPOL/Incidents/IncidentBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  IncidentBuilder.swift
+//  MPOL
+//
+//  Created by Pavel Boryseiko on 7/2/18.
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import UIKit
+import MPOLKit
+
+public class IncidentBuilder: IncidentBuilding {
+    public func createIncident(for type: IncidentType) -> (incident: Incident, displayable: EventListDisplayable) {
+        fatalError()
+    }
+    
+    public init() { }
+}
---
+++ MPOL/MPOL/Incidents/IncidentScreenBuilder.swift
@@ -0,0 +1,41 @@
+//
+//  IncidentScreenBuilder.swift
+//  MPOLKit
+//
+//  Created by Pavel Boryseiko on 31/1/18.
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import MPOLKit
+
+public class IncidentScreenBuilder: IncidentScreenBuilding {
+
+    public func viewControllers(for reportables: [Reportable]) -> [UIViewController] {
+        var viewControllers = [UIViewController]()
+
+        for report in reportables {
+            if let viewController = viewController(for: report) {
+                viewControllers.append(viewController)
+            }
+        }
+
+        return viewControllers
+    }
+
+    private func viewController(for report: Reportable) -> UIViewController? {
+        switch report {
+        case let report as DefaultDateTimeReport:
+            return DefaultEventDateTimeViewController(report: report)
+        case let report as DefaultLocationReport:
+            return DefaultEventLocationViewController(report: report)
+        case let report as DefaultNotesPhotosReport:
+            return DefaultEventNotesPhotosViewController(report: report)
+        default:
+            return nil
+        }
+    }
+
+    public init() { }
+}
+
+
