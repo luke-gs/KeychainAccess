fb7d85d4aa8a272a5603f5e04157bf4d345855d1
Megan Efron <megan.efron@gridstone.com.au>
2018-02-27 16:07:19 +1100

    Adding separator to large text header item

--- Classes/Subspec/CAD/ActivityLog/List/LargeTextHeaderCollectionViewCell.swift
+++ Classes/Subspec/CAD/ActivityLog/List/LargeTextHeaderCollectionViewCell.swift
@@ -11,6 +11,7 @@ import UIKit
 /// Collection view cell for a header with large text
 open class LargeTextHeaderCollectionViewCell: UICollectionViewCell, DefaultReusable {
     public let titleLabel = UILabel()
+    public let separatorView = UIView(frame: .zero)
     
     public static let minimumHeight: CGFloat = 72
 
@@ -21,6 +22,10 @@ open class LargeTextHeaderCollectionViewCell: UICollectionViewCell, DefaultReusa
         titleLabel.textColor = .primaryGray
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(titleLabel)
+        
+        separatorView.backgroundColor = iOSStandardSeparatorColor
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(separatorView)
 
         // Default margins that can be overridden
         contentView.layoutMargins = UIEdgeInsets(top: 16, left: 24, bottom: 24, right: 24)
@@ -29,7 +34,12 @@ open class LargeTextHeaderCollectionViewCell: UICollectionViewCell, DefaultReusa
             titleLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).withPriority(.almostRequired),
             titleLabel.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: separatorView.topAnchor),
+            
+            separatorView.heightAnchor.constraint(equalToConstant: 1.0),
+            separatorView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            separatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            separatorView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor)
         ])
     }
     
