3c4abd7ac9fa2b71b603df7f68fbe31c59ea414f
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2018-03-01 16:31:23 +1100

    Add validation into the incidentlistVC

--- Classes/Subspec/Events/OOTB/Incident/IncidentListViewController.swift
+++ Classes/Subspec/Events/OOTB/Incident/IncidentListViewController.swift
@@ -29,7 +29,7 @@ open class IncidentListViewController: FormBuilderViewController, EvaluationObse
         sidebarItem.regularTitle = "Incidents"
         sidebarItem.compactTitle = "Incidents"
         sidebarItem.image = AssetManager.shared.image(forKey: AssetManager.ImageKey.document)!
-        sidebarItem.color = .red
+        sidebarItem.color = (viewModel.report?.evaluator.isComplete ?? false) ? .red : .midGreen
     }
 
     public required convenience init?(coder aDecoder: NSCoder) {
