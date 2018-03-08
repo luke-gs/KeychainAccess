b625c040b9139c5cd78696ae32b434028f66221e
Kyle May <kyle.may@gridstone.com.au>
2018-03-09 10:46:12 +1100

    Move API manager

---
+++ ClientKit/ClientKit/CAD/APIManager+CAD.swift
@@ -0,0 +1,40 @@
+//
+//  APIManager+CAD.swift
+//  ClientKit
+//
+//  Created by Trent Fitzgibbon on 14/11/17.
+//  Copyright © 2017 Gridstone. All rights reserved.
+//
+
+import UIKit
+/// Extension for APIManager for CAD specific network requests
+extension APIManager: CADAPIManagerType {
+    
+    /// Fetch details about an officer by username
+    open func cadOfficerByUsername(username: String) -> Promise<CADOfficerDetailsResponse> {
+        
+        let path = "/cad/officer/username/{username}"
+        let parameters = ["username": username]
+        
+        let networkRequest = try! NetworkRequest(pathTemplate: path, parameters: parameters)
+        return firstly {
+            return try! performRequest(networkRequest)
+            }.then { (data, response) in
+                return try JSONDecoder.decode(data, to: CADOfficerDetailsResponse.self)
+        }
+    }
+    
+    /// Fetch all sync details
+    open func cadSyncDetails(request: CADSyncRequest) -> Promise<CADSyncResponse> {
+        // TODO: convert request to params
+        let path = "/cad/sync/details"
+        
+        let networkRequest = try! NetworkRequest(pathTemplate: path, parameters: [:], method: .post)
+        return firstly {
+            return try! performRequest(networkRequest)
+            }.then { (data, response) in
+                return try JSONDecoder.decode(data, to: CADSyncResponse.self)
+        }
+    }
+}
+
