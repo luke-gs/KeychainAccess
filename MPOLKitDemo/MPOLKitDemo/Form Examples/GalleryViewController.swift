7f77213d8eb0a67c6ab1e718d67106c6ede3ff7d
James Aramroongrot <jamesa@gridstone.com.au>
2018-03-10 19:47:48 -0800

    Update example

--- MPOLKitDemo/MPOLKitDemo/Form Examples/GalleryViewController.swift
+++ MPOLKitDemo/MPOLKitDemo/Form Examples/GalleryViewController.swift
@@ -72,37 +72,58 @@ class GalleryViewController: FormBuilderViewController {
         builder += mediaItem
     }
 
+    // MARK: - Example for VicRoad
+    
     func meganGallery(builder: FormBuilder) {
-        let meganStore = DataStoreCoordinator(dataStore: MeganMediaStore())
-
-        let filter = FilterValueDescriptor<Media, String>(key: { $0.title }, values: ["Herli"])
-        
-        let gallery = MediaGalleryCoordinatorViewModel(storeCoordinator: meganStore)
-        gallery.filterDescriptors = [filter]
-        
-        let magicItem = UIBarButtonItem(title: "MAGIC", style: .plain, target: self, action: #selector(magicTapped))
-        
-        let handler = MediaPreviewHandler(allowEditing: false, additionalBarButtons: [magicItem])
-
-        let mediaItem = MediaFormItem()
-            .dataSource(gallery)
-            .delegate(handler)
-
-        if let viewController = UIApplication.shared.keyWindow?.rootViewController {
-            mediaItem.previewingController(viewController)
-        }
-
-        builder += HeaderFormItem(text: "MEGAN GALLERY").actionButton(title: "VIEW", handler: { button in
-            if let viewController = mediaItem.delegate?.viewControllerForGalleryViewModel(gallery) {
-                self.present(viewController, animated: true, completion: nil)
-            }
-        })
-
-        builder += mediaItem
+        builder += HeaderFormItem(text: "MEGAN GALLERY")
+        builder += SubtitleFormItem(title: "Sample Gallery", subtitle: "Tap to open", image: nil, style: .default).onSelection({ [weak self] _ in
+            guard let `self` = self else { return }
+            
+            self.present(self.galleryViewController, animated: true, completion: nil)
+        }).width(.column(1))
     }
-
-    @objc func magicTapped() {
+    
+    lazy var meganGalleryViewModel: MediaGalleryCoordinatorViewModel<MeganMediaStore> = {
+        let meganStore = DataStoreCoordinator(dataStore: MeganMediaStore())
+        let gallery = MediaGalleryCoordinatorViewModel(storeCoordinator: meganStore)
+        return gallery
+    }()
+    
+    lazy var galleryViewController: UIViewController = {
+        let gallery = MediaGalleryViewController(viewModel: meganGalleryViewModel, initialPreview: nil, pickerSources: [])
+        gallery.allowEditing = false
+        gallery.additionalBarButtonItems = [UIBarButtonItem(title: "Magic", style: .plain, target: self, action: #selector(GalleryViewController.magicTapped(_:)))]
         
+        return UINavigationController(rootViewController: gallery)
+    }()
+    
+    var selectedMagicOption: MagicViewController.FilterOption = .all
+    
+    @objc func magicTapped(_ barItem: UIBarButtonItem) {
+        let magic = MagicViewController(handler: { [unowned self] selected in
+            self.galleryViewController.dismiss(animated: true, completion: nil)
+            
+            switch selected {
+            case .all:
+                self.meganGalleryViewModel.filterDescriptors = nil
+            case .sensitiveOnly:
+                self.meganGalleryViewModel.filterDescriptors = [FilterValueDescriptor(key: { $0.sensitive }, values: [true])]
+            case .nonSensitiveOnly:
+                self.meganGalleryViewModel.filterDescriptors = [FilterValueDescriptor(key: { $0.sensitive }, values: [false])]
+            case .peopleOnly:
+                self.meganGalleryViewModel.filterDescriptors = [FilterValueDescriptor(key: { $0.title }, values: ["Person"])]
+            }
+            
+            self.selectedMagicOption = selected
+        })
+        
+        magic.selectedOption = selectedMagicOption
+        
+        let navigationController = UINavigationController(rootViewController: magic)
+        navigationController.modalPresentationStyle = .popover
+        navigationController.popoverPresentationController?.barButtonItem = barItem
+        
+        galleryViewController.present(navigationController, animated: true, completion: nil)
     }
     
 }
@@ -139,10 +160,10 @@ class MeganMediaStore: WritableDataStore {
             return Promise { fullfill, reject in
                 DispatchQueue.global().async {
                     let media = images.map { image -> Media in
-                        let imageRef = UIImageJPEGRepresentation(image, 0.5)
+                        let imageRef = UIImageJPEGRepresentation(image.0, 0.5)
                         let imageFilePath = self.cacheDirectory.appendingPathComponent("\(UUID().uuidString).jpg")
                         try! imageRef!.write(to: imageFilePath)
-                        return (Media(url: imageFilePath, type: .photo, title: "Herli---"))
+                        return (Media(url: imageFilePath, type: .photo, title: image.1))
                     }
 
                     fullfill(MeganStoreResult(items: media, nextPageID: pageID))
@@ -166,11 +187,11 @@ class MeganMediaStore: WritableDataStore {
 
     // MARK: - Fake it to win it
 
-    private let fakeImageKeys: [AssetManager.ImageKey] = [
-        .entityCarLarge, .entityBoat, .entityTruckLarge, .entityPerson, .entityBuilding, .entityTrailerLarge, .entityMotorbikeLarge
+    private let fakeImageKeys: [(AssetManager.ImageKey, String)] = [
+        (.entityCarLarge, "Car"), (.entityBoat, "Boat"), (.entityTruckLarge, "Truck"), (.entityPerson, "Person"), (.entityBuilding, "Building"), (.entityTrailerLarge, "Trailer"), (.entityMotorbikeLarge, "Motorbike")
     ]
 
-    private func connectToBackendToDownloadMedia(page: Int) -> Promise<([UIImage], Int?)> {
+    private func connectToBackendToDownloadMedia(page: Int) -> Promise<([(UIImage, String)], Int?)> {
         return Promise { fullfill, reject in
             DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 4.0) {
                 // This backend only sends 2 results back at a time.
@@ -181,7 +202,7 @@ class MeganMediaStore: WritableDataStore {
                 let pageId: Int? = (endIndex == self.fakeImageKeys.count) ? nil : ((page) + 1)
 
                 let images = self.fakeImageKeys[beginIndex..<endIndex].flatMap {
-                    return AssetManager.shared.image(forKey: $0)
+                    return (AssetManager.shared.image(forKey: $0.0)!, $0.1)
                 }
 
                 fullfill((images, pageId))
@@ -189,4 +210,48 @@ class MeganMediaStore: WritableDataStore {
         }
     }
 
+}
+
+
+class MagicViewController: FormBuilderViewController {
+    
+    enum FilterOption: String {
+        case all = "Show All"
+        case sensitiveOnly = "Show sensitive only"
+        case nonSensitiveOnly = "Show non senstive only"
+        case peopleOnly = "Show people only"
+    }
+    
+    let availableOptions: [FilterOption]
+    
+    let handler: (FilterOption) -> ()
+    
+    var selectedOption: FilterOption = .all
+    
+    init(options: [FilterOption] = [.all, .sensitiveOnly, .nonSensitiveOnly, .peopleOnly], handler: @escaping (FilterOption) -> ()) {
+        self.availableOptions = options
+        self.handler = handler
+        
+        super.init()
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func construct(builder: FormBuilder) {
+        builder.title = "Magic"
+        
+        builder += availableOptions.map { option -> SubtitleFormItem in
+            let item = SubtitleFormItem()
+                .title(option.rawValue)
+                .onSelection({ [weak self] _ in
+                    self?.selectedOption = option
+                    self?.handler(option)
+                })
+                .accessory(selectedOption == option ? ItemAccessory.checkmark : nil)
+            return item
+        }
+    }
+    
 }
