e06e00cd996d85fb073a9eb3f8493b40fdc25a36
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-03-27 10:49:43 +1100

    Changed TaskDetailsOverviewViewController to use DraggableCardView

--- Classes/Subspec/CAD/Tasks/Details/Shared/Overview/TaskDetailsOverviewViewController.swift
+++ Classes/Subspec/CAD/Tasks/Details/Shared/Overview/TaskDetailsOverviewViewController.swift
@@ -12,7 +12,7 @@ open class TaskDetailsOverviewViewController: UIViewController {
 
     open var mapViewController: MapViewController?
     open var formViewController: FormBuilderViewController!
-    open var scrollView: UIScrollView!
+    open var cardView: DraggableCardView!
     
     open let viewModel: TaskDetailsOverviewViewModel
     
@@ -40,70 +40,74 @@ open class TaskDetailsOverviewViewController: UIViewController {
         edgesForExtendedLayout = []
         
         view.backgroundColor = .white
-        
-        scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(scrollView)
 
         if let mapViewModel = viewModel.mapViewModel() {
             let mapViewController = mapViewModel.createViewController()
-            addChildViewController(mapViewController, toView: scrollView)
+            addChildViewController(mapViewController, toView: view)
             mapViewController.showsMapButtons = false
             mapViewController.mapView.isZoomEnabled = false
             mapViewController.mapView.isPitchEnabled = false
             mapViewController.mapView.isRotateEnabled = false
             mapViewController.mapView.isScrollEnabled = false
-            mapViewController.view.translatesAutoresizingMaskIntoConstraints = false
             self.mapViewController = mapViewController
         }
+
+        cardView = DraggableCardView(frame: .zero)
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cardView)
+
         formViewController = viewModel.createFormViewController()
-        addChildViewController(formViewController, toView: scrollView)
-        formViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        addChildViewController(formViewController, toView: cardView.contentView)
         formViewController.collectionView?.isScrollEnabled = false
     }
     
     /// Activates view constraints
     private func setupConstraints() {
-        guard let collectionView = formViewController.collectionView else { return }
+        guard let formCollectionView = formViewController.collectionView else { return }
+        guard let formView = formViewController.view else { return }
+        let mapView = mapViewController?.view
         
         // Change collection view to not use autoresizing mask constraints so it uses intrinsic content height
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        formCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        formView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaOrFallbackTopAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            formCollectionView.topAnchor.constraint(equalTo: formView.topAnchor),
+            formCollectionView.leadingAnchor.constraint(equalTo: formView.leadingAnchor),
+            formCollectionView.trailingAnchor.constraint(equalTo: formView.trailingAnchor),
+            formCollectionView.bottomAnchor.constraint(equalTo: formView.bottomAnchor),
 
-            collectionView.topAnchor.constraint(equalTo: formViewController.view.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: formViewController.view.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: formViewController.view.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: formViewController.view.bottomAnchor),
+            formView.topAnchor.constraint(equalTo: cardView.contentView.topAnchor),
+            formView.leadingAnchor.constraint(equalTo: cardView.contentView.leadingAnchor),
+            formView.trailingAnchor.constraint(equalTo: cardView.contentView.trailingAnchor),
+            formView.bottomAnchor.constraint(equalTo: cardView.contentView.bottomAnchor),
         ])
 
-        if let mapViewController = mapViewController {
+        if let mapView = mapView {
             // Show both map and form
-            NSLayoutConstraint.activate([
-                mapViewController.view.topAnchor.constraint(equalTo: scrollView.topAnchor),
-                mapViewController.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-                mapViewController.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-                mapViewController.view.heightAnchor.constraint(greaterThanOrEqualToConstant: 280),
-                mapViewController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            mapView.translatesAutoresizingMaskIntoConstraints = false
 
-                formViewController.view.topAnchor.constraint(equalTo: mapViewController.view.bottomAnchor),
-                formViewController.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-                formViewController.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-                formViewController.view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-                formViewController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            NSLayoutConstraint.activate([
+                mapView.topAnchor.constraint(equalTo: view.safeAreaOrFallbackTopAnchor),
+                mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                mapView.widthAnchor.constraint(equalTo: view.widthAnchor),
+                mapView.heightAnchor.constraint(equalToConstant: 280),
+
+                cardView.topAnchor.constraint(equalTo: mapView.bottomAnchor),
+                cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                cardView.widthAnchor.constraint(equalTo: view.widthAnchor),
             ])
         } else {
             // Show just form
             NSLayoutConstraint.activate([
-                formViewController.view.topAnchor.constraint(equalTo: scrollView.topAnchor),
-                formViewController.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-                formViewController.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-                formViewController.view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-                formViewController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+                cardView.topAnchor.constraint(equalTo: view.safeAreaOrFallbackTopAnchor),
+                cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                cardView.widthAnchor.constraint(equalTo: view.widthAnchor),
             ])
         }
     }
