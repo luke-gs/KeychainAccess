90919e20c6d04c65901eaea6d468a9778cb59317
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-04-13 09:53:34 +1000

    Improved comment

--- Classes/Core/Notifications/NotificationHandler.swift
+++ Classes/Core/Notifications/NotificationHandler.swift
@@ -10,25 +10,13 @@ import UIKit
 import UserNotifications
 import PromiseKit
 
-/// Enum for the action a user made on a notification
-public enum NotificationAction {
-
-    // User tapped notification, opening application
-    case openApp
-
-    // User dismissed notification (only delivered if category object configured with the customDismissAction option)
-    case dismissed
-
-    // User selected a specific custom action on the notification
-    case customAction(actionIdentifier: String)
-}
-
 /// Protocol for the handler that will actually process incoming notifications
 /// Note: To be implemented in client app if using push notifications
 public protocol NotificationHandler: class {
 
-    // Note: If a silent notification ("content-available": 1) is sent together with an alert key,
-    // both handleSilentNotification and handleForegroundNotification will be called
+    // Note: Normally only a single handleX method will be called on receipt of a push notificaiton. BUT, if a silent
+    // notification, "content-available": 1, is sent together with an alert text, both handleSilentNotification and
+    // handleForegroundNotification will be called.
 
     /// Handle a notification while app is in the foreground and return the presentation options
     func handleForegroundNotification(_ notification: UNNotification) -> Promise<UNNotificationPresentationOptions>
@@ -43,3 +31,15 @@ public protocol NotificationHandler: class {
     func sourceAppForNotificationRegistration() -> String
 }
 
+/// Enum for the action a user made on a notification
+public enum NotificationAction {
+
+    // User tapped notification, opening application
+    case openApp
+
+    // User dismissed notification (only delivered if category object configured with the customDismissAction option)
+    case dismissed
+
+    // User selected a specific custom action on the notification
+    case customAction(actionIdentifier: String)
+}
