d119e97a7fdf066f5d569726bbe68340c38efba6
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-04-17 08:23:46 +1000

    Updated officers to use employeeNumber

--- CAD/CAD/Demo/DemoAPIManager.swift
+++ CAD/CAD/Demo/DemoAPIManager.swift
@@ -30,10 +30,12 @@ open class DemoAPIManager: CADAPIManagerType {
     }
 
     public func cadBookOn(with request: CADBookOnRequestType) -> Promise<Void> {
+        print("\(request.parameters.asLogString())")
         return after(seconds: 1).done {}
     }
 
     public func cadBookOff(with request: CADBookOffRequestType) -> Promise<Void> {
+        print("\(request.parameters.asLogString())")
         return after(seconds: 1).done {}
     }
 
