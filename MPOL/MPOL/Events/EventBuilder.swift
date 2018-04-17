b1190ee3afe7f165990fed003dba5c081c35c860
campbell-graham <35436805+campbell-graham@users.noreply.github.com>
2018-04-17 13:48:32 +1000

    [MPOLA-1075] - Entities List (main) (#275)
    * ViewController and Report classes added for entities
    
    * No Content state for entity view controller complete
    
    Contains a basic view model which contains a basic report
    
    * Wording change
    
    * File name change
    
    * Renamed file/classes for consistency
    
    * Fixed comment that should have been TODO
    
    * Fixed comment to match class name
    
    * Fixed another comment to match class name
    
    * Tidied up titles, set using view controller title

--- MPOL/MPOL/Events/EventBuilder.swift
+++ MPOL/MPOL/Events/EventBuilder.swift
@@ -18,6 +18,7 @@ public class EventBuilder: EventBuilding {
         event.add(report: DefaultLocationReport(event: event))
         event.add(report: OfficerListReport(event: event))
         event.add(report: IncidentListReport(event: event))
+        event.add(report: EventEntitiesListReport(event: event))
         event.add(report: DefaultNotesAssetsReport(event: event))
 
         let displayable = EventListDisplayable(title: "No incident selected",
