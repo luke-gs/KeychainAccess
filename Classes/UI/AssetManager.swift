bdfa9f767638cba74d0cfe33c04904b901e487dc
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2018-04-18 16:37:27 +1000

    Add relationship icon to assets

--- Classes/UI/AssetManager.swift
+++ Classes/UI/AssetManager.swift
@@ -263,6 +263,7 @@ extension AssetManager {
         public static let iconFolder   = ImageKey("iconFolder")
         public static let iconPencil   = ImageKey("iconPencil")
         public static let iconDocument = ImageKey("iconDocument")
+        public static let iconRelationships = ImageKey("iconGeneralRelationships")
 
         // Dialog images
         public static let dialogAlert = ImageKey("dialogAlert")
