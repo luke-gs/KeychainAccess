40faf5d1e52007c4993820841fdd4646091f5966
christian-gs <35436813+christian-gs@users.noreply.github.com>
2018-05-02 14:56:56 +1000

    [MPOLA-1073/1074] - Traffic Infringement Service (Main & Details) (#304)
    * moved all things from traffic infringement service PR
    
    * update incident sidebar header on completion/uncompletion
    
    * cleaned up entityVC’s construct code
    
    * Form items changed to use .required()
    
    * added comment
    
    * added empty state to service
    
    * fixed issues with decoding contacts
    
    * changed service report to be in empty state when no people are added
    
    * store variables on the report instead of the viewModel (before the values wernt being persisted)
    
    * lint warnings
    
    * changed service report to require a person
    
    * pr fixes
    
    * changed paramater name, utilised evaluator.isComplete
    
    * changed array count check to use isEmpty

--- MPOL/MPOL/Incidents/Traffic Infringement/Service/TrafficInfringementServiceViewController.swift
+++ MPOL/MPOL/Incidents/Traffic Infringement/Service/TrafficInfringementServiceViewController.swift
@@ -7,31 +7,30 @@
 
 import UIKit
 import MPOLKit
+import ClientKit
 
 fileprivate extension EvaluatorKey {
     static let viewed = EvaluatorKey("viewed")
 }
 open class TrafficInfringementServiceViewController: FormBuilderViewController, EvaluationObserverable {
 
-    weak var report: TrafficInfringementServiceReport?
+    let viewModel: TrafficInfringementServiceViewModel
 
-    public init(report: Reportable?) {
-        self.report = report as? TrafficInfringementServiceReport
+    public init(viewModel: TrafficInfringementServiceViewModel) {
+        self.viewModel = viewModel
         super.init()
-        report?.evaluator.addObserver(self)
+        viewModel.report.evaluator.addObserver(self)
 
-        title = "Service"
+        self.title = viewModel.title
 
         sidebarItem.regularTitle = title
         sidebarItem.compactTitle = title
         sidebarItem.image = AssetManager.shared.image(forKey: AssetManager.ImageKey.service)!
-        sidebarItem.color = (report?.evaluator.isComplete ?? false) ? .midGreen : .red
+        sidebarItem.color = viewModel.tabColor
 
         loadingManager.noContentView.titleLabel.text = "No Entities Added"
         loadingManager.noContentView.subtitleLabel.text = "Service requires a person or organisation"
         loadingManager.noContentView.imageView.image = AssetManager.shared.image(forKey: AssetManager.ImageKey.dialogAlert)
-
-        loadingManager.state = .noContent
     }
 
     public required convenience init?(coder aDecoder: NSCoder) {
@@ -40,18 +39,73 @@ open class TrafficInfringementServiceViewController: FormBuilderViewController, 
 
     open override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        report?.viewed = true
+        loadingManager.state = viewModel.currentLoadingManagerState
+        self.viewModel.updateValidation()
+        reloadForm()
     }
 
     override open func construct(builder: FormBuilder) {
         builder.title = title
         builder.forceLinearLayout = true
 
-        builder += HeaderFormItem(text: "GENERAL")
-        builder += HeaderFormItem(text: "SUMMARY / NOTES")
+        builder += HeaderFormItem(text: "Service Type")
+        builder += OptionDisplayableFormItem(options: [ServiceType.email, ServiceType.mms, ServiceType.post])
+            .selectedIndex(viewModel.report.selectedServiceType?.rawValue)
+            .selectionHandler({ (index) in
+                if let serviceType = ServiceType(rawValue: index) {
+                    self.viewModel.selectedServiceType(type: serviceType)
+                }
+                self.viewModel.updateValidation()
+                self.reloadForm()
+            })
+
+        if let selectedServiceType = viewModel.report.selectedServiceType {
+            builder += HeaderFormItem(text: "Details")
+            
+            switch selectedServiceType {
+            case .email:
+
+                builder += DropDownFormItem(title: "Email Address")
+                    .required()
+                    .width(.column(1))
+                    .options(viewModel.allEmails)
+                    .selectedValue(viewModel.selectedValues(for: selectedServiceType))
+                    .onValueChanged({ (value) in
+                        guard let value = value else { return }
+                        self.viewModel.setSelectedEmail(emails: value)
+                        self.viewModel.updateValidation()
+                    })
+
+            case .mms:
+
+                builder += DropDownFormItem(title: "Phone Number")
+                    .required()
+                    .options(viewModel.allMobiles)
+                    .selectedValue(viewModel.selectedValues(for: selectedServiceType))
+                    .onValueChanged({ (value) in
+                        guard let value = value else { return }
+                        self.viewModel.setSelectedMobile(mobiles: value)
+                        self.viewModel.updateValidation()
+                    })
+                    .width(.column(1))
+            default:
+                break
+            }
+
+            builder += DropDownFormItem(title: "Residential Address")
+                .required()
+                .width(.column(1))
+                .options(viewModel.allFullAddresses)
+                .selectedValue(viewModel.selectedValues(for: ServiceType.post))
+                .onValueChanged({ (value) in
+                    guard let value = value else { return }
+                    self.viewModel.setSelectedAddress(addresses: value)
+                    self.viewModel.updateValidation()
+                })
+        }
     }
 
     public func evaluationChanged(in evaluator: Evaluator, for key: EvaluatorKey, evaluationState: Bool) {
-        sidebarItem.color = evaluator.isComplete == true ? .midGreen : .red
+        sidebarItem.color = viewModel.tabColor
     }
 }
