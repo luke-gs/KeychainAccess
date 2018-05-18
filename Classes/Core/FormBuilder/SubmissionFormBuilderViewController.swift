53e583aa755680c0f086890775557891af866af8
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-05-18 10:04:26 +1000

    Added base class for submission forms

---
+++ Classes/Core/FormBuilder/SubmissionFormBuilderViewController.swift
@@ -0,0 +1,96 @@
+//
+//  SubmissionFormBuilderViewController.swift
+//  MPOLKit
+//
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import UIKit
+import PromiseKit
+
+/// A `FormBuilderViewController` subclass that provides OOTB behaviour for modal forms that submit data.
+open class SubmissionFormBuilderViewController: FormBuilderViewController {
+
+    /// Optional title and subtitle display in navigation bar
+    open var navTitles: (title: String, subtitle: String)?
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateNavTitle()
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancelButton(_:)))
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDoneButton(_:)))
+
+        // Set initial background color (this may change in wantsTransparentBackground)
+        let theme = ThemeManager.shared.theme(for: userInterfaceStyle)
+        view.backgroundColor = theme.color(forKey: .background)!
+
+        // Set default error text and retry handling
+        self.loadingManager.errorView.titleLabel.text = NSLocalizedString("Failed to Submit", comment: "")
+        self.loadingManager.errorView.actionButton.setTitle(NSLocalizedString("Try Again", comment: ""), for: .normal)
+        self.loadingManager.errorView.actionButton.addTarget(self, action: #selector(self.didTapDoneButton), for: .touchUpInside)
+    }
+
+    open func updateNavTitle() {
+        if let navTitles = navTitles {
+            setTitleView(title: navTitles.title, subtitle: navTitles.subtitle)
+        }
+    }
+
+    override open func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateNavTitle()
+    }
+
+    /// We need to override viewDidLayoutSubviews as well as willTransition to update nav title
+    /// due to the behaviour of PopoverNavigationController
+    override open func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateNavTitle()
+    }
+
+    override open func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        coordinator.animate(alongsideTransition: { (context) in
+            self.updateNavTitle()
+        }, completion: nil)
+    }
+
+    @objc open func didTapCancelButton(_ button: UIBarButtonItem) {
+        dismissAnimated()
+    }
+
+    @objc open func didTapDoneButton(_ button: UIBarButtonItem) {
+        setLoadingState(.loading)
+        firstly {
+            return performSubmit()
+        }.done { [weak self] in
+            guard let `self` = self else { return }
+            self.setLoadingState(.loaded)
+            self.dismissAnimated()
+        }.catch { [weak self] error in
+            guard let `self` = self else { return }
+            self.loadingManager.errorView.subtitleLabel.text = error.localizedDescription
+            self.setLoadingState(.error)
+        }
+    }
+
+    /// Update form based on loading state
+    open func setLoadingState(_ state: LoadingStateManager.State) {
+        loadingManager.state = state
+
+        // Enable cancel if not submitting
+        navigationItem.leftBarButtonItem?.isEnabled = state == .loaded || state == .error
+
+        // Enable submit if not submitting or error
+        navigationItem.rightBarButtonItem?.isEnabled = state == .loaded
+    }
+
+    /// Perform actual submit logic, override in subclass
+    open func performSubmit() -> Promise<Void> {
+        MPLRequiresConcreteImplementation()
+    }
+
+}
