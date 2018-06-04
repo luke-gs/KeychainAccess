e65b7a5ecd524378e33420dd7a73d3c6ed59787c
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2018-06-04 18:48:35 +1000

    Weakify and LargeHeader-ify

--- Classes/Subspec/Events/Property/Internal/PropertyDetailsPlugins.swift
+++ Classes/Subspec/Events/Property/Internal/PropertyDetailsPlugins.swift
@@ -25,31 +25,33 @@ internal struct AddPropertyGeneralPlugin: FormBuilderPlugin {
 
 internal struct AddPropertyGeneralPluginDecorator: FormBuilderPluginDecorator {
 
-    var viewModel: PropertyDetailsViewModel
-    var delegate: AddPropertyDelegate
+    let viewModel: Weak<PropertyDetailsViewModel>
+    let delegate: AddPropertyDelegate
 
     init(viewModel: PropertyDetailsViewModel, delegate: AddPropertyDelegate) {
-        self.viewModel = viewModel
+        self.viewModel = Weak(viewModel)
         self.delegate = delegate
     }
 
     public func formItems() -> [FormItem] {
+        guard let viewModel = viewModel.object else { return [] }
+
         return [
-            HeaderFormItem(text: "General", style: .plain),
+            LargeTextHeaderFormItem(text: "General").separatorColor(.clear),
             ValueFormItem(title: "Type",
                           value: viewModel.report.property?.type)
                 .accessory(ItemAccessory.dropDown)
                 .width(.column(2))
-                .onSelection { _ in
-                    self.delegate.didTapOnPropertyType()
+                .onSelection { [delegate] _ in
+                    delegate.didTapOnPropertyType()
             },
 
             ValueFormItem(title: "Sub Type",
                           value: viewModel.report.property?.subType)
                 .accessory(ItemAccessory.dropDown)
                 .width(.column(2))
-                .onSelection { _ in
-                    self.delegate.didTapOnPropertySubtype()
+                .onSelection { [delegate] _ in
+                    delegate.didTapOnPropertySubtype()
             },
 
             DropDownFormItem(title: "Involvements")
@@ -57,8 +59,8 @@ internal struct AddPropertyGeneralPluginDecorator: FormBuilderPluginDecorator {
                 .options(viewModel.involvements)
                 .selectedValue(viewModel.report.involvements)
                 .width(.column(1))
-                .onValueChanged { value in
-                    self.viewModel.report.involvements = value
+                .onValueChanged { [viewModel] value in
+                    viewModel.report.involvements = value
             }
         ]
     }
@@ -76,15 +78,17 @@ internal struct AddPropertyMediaPlugin: FormBuilderPlugin {
 
 internal struct AddPropertyMediaPluginDecorator: FormBuilderPluginDecorator {
 
-    weak var context: UIViewController?
-    var viewModel: PropertyDetailsViewModel
+    let context: Weak<UIViewController>
+    let viewModel: Weak<PropertyDetailsViewModel>
 
     init(viewModel: PropertyDetailsViewModel, context: UIViewController) {
-        self.viewModel = viewModel
-        self.context = context
+        self.viewModel = Weak(viewModel)
+        self.context = Weak(context)
     }
 
     public func formItems() -> [FormItem] {
+        guard let viewModel = viewModel.object else { return [] }
+        guard let context = context.object else { return [] }
 
         let localStore = DataStoreCoordinator(dataStore: MediaStorageDatastore(items: viewModel.report.media, container: viewModel.report))
         let gallery = MediaGalleryCoordinatorViewModel(storeCoordinator: localStore)
@@ -98,11 +102,14 @@ internal struct AddPropertyMediaPluginDecorator: FormBuilderPluginDecorator {
         if let viewController = UIApplication.shared.keyWindow?.rootViewController {
             mediaItem.previewingController(viewController)
         }
-        let header = HeaderFormItem(text: "Media").actionButton(title: "Manage", handler: { button in
-            if let viewController = mediaItem.delegate?.viewControllerForGalleryViewModel(gallery) {
-                self.context?.present(viewController, animated: true, completion: nil)
-            }
-        })
+
+        let header = LargeTextHeaderFormItem(text: "Media")
+            .separatorColor(.clear)
+            .actionButton(title: "Manage", handler: { [context] button in
+                if let viewController = mediaItem.delegate?.viewControllerForGalleryViewModel(gallery) {
+                    context.present(viewController, animated: true, completion: nil)
+                }
+            })
 
         return [
             header,
@@ -122,40 +129,43 @@ internal struct AddPropertyDetailsPlugin: FormBuilderPlugin {
 }
 
 internal struct AddPropertyDetailsPluginDecorator: FormBuilderPluginDecorator {
-    var viewModel: PropertyDetailsViewModel
+    let viewModel: Weak<PropertyDetailsViewModel>
 
     init(viewModel: PropertyDetailsViewModel) {
-        self.viewModel = viewModel
+        self.viewModel = Weak(viewModel)
     }
 
     public func formItems() -> [FormItem] {
+        guard let viewModel = viewModel.object else { return [] }
         guard let details = viewModel.report.property?.detailNames else { return [] }
         var formItems = [FormItem]()
 
         for detail in details {
-            formItems.append(formItem(for: detail))
+            formItems.append(formItem(for: detail)!)
         }
 
-        return [HeaderFormItem(text: "Details", style: .plain)] + formItems
+        return [LargeTextHeaderFormItem(text: "Property Details").separatorColor(.clear)] + formItems
     }
 
-    private func formItem(for propertyDetail: PropertyDetail) -> FormItem {
+    private func formItem(for propertyDetail: PropertyDetail) -> FormItem? {
+        guard let viewModel = viewModel.object else { return nil }
+
         switch propertyDetail.type {
         case let .picker(options):
             return DropDownFormItem(title: propertyDetail.title)
                 .options(options)
                 .width(.column(3))
                 .selectedValue([viewModel.report.details[propertyDetail.title] ?? ""])
-                .onValueChanged { value in
+                .onValueChanged { [viewModel] value in
                     guard let value = value?.first else { return }
-                    self.viewModel.report.details[propertyDetail.title] = value
+                    viewModel.report.details[propertyDetail.title] = value
             }
         case .text:
             return TextFieldFormItem(title: propertyDetail.title)
-                .text(self.viewModel.report.details[propertyDetail.title])
+                .text(viewModel.report.details[propertyDetail.title])
                 .width(.column(3))
-                .onValueChanged { text in
-                    self.viewModel.report.details[propertyDetail.title] = text
+                .onValueChanged { [viewModel] text in
+                    viewModel.report.details[propertyDetail.title] = text
             }
         }
     }
