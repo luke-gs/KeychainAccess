07a7b949724ea334305a2bf6047f444cd4ccb5fe
Herli <herli@gridstone.com.au>
2018-06-04 14:57:32 +1000

    Added different hockey identifiers

--- CAD/CAD/AppDelegate.swift
+++ CAD/CAD/AppDelegate.swift
@@ -78,14 +78,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         #if !DEBUG
             let manager = BITHockeyManager.shared()
-            manager.configure(withIdentifier: "6344d682b59c4ec5970c03192625efff")
+            manager.configure(withIdentifier: HOCKEY_APP_IDENTIFIER)
             manager.start()
 
             manager.updateManager.updateSetting = .checkStartup
             manager.crashManager.crashManagerStatus = .autoSend
 
             let authenticator = manager.authenticator
-            authenticator.authenticationSecret = "96bd361e7ec8fc1cba9c16bbc1118813"
+            authenticator.authenticationSecret = HOCKEY_SECRET
             authenticator.identificationType = .hockeyAppEmail
             authenticator.authenticateInstallation()
         #endif
