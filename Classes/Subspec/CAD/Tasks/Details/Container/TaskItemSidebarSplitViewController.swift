3b769857bf6bb3bda67980f0fd707352fdf17da6
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-06-07 09:23:10 +1000

    Fix for sidebar not updating to show title when async load of details

--- Classes/Subspec/CAD/Tasks/Details/Container/TaskItemSidebarSplitViewController.swift
+++ Classes/Subspec/CAD/Tasks/Details/Container/TaskItemSidebarSplitViewController.swift
@@ -128,6 +128,9 @@ open class TaskItemSidebarSplitViewController: SidebarSplitViewController {
         
         // Hide pencil icon if is compact or view model doesn't allow status changes
         pencilCircleView.isHidden = isCompact() || !detailViewModel.allowChangeResourceStatus()
+
+        // Reload the sidebar table as size may have changed
+        regularSidebarViewController.sidebarTableView?.reloadData()
     }
 
     /// Hides or shows compact change status bar based on trait collection, and configures views
