bbb557aafcf7df65ba16115895e08413f1308610
Kyle May <kyle.may@gridstone.com.au>
2018-06-07 13:25:56 +1000

    ARC, how does it work?

--- Classes/UI/Popover & Form Sheet/ForcedPopoverPresentationControllerDelegate.swift
+++ Classes/UI/Popover & Form Sheet/ForcedPopoverPresentationControllerDelegate.swift
@@ -7,6 +7,20 @@
 
 public class ForcedPopoverPresentationControllerDelegate: NSObject, UIPopoverPresentationControllerDelegate {
     
+    /// A strong reference to ourself as this is usually set to a weak delegate
+    private var strongSelf: ForcedPopoverPresentationControllerDelegate?
+    
+    public override init() {
+        super.init()
+        // Retain strong reference
+        strongSelf = self
+    }
+    
+    public func popoverPresentationControllerDidDismissPopover(_ popoverPresentationController: UIPopoverPresentationController) {
+        // Remove the strong self reference now we have dismissed
+        strongSelf = nil
+    }
+    
     public func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
     }
