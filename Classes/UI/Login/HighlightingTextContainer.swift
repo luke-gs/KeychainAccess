dbddbe507108c0ae84e2da46aec302681e6283f5
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2018-06-22 16:13:56 +1000

    Docs.

--- Classes/UI/Login/HighlightingTextContainer.swift
+++ Classes/UI/Login/HighlightingTextContainer.swift
@@ -5,11 +5,25 @@
 //  Copyright © 2018 Gridstone. All rights reserved.
 //
 
+/// A struct used to wrap the required information
+/// for the `HighlightingTextView`
 public struct HighlightTextContainerThing {
+
+    /// The text to display
     public var text: String
+
+    /// The text to highlight
     public var highlightText: String
+
+    /// The action to perform when the text is tapped
     public var action: ((UIViewController)->())?
 
+    /// Intializer
+    ///
+    /// - Parameters:
+    ///   - text: the text to display
+    ///   - highlightText: the text to highlight
+    ///   - action: the action to perform when the text is tapped
     public init(text: String, highlightText: String, action: ((UIViewController)->())?) {
         self.text = text
         self.highlightText = highlightText
@@ -17,7 +31,15 @@ public struct HighlightTextContainerThing {
     }
 }
 
+/// TextView which automatically highlights text in the form of a link
+/// to be used with the UITextViewDelegate`'s
+///
+/// `textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction)`
+///
+/// which still needs to be implemented by the delegate
 public class HighlightingTextView: UITextView {
+
+    /// The text highlight container
     public var highlightContainerThing: HighlightTextContainerThing? {
         didSet {
             guard let highlightContainerThing = highlightContainerThing else { return }
