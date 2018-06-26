797fbb9b423d53192f11c119f7d9ebbe754bd07d
Christian Bieniak <christian.bieniak@motorolasolutions.com>
2018-06-26 15:00:45 +1000

    TableViewStyle respects darkmode

--- Classes/Forms/Cell Animations/TableViewStyle.swift
+++ Classes/Forms/Cell Animations/TableViewStyle.swift
@@ -10,8 +10,11 @@ import Foundation
 public class TableViewStyle: CellSelectionAnimatable {
     public static func configure(_ cell: CollectionViewFormCell, isFocused focused: Bool) {
 
-
-        cell.backgroundColor = focused ? #colorLiteral(red: 0.9150436521, green: 0.9147670865, blue: 0.9054462314, alpha: 1) : .clear 
+        if ThemeManager.shared.currentInterfaceStyle.isDark {
+            cell.backgroundColor = focused ? .sidebarGray : .clear
+        } else {
+            cell.backgroundColor = focused ? #colorLiteral(red: 0.9150436521, green: 0.9147670865, blue: 0.9054462314, alpha: 1) : .clear
+        }
 
         let validationColor: UIColor? = cell.requiresValidation ? cell.validationColor : nil
         let finalColor = validationColor ?? cell.separatorColor
