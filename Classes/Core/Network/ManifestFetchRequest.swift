0612b782be6f639f516c83af8fdaf008da77dd67
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2018-07-11 13:39:50 +1000

    Add convenience for the Manifest fetching to network layer and update to use new interfaces.

--- Classes/Core/Network/ManifestFetchRequest.swift
+++ Classes/Core/Network/ManifestFetchRequest.swift
@@ -9,66 +9,77 @@ import Foundation
 import PromiseKit
 import Alamofire
 
-public struct ManifestFetchRequest: Parameterisable {
-    
-    public typealias ResultClass = [[String:Any]]
-    
-    /// The type to use for last update
-    public enum UpdateType {
-        /// A date time string
-        case dateTime
-        /// Time interval (for backwards compatibility)
-        case interval
+public enum ManifestFetchType: Equatable {
+    case full
+    case partial(collections: [ManifestCollection])
+
+    var path: String {
+        switch self {
+        case .full:
+            return "manifest/manifest"
+        case .partial(_):
+            return "manifest/manifest/categories"
+        }
     }
-    
-    
+
+    var method: HTTPMethod {
+        switch self {
+        case .full:
+            return .get
+        case .partial:
+            return .post
+        }
+    }
+
+    public static func ==(lhs: ManifestFetchType, rhs: ManifestFetchType) -> Bool {
+        switch (lhs, rhs) {
+        case (.partial, .full), (.full, .partial):
+            return false
+        default:
+            return true
+        }
+    }
+}
+
+public struct ManifestFetchRequest: Parameterisable {
+
+    public typealias ResultClass = [[String:Any]]
+
     public let parameters: [String: Any]
     public let path: String
     public let method: HTTPMethod
-    
-    public init(date: Date?, path: String = "manifest/app", parameters: [String: Any] = [:], method: HTTPMethod = .get, updateType: UpdateType = .interval) {
-        var path = path
-        var parameters = parameters
-        
+
+    public init(date: Date?, fetchType: ManifestFetchType = .full) {
+        var parameters = [String: Any]()
+
         // Set the date back 60 seconds to account for clock skew
         let date = date?.adding(seconds: -60)
-        
-        if updateType == .dateTime {
-            // Convert date to ISO8601
-            if let dateString = ISO8601DateTransformer.shared.reverse(date) {
-                // Only use it in the URL if this is a GET request
-                if method == .get {
-                    path.append("/{dateLastUpdated}")
-                }
-                parameters["dateLastUpdated"] = dateString
-            }
-        } else if updateType == .interval {
-            // Backwards compatibility to allow use of interval
-            if let date = date {
-                let interval = Int(date.timeIntervalSince1970)
-                parameters["interval"] = String(interval)
-            } else {
-                parameters["interval"] = "0"
-            }
-            
-            // Only use it in the URL if this is a GET request
-            if method == .get {
-                path.append("/{interval}")
-            }
+
+        // Convert date to ISO8601
+        if let dateString = ISO8601DateTransformer.shared.reverse(date) {
+            parameters["dateLastUpdated"] = dateString
         }
-        
-        self.path = path
+
+        switch fetchType {
+        case .full:
+            break
+        case .partial(let collections):
+            let categories = collections.map { $0.rawValue }
+            parameters = ["categories": categories]
+        }
+
+        self.path = fetchType.path
+        self.method = fetchType.method
         self.parameters = parameters
-        self.method = method
     }
 }
 
 public extension APIManager {
-    
+
     func fetchManifest(with request: ManifestFetchRequest) -> Promise<ManifestFetchRequest.ResultClass> {
         // Use JSON encoding if POST/PUT/PATCH, otherwise use default URL encoding
         let parameterEncoding: ParameterEncoding = [HTTPMethod.post, .put, .patch].contains(request.method) ? JSONEncoding.default : URLEncoding.queryString
-        
+
         let networkRequest = try! NetworkRequest(pathTemplate: request.path,
                                                  parameters: request.parameters,
                                                  method: request.method,
