d1ea774d572d1b0072edf8e9db667dae926a7b52
Herli <herli@gridstone.com.au>
2018-07-11 13:55:15 +1000

    Documentation and added extra key

--- Classes/Core/Location Manager/GeolocationPlugin.swift
+++ Classes/Core/Location Manager/GeolocationPlugin.swift
@@ -9,6 +9,10 @@
 import CoreLocation
 import PromiseKit
 
+/// Plugin that will inject **X-GPS-Latitude**, **X-GPS-Longitude**, **X-GPS-Time-Of-Day**, **X-GPS-Data-Age**,
+/// **X-GPS-Horizontal-Accuracy**, **X-GPS-Altitude-Accuracy**, **X-GPS-Altitude-Accuracy**, **X-GPS-Altitude**, **X-GPS-Direction-Of-Travel**,
+/// **X-GPS-Speed**, and **X-GPS-Timestamp**. X-GPS-Time-Of-Day will be removed in the future.
+/// to the header of requests. The data will be populated using `CLLocation`, sourced from internal LocationManager.
 open class GeolocationPlugin: PluginType {
     static let locationLatitudeKey = "X-GPS-Latitude"
     static let locationLongitudeKey = "X-GPS-Longitude"
@@ -19,6 +23,7 @@ open class GeolocationPlugin: PluginType {
     static let locationAltitudeKey = "X-GPS-Altitude"
     static let locationDirectionOfTravelKey = "X-GPS-Direction-Of-Travel"
     static let locationSpeed = "X-GPS-Speed"
+    static let locationTimestamp = "X-GPS-Timestamp"
     
     public init() {
 
@@ -33,8 +38,14 @@ open class GeolocationPlugin: PluginType {
             request.setValue(String(location.altitude), forHTTPHeaderField: GeolocationPlugin.locationAltitudeKey)
             request.setValue(String(location.horizontalAccuracy), forHTTPHeaderField: GeolocationPlugin.locationHorizontalAccuracyKey)
             request.setValue(String(location.verticalAccuracy), forHTTPHeaderField: GeolocationPlugin.locationVerticalAccuracyKey)
-            request.setValue(String(location.timestamp.minutesSinceMidnight()), forHTTPHeaderField: GeolocationPlugin.locationTimeOfDayKey)
             request.setValue(String(location.timestamp.timeSinceNow()), forHTTPHeaderField: GeolocationPlugin.locationDataAge)
+
+            // This is deprecated, remove when VicPol is fully ported.
+            request.setValue(String(location.timestamp.minutesSinceMidnight()), forHTTPHeaderField: GeolocationPlugin.locationTimeOfDayKey)
+
+            if let dateValue = ISO8601DateTransformer.shared.reverse(location.timestamp) {
+                request.setValue(dateValue, forHTTPHeaderField: GeolocationPlugin.locationTimestamp)
+            }
 
             if location.course >= 0.0 { // Check if valid
                 request.setValue(String(location.course), forHTTPHeaderField: GeolocationPlugin.locationDirectionOfTravelKey)
