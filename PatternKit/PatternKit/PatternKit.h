da06df3d4a8a82ff34e0f2b4b018e7c6f9774757
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-08-17 13:54:46 +1000

    CoreKit and PatternKit projects

---
+++ PatternKit/PatternKit/PatternKit.h
@@ -0,0 +1,19 @@
+//
+//  PatternKit.h
+//  PatternKit
+//
+//  Created by Trent Fitzgibbon on 17/8/18.
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PatternKit.
+FOUNDATION_EXPORT double PatternKitVersionNumber;
+
+//! Project version string for PatternKit.
+FOUNDATION_EXPORT const unsigned char PatternKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PatternKit/PublicHeader.h>
+
+
