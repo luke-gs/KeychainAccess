97b203ab5b6334fc792b581e64bc6b1b2c0f7803
Herli <herli@gridstone.com.au>
2018-09-05 20:29:14 +1000

    Additional protocols for BiometricDelegate

--- PublicSafetyKit/PublicSafetyKit/Classes/Features/Login/LoginProtocols.swift
+++ PublicSafetyKit/PublicSafetyKit/Classes/Features/Login/LoginProtocols.swift
@@ -63,8 +63,27 @@ public protocol CredentialsDelegate: LoginViewControllerDelegate {
     func loginViewController(_ controller: LoginViewController, didFinishWithCredentials: [LoginCredential])
 }
 
+public enum BiometricPromptEvent {
+    case loginViewControllerDidAppear
+    case applicationDidBecomeActive
+}
+
 /// LoginViewController delegate used with The basic credentials and biometrics
 public protocol BiometricDelegate: CredentialsDelegate {
+
+    /// Text to use for logging using biometric.
+    ///
+    /// - Parameter loginViewController: The login view controller.
+    /// - Returns: The appropriate prompt to login using biometric.
+    func biometricAuthenticationPrompt(for loginViewController: LoginViewController) -> String
+
+    /// Called on the specified event to ask whether the biometric prompt should be presented without user interaction.
+    ///
+    /// - Parameters:
+    ///   - loginViewController: The login view controller.
+    ///   - event: The event that cause the need to prompt.
+    /// - Returns: A Bool to indicate Whether the prompt should appear or not.
+    func loginViewController(_ loginViewController: LoginViewController, shouldPromptForEvent event: BiometricPromptEvent) -> Bool
 
     /// Triggers when The login button is pressed
     ///
@@ -72,6 +91,12 @@ public protocol BiometricDelegate: CredentialsDelegate {
     ///   - controller: The login view controller
     ///   - context: The local authentication context
     func loginViewControllerDidAuthenticateWithBiometric(_ controller: LoginViewController, context: LAContext)
+}
+
+extension BiometricDelegate {
+    public func loginViewController(_ loginViewController: LoginViewController, shouldPromptForEvent event: BiometricPromptEvent) -> Bool {
+        return false
+    }
 }
 
 /// LoginViewController delegate used with external authentication
