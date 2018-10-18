ce9713f44ac43eafa3928ebcf031881cdc1075aa
Evan <devillordking@gmail.com>
2018-10-18 15:42:44 +1100

    Fix memory leak in location view

--- MPOL/MPOL/Classes/UI/Events/Location/DefaultEventLocationViewController.swift
+++ MPOL/MPOL/Classes/UI/Events/Location/DefaultEventLocationViewController.swift
@@ -61,7 +61,8 @@ open class DefaultEventLocationViewController: MapFormBuilderViewController, Eva
             .selectedValue(LocationSelectionCore(eventLocation: viewModel.report.eventLocation))
             .accessory(ImageAccessoryItem(image: AssetManager.shared.image(forKey: .iconPencil)!))
             .required()
-            .onValueChanged({ (location) in
+            .onValueChanged({ [weak self] (location) in
+                guard let `self` = self else { return }
                 if let location = location {
                     self.viewModel.report.eventLocation = EventLocation(locationSelection: location)
                     self.updateAnnotation()
