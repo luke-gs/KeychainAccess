d58ac91731ae9c888375bbdbca8ded29997e48e7
Christian <christian.vassallo@gridstone.com.au>
2018-10-23 09:31:26 +1100

    remove employee number default value

--- MPOL/MPOL/Classes/Model/Objects/Officer.swift
+++ MPOL/MPOL/Classes/Model/Objects/Officer.swift
@@ -30,72 +30,77 @@ open class Officer: MPOLKitEntity, Identifiable {
     open var familyName: String?
     open var middleNames: String?
     open var rank: String?
-    open var employeeNumber: String = ""
+    open var employeeNumber: String
     open var region: String?
 
     // TODO: Proper Involvements
     open var involvements: [String] = []
 
     public override init(id: String = UUID().uuidString) {
+        employeeNumber = ""
         super.init(id: id)
     }
 
     public required init(unboxer: Unboxer) throws {
 
-        do { try super.init(unboxer: unboxer) }
         givenName = unboxer.unbox(key: CodingKeys.givenName.rawValue)
         middleNames = unboxer.unbox(key: CodingKeys.middleNames.rawValue)
         familyName = unboxer.unbox(key: CodingKeys.familyName.rawValue)
         rank = unboxer.unbox(key: CodingKeys.rank.rawValue)
         employeeNumber = unboxer.unbox(key: CodingKeys.employeeNumber.rawValue) ?? ""
         region = unboxer.unbox(key: CodingKeys.region.rawValue)
+
+        do { try super.init(unboxer: unboxer) }
     }
 
     public required init?(coder aDecoder: NSCoder) {
 
-        super.init(coder: aDecoder)
         givenName = aDecoder.decodeObject(of: NSString.self, forKey: CodingKeys.givenName.rawValue) as String?
         middleNames = aDecoder.decodeObject(of: NSString.self, forKey: CodingKeys.middleNames.rawValue) as String?
         familyName = aDecoder.decodeObject(of: NSString.self, forKey: CodingKeys.familyName.rawValue) as String?
         rank = aDecoder.decodeObject(of: NSString.self, forKey: CodingKeys.rank.rawValue) as String?
         employeeNumber = aDecoder.decodeObject(of: NSString.self, forKey: CodingKeys.employeeNumber.rawValue) as String? ?? ""
         region = aDecoder.decodeObject(of: NSString.self, forKey: CodingKeys.region.rawValue) as String?
+
+        super.init(coder: aDecoder)
     }
 
     open override func encode(with aCoder: NSCoder) {
-        super.encode(with: aCoder)
+
         aCoder.encode(givenName, forKey: CodingKeys.givenName.rawValue)
         aCoder.encode(middleNames, forKey: CodingKeys.middleNames.rawValue)
         aCoder.encode(familyName, forKey: CodingKeys.familyName.rawValue)
         aCoder.encode(rank, forKey: CodingKeys.rank.rawValue)
         aCoder.encode(employeeNumber, forKey: CodingKeys.employeeNumber.rawValue)
         aCoder.encode(region, forKey: CodingKeys.region.rawValue)
+
+        super.encode(with: aCoder)
     }
 
     public required init(from decoder: Decoder) throws {
 
-        do { try super.init(from: decoder) }
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        
         givenName = try values.decodeIfPresent(String.self, forKey: .givenName)
         familyName = try values.decodeIfPresent(String.self, forKey: .familyName)
         middleNames = try values.decodeIfPresent(String.self, forKey: .middleNames)
         rank = try values.decodeIfPresent(String.self, forKey: .rank)
         region = try values.decodeIfPresent(String.self, forKey: .region)
         employeeNumber = try values.decodeIfPresent(String.self, forKey: .employeeNumber) ?? ""
+
+        do { try super.init(from: decoder) }
     }
 
     open override func encode(to encoder: Encoder) throws {
 
-        try super.encode(to: encoder)
         var container = encoder.container(keyedBy: CodingKeys.self)
-
         try container.encodeIfPresent(givenName, forKey: .givenName)
         try container.encodeIfPresent(familyName, forKey: .familyName)
         try container.encodeIfPresent(middleNames, forKey: .middleNames)
         try container.encodeIfPresent(rank, forKey: .rank)
         try container.encodeIfPresent(region, forKey: .region)
         try container.encodeIfPresent(employeeNumber, forKey: .employeeNumber)
+
+        try super.encode(to: encoder)
     }
 
     // MARK: - Equality
