99423a157ad3c7a7c5ce5c92c312d3f4e0536929
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-10-25 11:53:42 +1100

    Alert

--- MPOL/MPOL/Classes/Model/Objects/Alert.swift
+++ MPOL/MPOL/Classes/Model/Objects/Alert.swift
@@ -12,9 +12,7 @@ import Unbox
 @objc(MPLAlert)
 open class Alert: NSObject, Serialisable {
 
-    private static let dateTransformer: ISO8601DateTransformer = ISO8601DateTransformer.shared
-
-    public enum Level: Int, UnboxableEnum {
+    public enum Level: Int, UnboxableEnum, Codable {
 
         case low    = 0
         case medium = 1
@@ -41,22 +39,20 @@ open class Alert: NSObject, Serialisable {
         public static var all: [Level] = [.high, .medium, .low]
     }
 
-    open var id: String
-    open var level: Alert.Level?
-
+    open var createdBy: String?
     open var dateCreated: Date?
     open var dateUpdated: Date?
-    open var createdBy: String?
-    open var updatedBy: String?
+    open var details: String?
     open var effectiveDate: Date?
-    open var expiryDate: Date?
     open var entityType: String?
+    open var expiryDate: Date?
+    open var id: String
     open var isSummary: Bool = false
-
+    open var jurisdiction: String?
+    open var level: Alert.Level?
     open var source: MPOLSource?
     open var title: String?
-    open var details: String?
-    open var jurisdiction: String?
+    open var updatedBy: String?
 
     // MARK: - Equality
 
@@ -77,6 +73,8 @@ open class Alert: NSObject, Serialisable {
     }
 
     // MARK: - Unboxable
+
+    private static let dateTransformer: ISO8601DateTransformer = ISO8601DateTransformer.shared
 
     required public init(unboxer: Unboxer) throws {
 
@@ -104,63 +102,63 @@ open class Alert: NSObject, Serialisable {
         super.init()
     }
 
-    // MARK: - NSSecureCoding
+    // MARK: - Codable
 
-    public required init?(coder aDecoder: NSCoder) {
-        guard let id = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.id.rawValue) as String? else {
-            return nil
-        }
-
-        self.id = id
-        isSummary = aDecoder.decodeBool(forKey: CodingKey.isSummary.rawValue)
-
-        super.init()
-
-        title = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.title.rawValue) as String?
-        details = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.details.rawValue) as String?
-        jurisdiction = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.jurisdiction.rawValue) as String?
-        effectiveDate = aDecoder.decodeObject(of: NSDate.self, forKey: CodingKey.effectiveDate.rawValue) as Date?
-
-        if aDecoder.containsValue(forKey: CodingKey.level.rawValue),
-            let level = Level(rawValue: aDecoder.decodeInteger(forKey: CodingKey.level.rawValue)) {
-            self.level = level
-        }
-
-        dateCreated = aDecoder.decodeObject(of: NSDate.self, forKey: CodingKey.dateCreated.rawValue) as Date?
-        dateUpdated = aDecoder.decodeObject(of: NSDate.self, forKey: CodingKey.dateUpdated.rawValue) as Date?
-        expiryDate = aDecoder.decodeObject(of: NSDate.self, forKey: CodingKey.expiryDate.rawValue) as Date?
-        createdBy = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.createdBy.rawValue) as String?
-        updatedBy = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.updatedBy.rawValue) as String?
-        entityType = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.entityType.rawValue) as String?
-
-        if let source = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.source.rawValue) as String? {
-            self.source = MPOLSource(rawValue: source)
-        }
+    private enum CodingKeys: String, CodingKey {
+        case createdBy
+        case dateCreated
+        case dateUpdated
+        case details
+        case effectiveDate
+        case entityType
+        case expiryDate
+        case id
+        case isSummary
+        case jurisdiction
+        case level
+        case source
+        case title
+        case updatedBy
+        case version
     }
 
-    open func encode(with aCoder: NSCoder) {
-        aCoder.encode(Alert.modelVersion, forKey: CodingKey.version.rawValue)
-        aCoder.encode(id, forKey: CodingKey.id.rawValue)
-        aCoder.encode(jurisdiction, forKey: CodingKey.jurisdiction.rawValue)
-        if let level = level?.rawValue {
-            aCoder.encode(level, forKey: CodingKey.level.rawValue)
-        }
-
-        aCoder.encode(dateCreated, forKey: CodingKey.dateCreated.rawValue)
-        aCoder.encode(dateUpdated, forKey: CodingKey.dateUpdated.rawValue)
-        aCoder.encode(expiryDate, forKey: CodingKey.expiryDate.rawValue)
-        aCoder.encode(createdBy, forKey: CodingKey.createdBy.rawValue)
-        aCoder.encode(updatedBy, forKey: CodingKey.updatedBy.rawValue)
-        aCoder.encode(entityType, forKey: CodingKey.entityType.rawValue)
-        aCoder.encode(isSummary, forKey: CodingKey.isSummary.rawValue)
-        aCoder.encode(source?.rawValue, forKey: CodingKey.source.rawValue)
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        createdBy = try container.decodeIfPresent(String.self, forKey: .createdBy)
+        dateCreated = try container.decodeIfPresent(Date.self, forKey: .dateCreated)
+        dateUpdated = try container.decodeIfPresent(Date.self, forKey: .dateUpdated)
+        details = try container.decodeIfPresent(String.self, forKey: .details)
+        effectiveDate = try container.decodeIfPresent(Date.self, forKey: .effectiveDate)
+        entityType = try container.decodeIfPresent(String.self, forKey: .entityType)
+        expiryDate = try container.decodeIfPresent(Date.self, forKey: .expiryDate)
+        id = try container.decode(String.self, forKey: .id)
+        isSummary = try container.decode(Bool.self, forKey: .isSummary)
+        jurisdiction = try container.decodeIfPresent(String.self, forKey: .jurisdiction)
+        level = try container.decodeIfPresent(Level.self, forKey: .level)
+        source = try container.decodeIfPresent(MPOLSource.self, forKey: .source)
+        title = try container.decodeIfPresent(String.self, forKey: .title)
+        updatedBy = try container.decodeIfPresent(String.self, forKey: .updatedBy)
     }
 
-    public static var supportsSecureCoding: Bool {
-        return true
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(createdBy, forKey: CodingKeys.createdBy)
+        try container.encode(dateCreated, forKey: CodingKeys.dateCreated)
+        try container.encode(dateUpdated, forKey: CodingKeys.dateUpdated)
+        try container.encode(details, forKey: CodingKeys.details)
+        try container.encode(effectiveDate, forKey: CodingKeys.effectiveDate)
+        try container.encode(entityType, forKey: CodingKeys.entityType)
+        try container.encode(expiryDate, forKey: CodingKeys.expiryDate)
+        try container.encode(id, forKey: CodingKeys.id)
+        try container.encode(isSummary, forKey: CodingKeys.isSummary)
+        try container.encode(jurisdiction, forKey: CodingKeys.jurisdiction)
+        try container.encode(level, forKey: CodingKeys.level)
+        try container.encode(source, forKey: CodingKeys.source)
+        try container.encode(title, forKey: CodingKeys.title)
+        try container.encode(updatedBy, forKey: CodingKeys.updatedBy)
     }
 
-    // MARK: - Model Versionable
+    // MARK: - ModelVersionable
     public static var modelVersion: Int {
         return 0
     }
@@ -185,20 +183,3 @@ extension Alert.Level: Comparable {
     }
 }
 
-private enum CodingKey: String {
-    case version
-    case id
-    case level
-    case title
-    case details
-    case jurisdiction
-    case effectiveDate
-    case dateCreated
-    case dateUpdated
-    case createdBy
-    case updatedBy
-    case expiryDate
-    case entityType
-    case isSummary
-    case source
-}
