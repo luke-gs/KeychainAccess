89850c2840bf3041974d3a60d12360912e22d19c
Trent Fitzgibbon <trentf@gridstone.com.au>
2018-10-26 14:18:37 +1100

    TelephoneNumber

--- MPOL/MPOL/Classes/Model/Objects/TelephoneNumber.swift
+++ MPOL/MPOL/Classes/Model/Objects/TelephoneNumber.swift
@@ -10,21 +10,20 @@ import Unbox
 import PublicSafetyKit
 
 @objc(MPLTelephoneNumber)
-open class TelephoneNumber: NSObject, Serialisable {
+open class TelephoneNumber: DefaultSerialisable {
 
     // MARK: - Properties
 
-    public let id: String
-
-    open var suffix: String?
+    open var areaCode: String?
     open var cityCode: String?
+    open var countryCode: String?
+    open var exchange: String?
     open var fullNumber: String?
+    open var id: String
+    open var numberType: String?
     open var prefix: String?
     open var subscriber: String?
-    open var areaCode: String?
-    open var exchange: String?
-    open var numberType: String?
-    open var countryCode: String?
+    open var suffix: String?
 
     public init(id: String) {
         self.id = id
@@ -48,53 +47,59 @@ open class TelephoneNumber: NSObject, Serialisable {
         exchange = unboxer.unbox(key: "exchange")
         numberType = unboxer.unbox(key: "numberType")
         countryCode = unboxer.unbox(key: "countryCode")
+        
+        try super.init(unboxer: unboxer)
     }
 
-    public required init?(coder aDecoder: NSCoder) {
-        id = (aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.id.rawValue) as String?)!
 
-        super.init()
+    // MARK: - Codable
 
-        suffix = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.suffix.rawValue) as String?
-        cityCode = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.cityCode.rawValue) as String?
-        fullNumber = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.fullNumber.rawValue) as String?
-        prefix = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.prefix.rawValue) as String?
-        subscriber = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.subscriber.rawValue) as String?
-        areaCode = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.areaCode.rawValue) as String?
-        exchange = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.exchange.rawValue) as String?
-        numberType = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.numberType.rawValue) as String?
-        countryCode = aDecoder.decodeObject(of: NSString.self, forKey: CodingKey.countryCode.rawValue) as String?
-    }
-
-    open func encode(with aCoder: NSCoder) {
-        aCoder.encode(TelephoneNumber.modelVersion, forKey: CodingKey.version.rawValue)
-        aCoder.encode(id, forKey: CodingKey.id.rawValue)
-        aCoder.encode(suffix, forKey: CodingKey.suffix.rawValue)
-        aCoder.encode(cityCode, forKey: CodingKey.cityCode.rawValue)
-        aCoder.encode(fullNumber, forKey: CodingKey.fullNumber.rawValue)
-        aCoder.encode(prefix, forKey: CodingKey.prefix.rawValue)
-        aCoder.encode(subscriber, forKey: CodingKey.subscriber.rawValue)
-        aCoder.encode(areaCode, forKey: CodingKey.areaCode.rawValue)
-        aCoder.encode(exchange, forKey: CodingKey.exchange.rawValue)
-        aCoder.encode(numberType, forKey: CodingKey.numberType.rawValue)
-        aCoder.encode(countryCode, forKey: CodingKey.countryCode.rawValue)
-    }
-
-    public static var supportsSecureCoding: Bool { return true }
-    public static var modelVersion: Int { return 0 }
-
-    private enum CodingKey: String {
-        case version
-        case id
-        case suffix
+    private enum CodingKeys: String, CodingKey {
+        case areaCode
         case cityCode
+        case countryCode
+        case exchange
         case fullNumber
+        case id
+        case numberType
         case prefix
         case subscriber
-        case areaCode
-        case exchange
-        case numberType
-        case countryCode
+        case suffix
+    }
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+
+        try super.init(from: decoder)
+        guard !dataMigrated else { return }
+
+        areaCode = try container.decodeIfPresent(String.self, forKey: .areaCode)
+        cityCode = try container.decodeIfPresent(String.self, forKey: .cityCode)
+        countryCode = try container.decodeIfPresent(String.self, forKey: .countryCode)
+        exchange = try container.decodeIfPresent(String.self, forKey: .exchange)
+        fullNumber = try container.decodeIfPresent(String.self, forKey: .fullNumber)
+        id = try container.decode(String.self, forKey: .id)
+        numberType = try container.decodeIfPresent(String.self, forKey: .numberType)
+        prefix = try container.decodeIfPresent(String.self, forKey: .prefix)
+        subscriber = try container.decodeIfPresent(String.self, forKey: .subscriber)
+        suffix = try container.decodeIfPresent(String.self, forKey: .suffix)
+    }
+
+    open override func encode(to encoder: Encoder) throws {
+        try super.encode(to: encoder)
+
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(areaCode, forKey: CodingKeys.areaCode)
+        try container.encode(cityCode, forKey: CodingKeys.cityCode)
+        try container.encode(countryCode, forKey: CodingKeys.countryCode)
+        try container.encode(exchange, forKey: CodingKeys.exchange)
+        try container.encode(fullNumber, forKey: CodingKeys.fullNumber)
+        try container.encode(id, forKey: CodingKeys.id)
+        try container.encode(numberType, forKey: CodingKeys.numberType)
+        try container.encode(prefix, forKey: CodingKeys.prefix)
+        try container.encode(subscriber, forKey: CodingKeys.subscriber)
+        try container.encode(suffix, forKey: CodingKeys.suffix)
     }
 
 }
