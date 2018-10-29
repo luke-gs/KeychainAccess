6dbeed188b0bdedc767aeab2030c45cb76c10a83
Christian <christian.vassallo@gridstone.com.au>
2018-10-30 07:55:30 +1100

    manage sections based on wether or not their are items for them

--- MPOL/MPOL/Classes/UI/Events/Officers/OfficerSearchViewModel.swift
+++ MPOL/MPOL/Classes/UI/Events/Officers/OfficerSearchViewModel.swift
@@ -24,11 +24,7 @@ class OfficerSearchViewModel: SearchDisplayableViewModel {
 
     private var sections: [OfficerSearchSectionViewModel] = []
 
-    public init() {
-
-        sections.append(OfficerSearchSectionViewModel(items: [], title: "My Call Sign"))
-        sections.append(OfficerSearchSectionViewModel(items: [], title: "Recently Used"))
-    }
+    public init() { }
 
     func numberOfSections() -> Int {
         if hasSections {
@@ -138,12 +134,15 @@ class OfficerSearchViewModel: SearchDisplayableViewModel {
     public func fetchRecentOfficers() -> Promise<Void> {
 
         items.removeAll()
+        sections.removeAll()
 
         // Add officers from myCallSign except yourself
         if let myCallSignOfficers = CADStateManager.shared.lastBookOn?.employees.compactMap({ $0 as? Object })
-            .filter({ $0.id !=  CADStateManager.shared.officerDetails?.id}) {
+            .filter({ $0.id !=  CADStateManager.shared.officerDetails?.id}), !myCallSignOfficers.isEmpty {
+
+                sections.append(OfficerSearchSectionViewModel(items: [], title: "My Call Sign"))
                 items = myCallSignOfficers
-                sections[0].items = myCallSignOfficers
+                sections.first?.items = myCallSignOfficers
             }
 
         //Add officers from UserPreferences recentlyUsed
@@ -157,8 +156,12 @@ class OfficerSearchViewModel: SearchDisplayableViewModel {
 
         return RecentlyUsedEntityManager.default.entities(forIds: officerIds, ofServerType: Officer.serverTypeRepresentation).done { [weak self] result in
             let recentOfficers = officerIds.compactMap { result[$0] as? Officer }
-            self?.items += recentOfficers
-            self?.sections[1].items = recentOfficers
+
+            if !recentOfficers.isEmpty {
+                self?.sections.append(OfficerSearchSectionViewModel(items: [], title: "Recently Used"))
+                self?.items += recentOfficers
+                self?.sections.last?.items = recentOfficers
+            }
         }.map {}
     }
 
