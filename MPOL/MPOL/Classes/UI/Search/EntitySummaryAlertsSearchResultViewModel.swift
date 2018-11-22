319d56cf3111bafa111d7e7222548d7a8aa010f2
Campbell Graham <campbell.graham@gridstone.com.au>
2018-11-22 16:12:21 +1100

    Refactoring

--- MPOL/MPOL/Classes/UI/Search/EntitySummaryAlertsSearchResultViewModel.swift
+++ MPOL/MPOL/Classes/UI/Search/EntitySummaryAlertsSearchResultViewModel.swift
@@ -97,16 +97,16 @@ public class EntitySummaryAlertsSearchResultViewModel<T: MPOLKitEntity>: EntityS
 
             if searchComplete && self.shouldReadAlerts {
                 if alertEntities.isEmpty {
-                    TextToSpeechHelper.default.queue(AssetManager.shared.string(forKey: .noResultsFoundMessage))
+                    TextToSpeechHelper.default.speak(AssetManager.shared.string(forKey: .noResultsFoundMessage))
                 } else {
                     let entity = alertEntities.first!
 
                     if let speakableEntity = entity as? Speakable {
-                        TextToSpeechHelper.default.queue(speakableEntity)
+                        TextToSpeechHelper.default.speak(speakableEntity)
                     }
 
                     if alertEntities.count > 1 {
-                        TextToSpeechHelper.default.queue(AssetManager.shared.string(forKey: .multipleMatchesFoundMessage))
+                        TextToSpeechHelper.default.speak(AssetManager.shared.string(forKey: .multipleMatchesFoundMessage))
                     }
                 }
             }
