f1629d22b0d8467e90c463e529b084dad108b11a
Evan <devillordking@gmail.com>
2018-11-26 10:41:55 +1100

    add sub viewController

---
+++ MPOL/MPOL/Classes/UI/Entity Creation/Organisation Edit/OrganisationEditContactFormViewController.swift
@@ -0,0 +1,95 @@
+//
+//  OrganisationEditContactFormViewController.swift
+//  MPOL
+//
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+import PublicSafetyKit
+
+public class OrganisationEditContactFormViewController: FormBuilderViewController {
+
+    // MARK: PUBLIC
+
+    public var viewModel: OrganisationEditContactFormViewModel
+
+    /// The handler for submitting the data
+    public var submitHandler: ((Contact?) -> Void)?
+
+    public init(viewModel: OrganisationEditContactFormViewModel, submitHandler: ((Contact?) -> Void)?) {
+        self.viewModel = viewModel
+        self.submitHandler = submitHandler
+        super.init()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        MPLCodingNotSupported()
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: AssetManager.shared.string(forKey: .submitFormCancel),
+                                                           style: .plain, target: self, action: #selector(didTapCancelButton(_:)))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: AssetManager.shared.string(forKey: .submitFormDone),
+                                                            style: .done, target: self, action: #selector(didTapDoneButton(_:)))
+    }
+
+    public override func construct(builder: FormBuilder) {
+        title = NSLocalizedString("Add Contact Details", comment: "")
+        if viewModel.contact == nil {
+            viewModel.contact = Contact(id: UUID().uuidString)
+        }
+        builder += DropDownFormItem()
+            .title(NSLocalizedString("Contact Type", comment: ""))
+            .options(Contact.ContactType.allCases.map { AnyPickable($0) })
+            .required()
+            .selectedValue(viewModel.selectedType != nil ? [AnyPickable(viewModel.selectedType!)] : [])
+            .onValueChanged { [unowned self] value in
+                self.viewModel.selectedType = value?.first?.base as? Contact.ContactType
+                self.viewModel.contact?.value = nil
+                self.reloadForm()
+            }
+            .width(.column(1))
+
+        guard let type = viewModel.selectedType else { return }
+        viewModel.contact?.type = type
+        let formItem = TextFieldFormItem()
+            .title(viewModel.selectedType?.title)
+            .text(viewModel.contact?.value)
+            .required()
+            .width(.column(1))
+            .accessory(ItemAccessory.pencil)
+            .onValueChanged {
+                self.viewModel.contact?.value = $0
+            }
+        if viewModel.selectedType == .email {
+            formItem.softValidate(EmailSpecification(), message: "Invalid email address")
+            formItem.keyboardType(.emailAddress)
+        } else {
+            formItem.keyboardType(.phonePad)
+        }
+        builder += formItem
+        builder += TextFieldFormItem()
+            .title(NSLocalizedString("Remarks", comment: ""))
+            .text(viewModel.contact?.remark)
+            .width(.column(1))
+            .onValueChanged {
+                self.viewModel.contact?.remark = $0
+        }
+    }
+
+    @objc open func didTapCancelButton(_ button: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @objc open func didTapDoneButton(_ button: UIBarButtonItem) {
+        let result = builder.validate()
+        switch result {
+        case .invalid:
+            builder.validateAndUpdateUI()
+        case .valid:
+            submitHandler?(viewModel.contact)
+            dismiss(animated: true, completion: nil)
+        }
+    }
+
+}
---
+++ MPOL/MPOL/Classes/UI/Entity Creation/Organisation Edit/OrganisationEditViewController.swift
@@ -0,0 +1,189 @@
+//
+//  OrganisationEditViewController.swift
+//  MPOL
+//
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import PublicSafetyKit
+
+public class OrganisationEditViewController: FormBuilderViewController {
+
+    // MARK: - Storage
+
+    private var finalOrganisation = Organisation(id: UUID().uuidString)
+
+    private var location: LocationSelectionType?
+
+    public init(initialOrganisation: Organisation? = nil) {
+        if let initialOrganisation = initialOrganisation {
+            self.finalOrganisation = initialOrganisation
+        }
+        super.init()
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTapped(_:)))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(submitButtonTapped(_:)))
+    }
+
+    required convenience public init?(coder aDecoder: NSCoder) {
+        MPLCodingNotSupported()
+    }
+
+    public override func construct(builder: FormBuilder) {
+        builder.title = NSLocalizedString("Create New Organisation", comment: "Nav Title")
+
+        builder += LargeTextHeaderFormItem(text: NSLocalizedString("Details", comment: "Details Section Header")).separatorColor(.clear)
+
+        if let items = Manifest.shared.entries(for: .organisationType)?.rawValues().map({ AnyPickable($0) }) {
+            builder += DropDownFormItem()
+                .title(NSLocalizedString("Organisation Type", comment: "Drop Down Title"))
+                .options(items)
+                .required()
+                .width(.column(4))
+        }
+
+        builder += TextFieldFormItem().title(NSLocalizedString("Organisation Name", comment: "Title"))
+            .required()
+            .width(.column(4))
+
+        builder += TextFieldFormItem().title(NSLocalizedString("ABN", comment: "Title"))
+            .width(.column(4))
+
+        builder += TextFieldFormItem().title(NSLocalizedString("ACN", comment: "Title"))
+            .width(.column(4))
+
+        builder += PickerFormItem(pickerAction:
+            LocationSelectionFormAction(workflowId: LocationSelectionPresenter.organisationEditWorkflowId))
+            .title(NSLocalizedString("Address", comment: "Address field title"))
+            .selectedValue(location)
+            .width(.column(1))
+            .required()
+            .onValueChanged { [unowned self] (location) in
+                if let location = location as? LocationSelectionCore {
+                    self.location = location
+                }
+                self.reloadForm()
+            }
+            .editActions([CollectionViewFormEditAction(title: NSLocalizedString("Remove", comment: ""),
+                                                       color: UIColor.red,
+                                                       handler: { [unowned self] (_, _) in
+                                                        self.location = nil
+                                                        self.reloadForm()
+            })])
+
+        // Contact section
+        let contactHeaderText = finalOrganisation.contacts?.isEmpty ?? true
+            ? NSLocalizedString("Contact Details", comment: "header when no contacts exist")
+            : String.localizedStringWithFormat(NSLocalizedString("Contact Details (%d)", comment: "header when contacts exist"), finalOrganisation.contacts!.count)
+        builder += LargeTextHeaderFormItem(text: contactHeaderText)
+            .actionButton(title: NSLocalizedString("Add", comment: ""), handler: { [unowned self] _ in
+                self.present(
+                    EntityScreen.createOrganisationContactDetail(contact: nil,
+                                                           submitHandler: { [unowned self] contact in
+                                                            guard let contact = contact else { return }
+                                                            if self.finalOrganisation.contacts != nil {
+                                                                self.finalOrganisation.contacts!.append(contact)
+                                                            } else {
+                                                                self.finalOrganisation.contacts = [contact]
+                                                            }
+                                                            self.reloadForm()
+                    }))
+            })
+
+        if let contacts = finalOrganisation.contacts {
+            for (index, contact) in contacts.enumerated() {
+                let formItem = ValueFormItem()
+                    .title(contact.type?.title)
+                    .value(StringSizing(string: contact.value, font: .preferredFont(forTextStyle: .headline, compatibleWith: traitCollection)))
+                    .width(.column(1))
+                    .accessory(ItemAccessory.pencil)
+                    .editActions([CollectionViewFormEditAction(title: NSLocalizedString("Remove", comment: ""),
+                                                               color: UIColor.red,
+                                                               handler: { [unowned self] (_, _) in
+                                                                self.finalOrganisation.contacts?.remove(at: index)
+                                                                self.reloadForm()
+                    })])
+                    .onSelection { [unowned self] _ in
+                        self.present(
+                            EntityScreen.createOrganisationContactDetail(contact: contact,
+                                                                   submitHandler: { [unowned self] contact in
+                                                                    guard let contact = contact else { return }
+                                                                    self.finalOrganisation.contacts?[index] = contact
+                                                                    self.reloadForm()
+                            }))
+                }
+                builder += formItem
+            }
+        }
+
+        // Alias section
+        let aliasHeaderText = finalOrganisation.aliases?.isEmpty ?? true
+            ? NSLocalizedString("Aliases", comment: "header when no aliases exist")
+            : String.localizedStringWithFormat(NSLocalizedString("Aliases (%d)", comment: "header when aliases exist"), finalOrganisation.aliases!.count)
+        builder += LargeTextHeaderFormItem(text: aliasHeaderText)
+            .actionButton(title: NSLocalizedString("Add", comment: ""), handler: { [unowned self] _ in
+                self.present(
+                    EntityScreen.createOrganisationAliasDetail(alias: nil,
+                                                         submitHandler: { [unowned self] organisationAlias in
+                                                            guard let organisationAlias = organisationAlias else { return }
+                                                            if self.finalOrganisation.aliases != nil {
+                                                                self.finalOrganisation.aliases!.append(organisationAlias)
+                                                            } else {
+                                                                self.finalOrganisation.aliases = [organisationAlias]
+                                                            }
+                                                            self.reloadForm()
+                    }))
+            })
+
+        if let aliases = finalOrganisation.aliases {
+            for (index, alias) in aliases.enumerated() {
+                let formItem = ValueFormItem()
+                    .title(alias.type?.title)
+                    .value(StringSizing(string: alias.alias, font: .preferredFont(forTextStyle: .headline, compatibleWith: traitCollection)))
+                    .width(.column(1))
+                    .accessory(ItemAccessory.pencil)
+                    .editActions([CollectionViewFormEditAction(title: NSLocalizedString("Remove", comment: ""),
+                                                               color: UIColor.red,
+                                                               handler: { [unowned self] (_, _) in
+                                                                self.finalOrganisation.contacts?.remove(at: index)
+                                                                self.reloadForm()
+                    })])
+                    .onSelection { [unowned self] _ in
+                        self.present(
+                            EntityScreen.createOrganisationAliasDetail(alias: alias,
+                                                                         submitHandler: { [unowned self] alias in
+                                                                            guard let alias = alias else { return }
+                                                                            self.finalOrganisation.aliases?[index] = alias
+                                                                            self.reloadForm()
+                            }))
+                }
+                builder += formItem
+            }
+        }
+
+    }
+
+    // MARK: - Private
+
+    @objc private func cancelButtonTapped(_ item: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @objc private func submitButtonTapped(_ item: UIBarButtonItem) {
+        let result = builder.validate()
+        switch result {
+        case .invalid:
+            builder.validateAndUpdateUI()
+        case .valid:
+            do {
+                try UserSession.current.userStorage?.addEntity(object: finalOrganisation,
+                                                               key: UserStorage.createdEntitiesKey,
+                                                               notification: NSNotification.Name.createdEntitiesDidUpdate)
+            } catch {
+                // TODO: Handles error if it cannot be saved
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+
+}
